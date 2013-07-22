765a25bed04eb4f97b544a63e91deee60c7428bc
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-07-22 16:59:20 +0200

    test nested views under "principal"

--- public-angular/app/scripts/controllers/principal.js
+++ public-angular/app/scripts/controllers/principal.js
@@ -8,3 +8,29 @@ angular.module('publicAngularApp')
       'Karma'
     ];
   });
+
+angular.module('publicAngularApp')
+  .controller('PrincipalClassesCtrl', function ($scope) {
+    $scope.classes = [
+      '3ème A',
+      '6ème ZX',
+      'Karma'
+    ];
+  });
+
+angular.module('publicAngularApp')
+  .controller('PrincipalEnseignantsCtrl', function ($scope) {
+    $scope.enseignants = [
+      'M. Raymond',
+      'Melle Roux',
+      'Maitre Hibou'
+    ];
+  });
+
+angular.module('publicAngularApp')
+  .controller('PrincipalEnseignantCtrl', function ($scope) {
+    $scope.classes = [
+      '3ème A',
+      '6ème ZX'
+    ];
+  });
