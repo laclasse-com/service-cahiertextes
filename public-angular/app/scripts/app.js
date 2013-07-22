4df4b789f7c159825d141b2786c68fc96adbece1
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-07-22 14:49:53 +0200

    ajout 3 embryons de vues

--- public-angular/app/scripts/app.js
+++ public-angular/app/scripts/app.js
@@ -7,6 +7,18 @@ angular.module('publicAngularApp', [])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
+      .when('/principal', {
+        templateUrl: 'views/principal.html',
+        controller: 'PrincipalCtrl'
+      })
+      .when('/eleve', {
+        templateUrl: 'views/eleve.html',
+        controller: 'EleveCtrl'
+      })
+      .when('/professeur', {
+        templateUrl: 'views/professeur.html',
+        controller: 'ProfesseurCtrl'
+      })
       .otherwise({
         redirectTo: '/'
       });
