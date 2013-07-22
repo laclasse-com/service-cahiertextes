eea6de1195d86aa811f5753adb442b229c11b1c1
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-07-22 16:34:27 +0200

    install angular-ui-router

--- public-angular/app/scripts/app.js
+++ public-angular/app/scripts/app.js
@@ -4,6 +4,7 @@ angular.module('publicAngularApp', ['ui.bootstrap'])
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
+angular.module('publicAngularApp', ['ui.bootstrap', 'ui.state'])
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
