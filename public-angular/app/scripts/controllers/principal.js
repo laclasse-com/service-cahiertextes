cebf438574c9397682e93e4370944d97512bdcde
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-07-23 17:11:47 +0200

    mise en page

--- public-angular/app/scripts/controllers/principal.js
+++ public-angular/app/scripts/controllers/principal.js
@@ -7,29 +7,14 @@ angular.module('publicAngularApp')
 
 angular.module('publicAngularApp')
 	.controller('PrincipalClassesCtrl', function ($scope) {
-		$scope.classes = ['6ème A',
-						  '4ème O',
-						  '5ème L'];
+		$scope.classes = ['6ème A', '4ème O', '5ème L'];
 		$scope.classeCourante = $scope.classes[1];
-		$scope.mois = ['January',
-					   'February',
-					   'March',
-					   'April',
-					   'May',
-					   'June',
-					   'July',
-					   'August',
-					   'September',
-					   'October',
-					   'November',
-					   'December'];
+		
+		$scope.mois = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 		$scope.moisCourant = $scope.mois[1];
+		
 		$scope.matieres = ['Français','Mathématiques', 'Équilibrisme'];
-		$scope.matiereCourante = $scope.matieres[1];
-		$scope.chartData = [[
-			['6ème A', 12],['4ème O', 9], ['5ème L', 14], 
-			['Out of home', 16],['Commuting', 7], ['Orientation', 9]
-		]];
+		$scope.matiereCourante = $scope.matieres[1];		
 	});
 
 angular.module('publicAngularApp')
