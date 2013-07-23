ac4113a2ebd250fc4cf65eb8120139920d4e0756
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-07-23 12:30:58 +0200

    nettoyage des placeholders

--- public-angular/app/scripts/controllers/main.js
+++ public-angular/app/scripts/controllers/main.js
@@ -2,9 +2,5 @@
 
 angular.module('publicAngularApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	  
   });
--- public-angular/app/scripts/controllers/principal.js
+++ public-angular/app/scripts/controllers/principal.js
@@ -2,11 +2,7 @@
 
 angular.module('publicAngularApp')
   .controller('PrincipalCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	  
   });
 
 angular.module('publicAngularApp')
