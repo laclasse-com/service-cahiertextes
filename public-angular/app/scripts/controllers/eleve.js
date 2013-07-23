18bb2d6eef7953e63b0fb6e0a9b154b83ca2f125
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-07-23 12:30:58 +0200

    nettoyage des placeholders

--- public-angular/app/scripts/controllers/eleve.js
+++ public-angular/app/scripts/controllers/eleve.js
@@ -2,11 +2,7 @@
 
 angular.module('publicAngularApp')
   .controller('EleveCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+	  
   })
   .controller('EleveDisplayController', function ($scope) {
     $scope.partToShow = 'Cours';
