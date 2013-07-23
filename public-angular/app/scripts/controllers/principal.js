4db656a2840d06ff57b70b8f7b075657b109f5bf
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-07-23 15:53:58 +0200

    plus de données de remplissage

--- public-angular/app/scripts/controllers/principal.js
+++ public-angular/app/scripts/controllers/principal.js
@@ -6,13 +6,29 @@ angular.module('publicAngularApp')
 	});
 
 angular.module('publicAngularApp')
-  .controller('PrincipalClassesCtrl', function ($scope) {
-    $scope.classes = [
-      '3ème A',
-      '6ème ZX',
-      'Karma'
-    ];
-  });
+	.controller('PrincipalClassesCtrl', function ($scope) {
+		$scope.classes = ['6ème A',
+						  '4ème O',
+						  '5ème L'];
+		$scope.classeCourante = $scope.classes[1];
+		$scope.mois = ['January',
+					   'February',
+					   'March',
+					   'April',
+					   'May',
+					   'June',
+					   'July',
+					   'August',
+					   'September',
+					   'October',
+					   'November',
+					   'December'];
+		$scope.matieres = ['Français','Mathématiques', 'Équilibrisme'];
+		$scope.chartData = [[
+			['6ème A', 12],['4ème O', 9], ['5ème L', 14], 
+			['Out of home', 16],['Commuting', 7], ['Orientation', 9]
+		]];
+	});
 
 angular.module('publicAngularApp')
 	.controller('PrincipalEnseignantsCtrl', function ($scope) {
