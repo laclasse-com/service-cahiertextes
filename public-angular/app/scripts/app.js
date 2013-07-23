fc235d44db86ba580a3d55187a09e315fc18f88f
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-07-23 17:14:39 +0200

    remove ui-chart

--- public-angular/app/scripts/app.js
+++ public-angular/app/scripts/app.js
@@ -2,7 +2,6 @@
 
 angular.module('publicAngularApp', ['ui.bootstrap',
 									'ui.state',
-									'ui.chart',
 									'ui.select2'])
 	.config(function ($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise("/");
