3e984876824e78aab485f6c1019e8e4b312d9b9f
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-01 16:48:43 +0200

    indentation

--- public/app/scripts/services/EmploiDuTemps.js
+++ public/app/scripts/services/EmploiDuTemps.js
@@ -1,21 +1,21 @@
 'use strict';
 
 angular.module('publicAngularApp')
-  .service('EmploiDuTemps', function EmploiDuTemps($http) {
-			this.getMois = function() {
-					return $http.get( 'mocks/mois.json' );
-			};
-	  
-			this.getClasses = function() {
-					return $http.get( 'mocks/classes.json' );
-			};
-	  
-			this.getMatieres = function() {
-					return $http.get( 'mocks/matieres.json' );
-			};
-	  
-			this.getEnseignants = function() {
-					return $http.get( 'mocks/enseignants.json' );
-			};
-	  
-  });
+	.service('EmploiDuTemps', function EmploiDuTemps($http) {
+		this.getMois = function() {
+			return $http.get( 'mocks/mois.json' );
+		};
+		
+		this.getClasses = function() {
+			return $http.get( 'mocks/classes.json' );
+		};
+		
+		this.getMatieres = function() {
+			return $http.get( 'mocks/matieres.json' );
+		};
+		
+		this.getEnseignants = function() {
+			return $http.get( 'mocks/enseignants.json' );
+		};
+		
+	});
