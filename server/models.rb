52e7b96e2a51921318b3be2c28fc5bc0c2c91cb2
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-20 12:44:17 +0200

    renommé

--- server/models.rb
+++ server/models.rb
@@ -1,14 +1,14 @@
-#!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
-
 require 'sequel'
 require 'sequel/extensions/migration'
 
 #Connexion DB
 DB = Sequel.sqlite( './cahier_de_texte.sqlite3' )
 
+#application des migrations
 Sequel::Migrator.run( DB, "migrations" )
 
+#définition des modèles
 class TrancheHoraire < Sequel::Model(:tranche_horaire)
   plugin :json_serializer
 end
