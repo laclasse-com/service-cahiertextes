273452c2fc08a51d4bc1144cd189df5662da9436
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-21 16:52:28 +0200

    clarifie le nom du champs

--- server/migrations/005_create_cours.rb
+++ server/migrations/005_create_cours.rb
@@ -6,7 +6,7 @@ Sequel.migration do
       primary_key :id
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       foreign_key :cahier_de_textes_id, :cahier_de_textes
-      Integer :user_id  # enseignant
+      Integer :enseignant_id
       Date :date_cours
       DateTime :date_creation
       DateTime :date_modification
