6894db7acc5ff3e3d1f275292ee8b2484e3f7f8b
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-21 17:15:05 +0200

    unrestricted_primary_key permet d'assigner manuellement une valeur à la clef primaire.

--- server/migrations/003_create_creneau_emploi_du_temps.rb
+++ server/migrations/003_create_creneau_emploi_du_temps.rb
@@ -13,22 +13,22 @@ Sequel.migration do
     create_table(:creneau_emploi_du_temps_salle) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       foreign_key :salle_id, :salle
-      primary_key [:creneau_emploi_du_temps_id, :salle_id]
       Bignum :semaines_de_presence
+      unrestricted_primary_key [:creneau_emploi_du_temps_id, :salle_id]
     }
 
     create_table(:creneau_emploi_du_temps_enseignant) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :enseignant_id
-      primary_key [:creneau_emploi_du_temps_id, :enseignant_id]
       Bignum :semaines_de_presence
+      unrestricted_primary_key [:creneau_emploi_du_temps_id, :enseignant_id]
     }
 
     create_table(:creneau_emploi_du_temps_regroupement) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :regroupement_id
-      primary_key [:creneau_emploi_du_temps_id, :regroupement_id]
       Bignum :semaines_de_presence
+      unrestricted_primary_key [:creneau_emploi_du_temps_id, :regroupement_id]
     }
   end
 end
