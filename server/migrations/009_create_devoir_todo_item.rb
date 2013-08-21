62b10bc2cb679bd59557be97c87736267637bdbc
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-21 16:32:38 +0200

    ajout modèle pour gestion de todo-list

---
+++ server/migrations/009_create_devoir_todo_item.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+
+Sequel.migration do
+  change do
+    create_table(:devoir_todo_item) {
+      primary_key :id
+      foreign_key :devoir_id, :devoir
+      Integer :eleve_id
+      DateTime :date_fait
+    }
+  end
+end
