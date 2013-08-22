2794d2712bc46c9e45126d1d1e0cbb0c2bbc75fe
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-22 17:10:14 +0200

    mysql n'aimes pas Presence

--- migrations/003_create_creneau_emploi_du_temps.rb
+++ migrations/003_create_creneau_emploi_du_temps.rb
@@ -15,21 +15,21 @@ Sequel.migration do
     create_table(:creneau_emploi_du_temps_salle) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       foreign_key :salle_id, :salle
-      Presence :semaines_de_presence
+      Integer :semaines_de_presence
       primary_key [:creneau_emploi_du_temps_id, :salle_id]
     }
 
     create_table(:creneau_emploi_du_temps_enseignant) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :enseignant_id
-      Presence :semaines_de_presence
+      Integer :semaines_de_presence
       primary_key [:creneau_emploi_du_temps_id, :enseignant_id]
     }
 
     create_table(:creneau_emploi_du_temps_regroupement) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :regroupement_id
-      Presence :semaines_de_presence
+      Integer :semaines_de_presence
       primary_key [:creneau_emploi_du_temps_id, :regroupement_id]
     }
   end
