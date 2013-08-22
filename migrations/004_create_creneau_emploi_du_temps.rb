78b28e80535a76e5473b7e13efbb5499be7cbfe1
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-22 18:03:09 +0200

    type de donnée plus approprié pour MySQL

--- migrations/004_create_creneau_emploi_du_temps.rb
+++ migrations/004_create_creneau_emploi_du_temps.rb
@@ -15,21 +15,21 @@ Sequel.migration do
     create_table(:creneau_emploi_du_temps_salle) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       foreign_key :salle_id, :salle
-      Integer :semaines_de_presence
+      Bignum :semaines_de_presence, unsigned: true
       primary_key [:creneau_emploi_du_temps_id, :salle_id]
     }
 
     create_table(:creneau_emploi_du_temps_enseignant) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :enseignant_id
-      Integer :semaines_de_presence
+      Bignum :semaines_de_presence, unsigned: true
       primary_key [:creneau_emploi_du_temps_id, :enseignant_id]
     }
 
     create_table(:creneau_emploi_du_temps_regroupement) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :regroupement_id
-      Integer :semaines_de_presence
+      Bignum :semaines_de_presence, unsigned: true
       primary_key [:creneau_emploi_du_temps_id, :regroupement_id]
     }
   end
