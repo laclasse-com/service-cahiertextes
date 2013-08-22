d1864a93e0a1d73a4c91c835c6dda66e86047f15
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-22 18:41:30 +0200

    notes pour plus tard

--- migrations/007_create_cours.rb
+++ migrations/007_create_cours.rb
@@ -6,12 +6,12 @@ Sequel.migration do
       primary_key :id
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       foreign_key :cahier_de_textes_id, :cahier_de_textes
-      Integer :enseignant_id
+      Integer :enseignant_id    # tiré depuis l'annuaire en fonction de (etablissement, nom, prénom)
       Date :date_cours
       DateTime :date_creation
       DateTime :date_modification
       DateTime :date_validation
-      String :contenu
+      String :contenu           # séquence pédagogique
       TrueClass :deleted, default: false
     }
     create_join_table(cours_id: :cours, ressource_id: :ressource)
