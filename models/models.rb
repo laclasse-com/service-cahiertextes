90f8b515c1fd4b4ce8ac0e3692a7fa4e9a6d2a1c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-22 18:15:24 +0200

    gestion des semaines de présence dans les modèles

--- models/models.rb
+++ models/models.rb
@@ -25,11 +25,23 @@ class CreneauEmploiDuTemps < Sequel::Model(:creneau_emploi_du_temps)
   many_to_one :salle
   # FIXME: associations pour les plages horaires de début et de fin ?
 end
+
+module SemainesDePresenceMixin
+  def present_pour_la_semaine?( n )
+    semaines_de_presence[n] == 1
+  end
+end
+
 class CreneauEmploiDuTempsSalle < Sequel::Model(:creneau_emploi_du_temps_salle)
+  include SemainesDePresenceMixin
 end
+
 class CreneauEmploiDuTempsEnseignant < Sequel::Model(:creneau_emploi_du_temps_enseignant)
+  include SemainesDePresenceMixin
 end
+
 class CreneauEmploiDuTempsRegroupement < Sequel::Model(:creneau_emploi_du_temps_regroupement)
+  include SemainesDePresenceMixin
 end
 
 class Ressource < Sequel::Model(:ressource)
