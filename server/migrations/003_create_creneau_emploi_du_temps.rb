dcde7dc8247b0739e0d5e8cf66a63d511e6febed
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-22 15:16:14 +0200

    création d'un objet de manipulation de l'indicateur de présence de Pronote

--- server/migrations/003_create_creneau_emploi_du_temps.rb
+++ server/migrations/003_create_creneau_emploi_du_temps.rb
@@ -1,4 +1,5 @@
 # coding: utf-8
+require './database_types'
 
 Sequel.migration do
   change do
@@ -13,21 +14,21 @@ Sequel.migration do
     create_table(:creneau_emploi_du_temps_salle) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       foreign_key :salle_id, :salle
-      Integer :semaines_de_presence
+      Presence :semaines_de_presence
       unrestricted_primary_key [:creneau_emploi_du_temps_id, :salle_id]
     }
 
     create_table(:creneau_emploi_du_temps_enseignant) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :enseignant_id
-      Integer :semaines_de_presence
+      Presence :semaines_de_presence
       unrestricted_primary_key [:creneau_emploi_du_temps_id, :enseignant_id]
     }
 
     create_table(:creneau_emploi_du_temps_regroupement) {
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :regroupement_id
-      Integer :semaines_de_presence
+      Presence :semaines_de_presence
       unrestricted_primary_key [:creneau_emploi_du_temps_id, :regroupement_id]
     }
   end
