1b2863f1268d7b45822c5510eef29006d5621f52
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-27 10:48:45 +0200

    ordonnement des déclarations

--- migrations/001_creation_de_toutes_les_tables.rb
+++ migrations/001_creation_de_toutes_les_tables.rb
@@ -26,31 +26,31 @@ Sequel.migration do
 
     create_table!(:creneau_emploi_du_temps) {
       primary_key :id
-      Integer :jour_de_la_semaine
       foreign_key :debut, :plage_horaire
       foreign_key :fin, :plage_horaire
+      Integer :jour_de_la_semaine
       Integer :matiere_id, null: false # tiré de l'annuaire en fonction de (établissement, code, libellé)
     }
 
     create_table!(:creneau_emploi_du_temps_salle) {
+      primary_key [:creneau_emploi_du_temps_id, :salle_id]
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       foreign_key :salle_id, :salle
       Bignum :semaines_de_presence, unsigned: true
-      primary_key [:creneau_emploi_du_temps_id, :salle_id]
     }
 
     create_table!(:creneau_emploi_du_temps_enseignant) {
+      primary_key [:creneau_emploi_du_temps_id, :enseignant_id]
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :enseignant_id    # tiré depuis l'annuaire en fonction de (etablissement, nom, prénom)
       Bignum :semaines_de_presence, unsigned: true
-      primary_key [:creneau_emploi_du_temps_id, :enseignant_id]
     }
 
     create_table!(:creneau_emploi_du_temps_regroupement) {
+      primary_key [:creneau_emploi_du_temps_id, :regroupement_id]
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       Integer :regroupement_id    # tiré depuis l'annuaire en fonction de (établissement, nom)
       Bignum :semaines_de_presence, unsigned: true
-      primary_key [:creneau_emploi_du_temps_id, :regroupement_id]
     }
 
     create_table!(:ressource) {
