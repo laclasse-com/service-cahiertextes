ff2faa132435d1f1d0fa49bca96a0ab324bdef3b
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-28 12:53:27 +0200

    premières API réelles

--- api/principal.rb
+++ api/principal.rb
@@ -15,7 +15,25 @@ module CahierDeTextesAPI
       resource :classes do
         desc 'statistiques de toutes les classes'
         get do
-          PlageHoraire[label: "18"]  # == .filter().first
+          #TODO: get this from actual (Principal) user
+          regroupements_ids = [ 1,2,3,4,5,12 ]
+
+          regroupements_ids.map {
+            |regroupement_id|
+            CahierDeTextes[regroupement_id: regroupement_id].statistics
+          }.to_json
+        end
+      end
+
+      resource :classe do
+        desc 'statistiques d\'une classe'
+        params {
+          requires :id, type: Integer
+        }
+        get do
+          CahierDeTextes[ regroupement_id: params[:id] ] &&
+            CahierDeTextes[ regroupement_id: params[:id] ].statistics.to_json ||
+            error!( "Classe inconnue", 404 )
         end
       end
 
