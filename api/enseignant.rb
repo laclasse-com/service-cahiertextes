f9112c69408836dd03aab878f42710d0079f79a2
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-28 16:21:48 +0200

    plus d'API

--- api/enseignant.rb
+++ api/enseignant.rb
@@ -7,7 +7,32 @@ module CahierDeTextesAPI
     resource :enseignant do
 
       resource :emploi_du_temps do
-        # TODO
+        desc 'contenu combiné des cahiers de textes concernant l\'élève durant l\'intervalle de dates données ou par défaut le jour courant'
+        params {
+          optional :debut, type: Time
+          optional :fin, type: Time
+        }
+        get do
+          # TODO
+        end
+      end
+
+      resource :cahier_de_textes do
+        desc 'contenu combiné des cahiers de textes concernant l\'enseignant durant l\'intervalle de dates données ou par défaut le jour courant'
+        params {
+          optional :debut, type: Time
+          optional :fin, type: Time
+        }
+        get do
+          #TODO: get this from actual (Enseignant) user
+          regroupements_ids = [ 1, 2, 3, 4, 5, 12 ]
+
+          regroupements_ids.map {
+            |regroupement_id|
+            CahierDeTextes[ regroupement_id: regroupement_id ].content( params[:debut] ? params[:debut] : Time.now,
+                                                                        params[:fin] ? params[:fin] : Time.now )
+          }.to_json
+        end
       end
 
     end
