8cd99cf5cef1f8251e771c1fb48a46f5559092cc
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-29 18:02:30 +0200

    API cours pour enseignant (FIXME)

--- models/cours.rb
+++ models/cours.rb
@@ -4,6 +4,14 @@ class Cours < Sequel::Model( :cours )
   many_to_many :ressource
   many_to_one :creneau_emploi_du_temps
   many_to_one :cahier_de_textes
+
+  attr_accessor :enseignant_id
+  attr_accessor :date_cours
+  attr_accessor :date_creation
+  attr_accessor :date_modification
+  attr_accessor :date_validation
+  attr_accessor :contenu
+  attr_accessor :deleted
 end
 
 class CoursRessource < Sequel::Model( :cours_ressource )
