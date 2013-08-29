ff84d40b2bbc7b4dc7471f0206c1be940ba72796
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-29 15:51:33 +0200

    gestion des todo par Devoir

--- models/devoir.rb
+++ models/devoir.rb
@@ -2,10 +2,23 @@
 
 class Devoir < Sequel::Model( :devoir )
   many_to_many :ressource
+  one_to_many :devoir_todo_item
+
+  def fait_par( eleve_id )
+    add_devoir_todo_item( eleve_id: eleve_id, date_fait: Time.now )
+  end
+  def fait_par?( eleve_id )
+    # FIXME: peut sûrement mieux faire
+    devoir_todo_item_dataset.where(eleve_id: eleve_id).count > 0
+  end
+  def plus_fait_par( eleve_id )
+    # FIXME: peut sûrement mieux faire
+    devoir_todo_item_dataset.where(eleve_id: eleve_id).destroy
+  end
 end
 
 class TypeDevoir < Sequel::Model( :type_devoir )
-
+  one_to_many :devoir
 end
 
 class DevoirRessource < Sequel::Model( :devoir_ressource )
