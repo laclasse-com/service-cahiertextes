9b6e3c757e55ea4e25fcc969ac2b3cd8fdf1d7bb
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-29 16:55:43 +0200

    ajout des contraintes de non-nullité

--- migrations/001_creation_de_toutes_les_tables.rb
+++ migrations/001_creation_de_toutes_les_tables.rb
@@ -4,7 +4,7 @@ Sequel.migration do
   change do
     create_table!(:etablissement) {
       primary_key :id
-      String :UAI
+      String :UAI, null: false
       Date :debut_annee_scolaire
       Date :fin_annee_scolaire
       Date :date_premier_jour_premiere_semaine
@@ -12,15 +12,15 @@ Sequel.migration do
 
     create_table!(:plage_horaire) {
       primary_key :id
-      String :label
-      Time :debut, only_time: true
-      Time :fin, only_time: true
+      String :label, null: false
+      Time :debut, null: false, only_time: true
+      Time :fin, null: false, only_time: true
     }
 
     create_table!(:salle) {
       primary_key :id
       foreign_key :etablissement_id, :etablissement
-      String :identifiant
+      String :identifiant, null: false
       String :nom
     }
 
@@ -28,7 +28,7 @@ Sequel.migration do
       primary_key :id
       foreign_key :debut, :plage_horaire
       foreign_key :fin, :plage_horaire
-      Integer :jour_de_la_semaine
+      Integer :jour_de_la_semaine, null: false
       Integer :matiere_id, null: false # tiré de l'annuaire en fonction de (établissement, code, libellé)
     }
 
@@ -56,15 +56,15 @@ Sequel.migration do
     create_table!(:ressource) {
       primary_key :id
       String :label
-      Integer :doc_id  # TODO: remplacer par une URL?
+      Integer :url, null: false
     }
 
     create_table!(:cahier_de_textes) {
       primary_key :id
-      Integer :regroupement_id    # tiré depuis l'annuaire en fonction de (établissement, nom)
+      Integer :regroupement_id, null: false    # tiré depuis l'annuaire en fonction de (établissement, nom)
       Date :debut_annee_scolaire
       Date :fin_annee_scolaire
-      DateTime :date_creation
+      DateTime :date_creation, null: false
       String :label
       TrueClass :deleted, default: false
     }
@@ -73,9 +73,9 @@ Sequel.migration do
       primary_key :id
       foreign_key :creneau_emploi_du_temps_id, :creneau_emploi_du_temps
       foreign_key :cahier_de_textes_id, :cahier_de_textes
-      Integer :enseignant_id    # tiré depuis l'annuaire en fonction de (etablissement, nom, prénom)
-      Date :date_cours
-      DateTime :date_creation
+      Integer :enseignant_id, null: false    # tiré depuis l'annuaire en fonction de (etablissement, nom, prénom)
+      Date :date_cours, null: false
+      DateTime :date_creation, null: false
       DateTime :date_modification
       DateTime :date_validation
       String :contenu, size: 4096           # séquence pédagogique
@@ -86,7 +86,7 @@ Sequel.migration do
 
     create_table!(:type_devoir) {
       primary_key :id
-      String :label
+      String :label, null: false
       String :description
     }
 
@@ -94,11 +94,11 @@ Sequel.migration do
       primary_key :id
       foreign_key :cours_id, :cours
       foreign_key :type_devoir_id, :type_devoir
-      DateTime :date_creation
       String :contenu, size: 4096
+      DateTime :date_creation, null: false
       DateTime :date_modification
       DateTime :date_validation
-      Date :date_due
+      Date :date_due, null: false
       Integer :temps_estime
     }
     drop_table?(:devoir_ressource)
@@ -107,8 +107,8 @@ Sequel.migration do
     create_table!(:devoir_todo_item) {
       primary_key :id
       foreign_key :devoir_id, :devoir
-      Integer :eleve_id    # tiré depuis l'annuaire en fonction de (etablissement, nom, prénom, sexe, date_de_naissance)
-      DateTime :date_fait
+      Integer :eleve_id, null: false    # tiré depuis l'annuaire en fonction de (etablissement, nom, prénom, sexe, date_de_naissance)
+      DateTime :date_fait, null: false
     }
   end
 end
