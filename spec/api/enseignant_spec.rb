5cc218787bb6428d23212ba5f6a86827dc0ffc94
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-30 16:51:17 +0200

    fix spec

--- spec/api/enseignant_spec.rb
+++ spec/api/enseignant_spec.rb
@@ -22,26 +22,23 @@ describe CahierDeTextesAPI::API do
   it "posts a new séquence pédagogique" do
     cahier_de_textes_id = CahierDeTextes.all[ rand(0..CahierDeTextes.count-1) ][:id]
     creneau_emploi_du_temps_id = CreneauEmploiDuTemps.all[ rand(0..CreneauEmploiDuTemps.count-1) ][:id]
-    post "/enseignant/cours", { cahier_de_textes_id: cahier_de_textes_id, creneau_emploi_du_temps_id: creneau_emploi_du_temps_id, date_cours: "2013-08-29", contenu: "test de contenu\nallo"}.to_json, "CONTENT_TYPE" => "application/json"
+
+    post "/enseignant/cours", {
+      cahier_de_textes_id: cahier_de_textes_id,
+      creneau_emploi_du_temps_id: creneau_emploi_du_temps_id,
+      date_cours: "2013-08-29",
+      contenu: "test de <em>contenu</em>\nallo"}.to_json, "CONTENT_TYPE" => "application/json"
     last_response.status.should == 201
 
-    # last_response.body.should == { cahier_de_textes_id: cahier_de_textes_id,
-    #   creneau_emploi_du_temps_id: creneau_emploi_du_temps_id,
-    #   date_cours: "2013-08-29",
-    #   date_creation: nil,
-    #   date_modification: nil,
-    #   date_validation: nil,
-    #   contenu: "test de contenu\nallo",
-    #   deleted: false }.to_json
     response_body = JSON.parse(last_response.body)
     response_body["cahier_de_textes_id"].should ==  cahier_de_textes_id
     response_body["creneau_emploi_du_temps_id"].should == creneau_emploi_du_temps_id
-    response_body["onsedate_cours"].should == "2013-08-29"
-    response_body["onsedate_creation"].should == nil
-    response_body["onsedate_modification"].should == nil
-    response_body["onsedate_validation"].should == nil
-    response_body["onsecontenu"].should == "test de contenu\nallo"
-    response_body["onsedeleted"].should == false
+    response_body["date_cours"].should == "2013-08-29"
+    response_body["date_creation"].should == nil
+    response_body["date_modification"].should == nil
+    response_body["date_validation"].should == nil
+    response_body["contenu"].should == "test de <em>contenu</em>\nallo"
+    response_body["deleted"].should == false
   end
 
 end
