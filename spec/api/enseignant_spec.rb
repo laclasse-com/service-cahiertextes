723f5e36869e44827a5d1fb75ba7227bbfaf8616
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-30 17:11:35 +0200

    please rubocop

--- spec/api/enseignant_spec.rb
+++ spec/api/enseignant_spec.rb
@@ -9,8 +9,8 @@ describe CahierDeTextesAPI::API do
     db_cleaner = TableCleaner.new DB, []
     db_cleaner.clean
 
-    xml_filename = "spec/fixtures/Edt_To_LaclasseCom_0134567A_Enclair.xml"
-    post "/pronote/xml", :xml_file => Rack::Test::UploadedFile.new(xml_filename, 'text/xml')
+    xml_filename = 'spec/fixtures/Edt_To_LaclasseCom_0134567A_Enclair.xml'
+    post '/pronote/xml', xml_file: Rack::Test::UploadedFile.new(xml_filename, 'text/xml')
 
     require_relative '../fixtures/insertion-test-data.rb'
   end
@@ -19,26 +19,25 @@ describe CahierDeTextesAPI::API do
     CahierDeTextesAPI::API
   end
 
-  it "posts a new séquence pédagogique" do
+  it 'posts a new séquence pédagogique' do
     cahier_de_textes_id = CahierDeTextes.all[ rand(0..CahierDeTextes.count-1) ][:id]
     creneau_emploi_du_temps_id = CreneauEmploiDuTemps.all[ rand(0..CreneauEmploiDuTemps.count-1) ][:id]
 
-    post "/enseignant/cours", {
-      cahier_de_textes_id: cahier_de_textes_id,
+    post '/enseignant/cours', { cahier_de_textes_id: cahier_de_textes_id,
       creneau_emploi_du_temps_id: creneau_emploi_du_temps_id,
-      date_cours: "2013-08-29",
-      contenu: "test de <em>contenu</em>\nallo"}.to_json, "CONTENT_TYPE" => "application/json"
+      date_cours: '2013-08-29',
+      contenu: 'test de <em>contenu</em>\nallo'}.to_json, 'CONTENT_TYPE' => 'application/json'
     last_response.status.should == 201
 
     response_body = JSON.parse(last_response.body)
-    response_body["cahier_de_textes_id"].should ==  cahier_de_textes_id
-    response_body["creneau_emploi_du_temps_id"].should == creneau_emploi_du_temps_id
-    response_body["date_cours"].should == "2013-08-29"
-    response_body["date_creation"].should == nil
-    response_body["date_modification"].should == nil
-    response_body["date_validation"].should == nil
-    response_body["contenu"].should == "test de <em>contenu</em>\nallo"
-    response_body["deleted"].should == false
+    response_body['cahier_de_textes_id'].should        == cahier_de_textes_id
+    response_body['creneau_emploi_du_temps_id'].should == creneau_emploi_du_temps_id
+    response_body['date_cours'].should                 == '2013-08-29'
+    response_body['date_creation'].should              == nil
+    response_body['date_modification'].should          == nil
+    response_body['date_validation'].should            == nil
+    response_body['contenu'].should                    == 'test de <em>contenu</em>\nallo'
+    response_body['deleted'].should                    == false
   end
 
 end
--- spec/api/upload_file_spec.rb
+++ spec/api/upload_file_spec.rb
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 require 'spec_helper'
 
 describe CahierDeTextesAPI::API do
@@ -7,21 +9,22 @@ describe CahierDeTextesAPI::API do
     db_cleaner = TableCleaner.new DB, []
     db_cleaner.clean
   end
-  
+
   def app
     CahierDeTextesAPI::API
   end
 
-  it "uploads a file" do
-    xml_filename = "spec/fixtures/Edt_To_LaclasseCom_0134567A_Enclair.xml"
-    post "/pronote/xml", :xml_file => Rack::Test::UploadedFile.new(xml_filename, 'text/xml')
+  it 'uploads a file' do
+    xml_filename = 'spec/fixtures/Edt_To_LaclasseCom_0134567A_Enclair.xml'
+    post '/pronote/xml', xml_file: Rack::Test::UploadedFile.new(xml_filename, 'text/xml')
     last_response.status.should == 201
+
     last_response.body.should == {
-      "filename" => "Edt_To_LaclasseCom_0134567A_Enclair.xml",
-      "size" => File.size(xml_filename),
-      "nb_salles" => 24,
-      "nb_plages_horaires" => 20,
-      "nb_creneau_emploi_du_temps" => 701,
+      'filename' => 'Edt_To_LaclasseCom_0134567A_Enclair.xml',
+      'size' => File.size(xml_filename),
+      'nb_salles' => 24,
+      'nb_plages_horaires' => 20,
+      'nb_creneau_emploi_du_temps' => 701,
     }.to_json
   end
 
