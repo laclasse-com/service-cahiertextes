22c5570457d127a7dbb0118dbf20008763e2a8cc
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-30 12:59:24 +0200

    API création cours

--- spec/api/enseignant_spec.rb
+++ spec/api/enseignant_spec.rb
@@ -4,22 +4,44 @@ require 'spec_helper'
 
 describe CahierDeTextesAPI::API do
   include Rack::Test::Methods
-  
+
+  before :each do
+    db_cleaner = TableCleaner.new DB, []
+    db_cleaner.clean
+
+    xml_filename = "spec/fixtures/Edt_To_LaclasseCom_0134567A_Enclair.xml"
+    post "/pronote/xml", :xml_file => Rack::Test::UploadedFile.new(xml_filename, 'text/xml')
+
+    require_relative '../fixtures/insertion-test-data.rb'
+  end
+
   def app
     CahierDeTextesAPI::API
   end
 
   it "posts a new séquence pédagogique" do
-    post "/enseignant/cours", cahier_de_textes_id: 1, creneau_emploi_du_temps_id: 2, date_cours: "2013-08-29 17:24:54 +0200", contenu: "test de contenu\nallo"
+    cahier_de_textes_id = CahierDeTextes.all[ rand(0..CahierDeTextes.count-1) ][:id]
+    creneau_emploi_du_temps_id = CreneauEmploiDuTemps.all[ rand(0..CreneauEmploiDuTemps.count-1) ][:id]
+    post "/enseignant/cours", { cahier_de_textes_id: cahier_de_textes_id, creneau_emploi_du_temps_id: creneau_emploi_du_temps_id, date_cours: "2013-08-29", contenu: "test de contenu\nallo"}.to_json, "CONTENT_TYPE" => "application/json"
     last_response.status.should == 201
-    # last_response.body.should == {
-    #   "filename" => "Edt_To_LaclasseCom_0134567A_Enclair.xml",
-    #   "size" => File.size(xml_filename),
-    #   "nb_salles" => 24,
-    #   "nb_plages_horaires" => 20,
-    #   "nb_creneau_emploi_du_temps" => 701,
-    # }.to_json
+
+    # last_response.body.should == { cahier_de_textes_id: cahier_de_textes_id,
+    #   creneau_emploi_du_temps_id: creneau_emploi_du_temps_id,
+    #   date_cours: "2013-08-29",
+    #   date_creation: nil,
+    #   date_modification: nil,
+    #   date_validation: nil,
+    #   contenu: "test de contenu\nallo",
+    #   deleted: false }.to_json
+    response_body = JSON.parse(last_response.body)
+    response_body["cahier_de_textes_id"].should ==  cahier_de_textes_id
+    response_body["creneau_emploi_du_temps_id"].should == creneau_emploi_du_temps_id
+    response_body["onsedate_cours"].should == "2013-08-29"
+    response_body["onsedate_creation"].should == nil
+    response_body["onsedate_modification"].should == nil
+    response_body["onsedate_validation"].should == nil
+    response_body["onsecontenu"].should == "test de contenu\nallo"
+    response_body["onsedeleted"].should == false
   end
 
 end
-
