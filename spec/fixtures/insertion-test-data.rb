ac8ec6d199f5867d12b8aa6b3e34fb639b36ff13
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-08-30 12:59:37 +0200

    automatisation

--- spec/fixtures/insertion-test-data.rb
+++ spec/fixtures/insertion-test-data.rb
@@ -15,20 +15,33 @@ types_devoir.insert(label: 'Recherche', description: 'Recherche à faire')
 types_devoir.insert(label: 'Exercice', description: 'Exercice à faire')
 
 cahier_de_textes = DB[:cahier_de_textes]
-cahier_de_textes.insert(regroupement_id: 1, debut_annee_scolaire: Time.now, fin_annee_scolaire: Time.now, date_creation: Time.now, label: 'Oh le joli cahier de textes!')
-cahier_de_textes.insert(regroupement_id: 2, debut_annee_scolaire: Time.now, fin_annee_scolaire: Time.now, date_creation: Time.now, label: 'Oh le joli cahier de textes!')
-cahier_de_textes.insert(regroupement_id: 3, debut_annee_scolaire: Time.now, fin_annee_scolaire: Time.now, date_creation: Time.now, label: 'Oh le joli cahier de textes!')
+5.times { |i|
+  cahier_de_textes.insert(regroupement_id: i,
+                          debut_annee_scolaire: Time.now,
+                          fin_annee_scolaire: Time.now,
+                          date_creation: Time.now,
+                          label: 'Oh le joli cahier de textes!')
+}
 
 ressources = DB[:ressource]
-ressources.insert(label: 'test 1', doc_id: 983456)
-ressources.insert(label: 'test 2', doc_id: 56324)
-ressources.insert(label: 'test 3', doc_id: 983)
-ressources.insert(label: 'test 4', doc_id: 67265)
+9.times { |i|
+  ressources.insert(label: 'test #{i}',
+                    url: "http://bla/#{i}")
+}
 
 cours = DB[:cours]
-cours.insert(cahier_de_textes_id: 1, creneau_emploi_du_temps_id: 1, enseignant_id: 2, date_cours: Time.now, contenu: '<em>Alors là</em><br>C\'était absolument fascinant' )
-cours.insert(cahier_de_textes_id: 2, creneau_emploi_du_temps_id: 11, enseignant_id: 2, date_cours: Time.now, contenu: '<em>Alors là</em><br>C\'était absolument fascinant' )
-cours.insert(cahier_de_textes_id: 3, creneau_emploi_du_temps_id: 6, enseignant_id: 2, date_cours: Time.now, contenu: '<em>Alors là</em><br>C\'était absolument fascinant' )
+13.times { |i|
+  cours.insert(cahier_de_textes_id: CahierDeTextes.all[ rand(0..CahierDeTextes.count-1) ][:id],
+               creneau_emploi_du_temps_id: CreneauEmploiDuTemps.all[ rand(0..CreneauEmploiDuTemps.count-1) ][:id],
+               enseignant_id: 2,
+               date_cours: Time.now,
+               contenu: '<em>Alors là</em><br>C\'était absolument fascinant')
+}
 
 devoirs = DB[:devoir]
-devoirs.insert(cours_id: 1, type_devoir_id: 3, contenu: 'faire le poirier 10 minutes', date_creation: Time.now)
+8.times { |i|
+  devoirs.insert(cours_id: Cours.all[ rand(0..Cours.count-1) ][:id],
+                 type_devoir_id: TypeDevoir.all[ rand(0..TypeDevoir.count-1) ][:id],
+                 contenu: 'faire le poirier 10 minutes',
+                 date_creation: Time.now)
+}
