1ba94c30e3c0cefca7ecb04105435829abd3d061
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-02 15:23:14 +0200

    test tout

--- spec/api/enseignant_spec.rb
+++ spec/api/enseignant_spec.rb
@@ -5,10 +5,11 @@ require 'spec_helper'
 describe CahierDeTextesAPI::API do
   include Rack::Test::Methods
 
-  before :each do
+  before :all do
     db_cleaner = TableCleaner.new DB, []
     db_cleaner.clean
 
+    # FIXME: something faster
     xml_filename = 'spec/fixtures/Edt_To_LaclasseCom_0134567A_Enclair.xml'
     post '/pronote/xml', xml_file: Rack::Test::UploadedFile.new(xml_filename, 'text/xml')
 
@@ -19,25 +20,79 @@ describe CahierDeTextesAPI::API do
     CahierDeTextesAPI::API
   end
 
-  it 'posts a new séquence pédagogique' do
-    cahier_de_textes_id = CahierDeTextes.all[ rand(0..CahierDeTextes.count-1) ][:id]
-    creneau_emploi_du_temps_id = CreneauEmploiDuTemps.all[ rand(0..CreneauEmploiDuTemps.count-1) ][:id]
+  it 'creates a new cours' do
+    cahier_de_textes_id = CahierDeTextes.all[ rand(0 .. CahierDeTextes.count - 1) ][:id]
+    creneau_emploi_du_temps_id = CreneauEmploiDuTemps.all[ rand(0 .. CreneauEmploiDuTemps.count - 1) ][:id]
 
-    post '/enseignant/cours', { cahier_de_textes_id: cahier_de_textes_id,
-      creneau_emploi_du_temps_id: creneau_emploi_du_temps_id,
-      date_cours: '2013-08-29',
-      contenu: 'test de <em>contenu</em>\nallo'}.to_json, 'CONTENT_TYPE' => 'application/json'
+    post( '/enseignant/cours',
+          { cahier_de_textes_id: cahier_de_textes_id,
+            creneau_emploi_du_temps_id: creneau_emploi_du_temps_id,
+            date_cours: '2013-08-29',
+            contenu: 'Exemple de séquence pédagogique.'}.to_json,
+          'CONTENT_TYPE' => 'application/json' )
     last_response.status.should == 201
 
     response_body = JSON.parse(last_response.body)
-    response_body['cahier_de_textes_id'].should        == cahier_de_textes_id
+
+    response_body['cahier_de_textes_id'].should == cahier_de_textes_id
     response_body['creneau_emploi_du_temps_id'].should == creneau_emploi_du_temps_id
-    response_body['date_cours'].should                 == '2013-08-29'
-    response_body['date_creation'].should              == nil
-    response_body['date_modification'].should          == nil
-    response_body['date_validation'].should            == nil
-    response_body['contenu'].should                    == 'test de <em>contenu</em>\nallo'
-    response_body['deleted'].should                    == false
+    response_body['date_cours'].should == '2013-08-29'
+    response_body['date_creation'].should_not equal nil
+    response_body['date_modification'].should equal nil
+    response_body['date_validation'].should equal nil
+    response_body['contenu'].should == 'Exemple de séquence pédagogique.'
+    response_body['deleted'].should equal false
   end
 
+  it 'updates a cours' do
+    cours = Cours.last
+
+    post( "/enseignant/cours/#{cours.id}",
+          { contenu: 'Mise à jour de la séquence pédagogique.'}.to_json,
+          'CONTENT_TYPE' => 'application/json' )
+    last_response.status.should == 201
+
+    response_body = JSON.parse(last_response.body)
+
+    response_body['cahier_de_textes_id'].should == cours.cahier_de_textes_id
+    response_body['creneau_emploi_du_temps_id'].should == cours.creneau_emploi_du_temps_id
+    response_body['date_cours'].should == cours.date_cours.to_s
+    response_body['date_creation'].should == cours.date_creation ? cours.date_creation.to_s : nil
+    response_body['date_modification'].should == cours.date_creation ? cours.date_creation.to_s : nil
+    response_body['date_validation'].should == cours.date_validation ? cours.date_validation.to_s : nil
+    response_body['contenu'].should == 'Mise à jour de la séquence pédagogique.'
+    response_body['deleted'].should == cours.deleted
+  end
+
+  it 'gets the details of a cours' do
+    cours = Cours.last
+
+    get "/enseignant/cours/#{cours.id}"
+    last_response.status.should == 200
+
+    response_body = JSON.parse(last_response.body)
+
+    response_body['cahier_de_textes_id'].should == cours.cahier_de_textes_id
+    response_body['creneau_emploi_du_temps_id'].should == cours.creneau_emploi_du_temps_id
+    response_body['date_cours'].should == cours.date_cours.to_s
+    # response_body['date_creation'].should == cours.date_creation ? cours.date_creation.to_s : nil
+    # response_body['date_modification'].should == cours.date_creation ? cours.date_creation.to_s : nil
+    # response_body['date_validation'].should == cours.date_validation ? cours.date_validation.to_s : nil
+    response_body['date_creation'].should_not equal nil
+    response_body['contenu'].should == cours.contenu
+    response_body['deleted'].should == cours.deleted
+  end
+
+  it 'deletes a cours' do
+    cours = Cours.last
+    cours.deleted.should == false
+
+    delete "/enseignant/cours/#{cours.id}"
+
+    cours = Cours.last
+    cours.deleted.should == true
+
+    get "/enseignant/cours/#{cours.id}"
+    last_response.status.should == 404
+  end
 end
