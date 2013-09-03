ad7dc64b92e2738c7fdb40b0baf6cf8500cf31df
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-03 17:34:43 +0200

    premier jet de l'API élève

--- api/eleve.rb
+++ api/eleve.rb
@@ -6,6 +6,20 @@ module CahierDeTextesAPI
 
     resource :eleve do
 
+      resource :emploi_du_temps do
+
+        desc 'emploi du temps de l\'élève durant l\'intervalle de dates données ou par défaut le jour courant'
+        params {
+          optional :debut, type: Time
+          optional :fin, type: Time
+        }
+        get do
+          # TODO
+          {}
+        end
+
+      end
+
       resource :cahier_de_textes do
         desc 'contenu combiné des cahiers de textes concernant l\'élève durant l\'intervalle de dates données ou par défaut le jour courant'
         params {
@@ -13,27 +27,71 @@ module CahierDeTextesAPI
           optional :fin, type: Time
         }
         get do
-          #TODO: get this from actual (Élève) user
+          # TODO: get this from actual (Élève) user
           regroupements_ids = [ 1, 2, 3, 4, 5, 12 ]
 
           regroupements_ids.map {
             |regroupement_id|
-            CahierDeTextes[ regroupement_id: regroupement_id ].content( params[:debut] ? params[:debut] : Time.now,
-                                                                        params[:fin] ? params[:fin] : Time.now )
+            cdt = CahierDeTextes[ regroupement_id: regroupement_id ]
+            unless cdt.nil?
+              CahierDeTextes[ regroupement_id: regroupement_id ].content( params[:debut] ? params[:debut] : Time.now,
+                                                                          params[:fin] ? params[:fin] : Time.now )
+            end
           }.to_json
         end
       end
 
+      resource :cours do
+
+        # FIXME: copypasta de l'api enseignant
+        desc 'renvoi une séquence pédagogique'
+        get '/:id' do
+          # FIXME: gestion des droits
+          if Cours[ params[:id] ].nil? || Cours[ params[:id] ].deleted
+            error!( 'Cours inconnu', 404 )
+          else
+            cours = Cours[ params[:id] ]
+            unless cours.nil?
+              hash = cours.to_hash
+              hash[:ressources] = cours.ressources
+
+              hash
+            end
+          end
+        end
+
+      end
+
       resource :devoir do
+
+        # FIXME: copypasta de l'api enseignant
+
+        desc 'renvoi le détail d\'un devoir'
+        params {
+          requires :id
+        }
+        get '/:id' do
+          devoir = Devoir[ params[:id] ]
+          if devoir.nil?
+            error!( 'Devoir inconnu', 404 )
+          else
+            hash = devoir.to_hash
+            hash[:ressources] = devoir.ressources
+
+            hash
+          end
+        end
+
         desc 'permet de noter un devoir comme fait/à faire'
         params {
           requires :id
         }
         put '/:id' do
           # FIXME: get real (Élève) user
-          user_id = 1
+          eleve_id = 1
 
-          Devoir[:id].fait_par?( user_id ) && Devoir[:id].plus_fait_par( user_id ) || Devoir[:id].fait_par( user_id )
+          # Devoir[:id].fait_par?( user_id ) && Devoir[:id].plus_fait_par( user_id ) ||
+          Devoir[:id].fait_par( eleve_id )
         end
       end
 
---
+++ spec/api/eleve_spec.rb
@@ -0,0 +1,107 @@
+# -*- coding: utf-8 -*-
+
+require 'spec_helper'
+
+describe CahierDeTextesAPI::API do
+  include Rack::Test::Methods
+
+  before :all do
+    TableCleaner.new( DB, [] ).clean
+
+    cahier_de_textes = CahierDeTextes.create(regroupement_id: 1,
+                          date_creation: Time.now,
+                          deleted: false)
+    plage_horaire_debut = PlageHoraire.create(label: 'test_debut',
+                                              debut: '08:30:00',
+                                              fin: '09:00:00')
+    plage_horaire_fin = PlageHoraire.create(label: 'test_fin',
+                                            debut: '09:30:00',
+                                            fin: '10:00:00')
+    creneau_emploi_du_temps = CreneauEmploiDuTemps.create(debut: plage_horaire_debut.id,
+                                fin: plage_horaire_fin.id)
+    type_devoir = TypeDevoir.create(label: 'RSpec',
+                      description: 'Type de devoir tout spécial pour rspec')
+
+    cours = Cours.create(cahier_de_textes_id: cahier_de_textes.id,
+                 creneau_emploi_du_temps_id: creneau_emploi_du_temps.id,
+                 date_cours: '2013-08-29',
+                 contenu: 'Exemple de séquence pédagogique.' )
+    Devoir.create(cours_id: cours.id,
+                  type_devoir_id: type_devoir.id,
+                  date_due: Time.now,
+                  contenu: 'Exemple de devoir.',
+                  temps_estime: rand(0..120) )
+  end
+
+  def app
+    CahierDeTextesAPI::API
+  end
+
+  # {{{ Emploi du Temps
+  ############ GET ############
+  it 'récupère l\'emploi du temps de l\'élève' do
+
+    get '/eleve/emploi_du_temps/'
+    last_response.status.should == 200
+  end
+  # }}}
+
+  # {{{ Cahier de Textes
+  ############ GET ############
+  it 'récupère le cahier de textes de l\'élève' do
+
+    get '/eleve/cahier_de_textes/'
+    last_response.status.should == 200
+  end
+  # }}}
+
+  # {{{ Cours
+  ############ GET ############
+  it 'récupère le détail d\'une séquence pédagogique' do
+    cours = Cours.last
+
+    get "/eleve/cours/#{cours.id}"
+    last_response.status.should == 200
+
+    response_body = JSON.parse(last_response.body)
+
+    response_body['cahier_de_textes_id'].should == cours.cahier_de_textes_id
+    response_body['creneau_emploi_du_temps_id'].should == cours.creneau_emploi_du_temps_id
+    response_body['date_cours'].should == cours.date_cours.to_s
+    expect( Date.parse( response_body['date_creation'] ) ).to eq Date.parse( cours.date_creation.to_s ) unless cours.date_creation.nil?
+    expect( Date.parse( response_body['date_modification'] ) ).to eq Date.parse( cours.date_modification.to_s ) unless cours.date_modification.nil?
+    expect( Date.parse( response_body['date_validation'] ) ).to eq Date.parse( cours.date_validation.to_s ) unless cours.date_validation.nil?
+    response_body['contenu'].should == cours.contenu
+    response_body['deleted'].should be_false
+    response_body['ressources'].size.should == cours.ressources.size
+  end
+  # }}}
+
+  # {{{ Devoir
+  ############ GET ############
+  it 'récupère les détails d\'un devoir' do
+    eleve_id = 1
+    devoir = Devoir.all[ rand(0 .. Devoir.count - 1) ]
+
+    get "/eleve/devoir/#{devoir.id}"
+    last_response.status.should == 200
+
+    response_body = JSON.parse( last_response.body )
+
+    response_body['cours_id'].should == devoir.cours_id
+    response_body['type_devoir_id'].should == devoir.type_devoir_id
+    response_body['contenu'].should == devoir.contenu
+    # response_body['fait'].should == devoir.fait_par?( eleve_id )
+  end
+
+  ############ PUT ############
+  it 'note un devoir comme fait' do
+    devoir = Devoir.all[ rand(0 .. Devoir.count - 1) ]
+
+    put "/eleve/devoir/#{devoir.id}", {}
+    last_response.status.should == 200
+
+    devoir.fait_par?( 1 ).should be_true
+  end
+  # }}}
+end
