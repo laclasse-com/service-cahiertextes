2fbe74f9229a6fe13f189d47c4325050d56d7e78
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-03 16:21:50 +0200

    méthode manquante

--- models/cahier_de_textes.rb
+++ models/cahier_de_textes.rb
@@ -2,6 +2,12 @@
 
 class CahierDeTextes < Sequel::Model( :cahiers_de_textes )
   def statistics
+    # TODO: return statistics about this Cahier de textes
     { name: '6⁰Q' }
+  end
+
+  def content( debut, fin )
+    # TODO: return the content of this Cahier de textes during the given dates interval
+    {}
   end
 end
