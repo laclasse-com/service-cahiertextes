0e23ef9eef41a157363ed9d14c2d0a715d9f3727
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-03 17:27:59 +0200

    pluriel

--- models/devoir.rb
+++ models/devoir.rb
@@ -6,15 +6,15 @@ class Devoir < Sequel::Model( :devoirs )
   many_to_one :type_devoir
 
   def fait_par( eleve_id )
-    add_devoir_todo_item( eleve_id: eleve_id, date_fait: Time.now )
+    add_devoir_todo_items( eleve_id: eleve_id, date_fait: Time.now )
   end
   def fait_par?( eleve_id )
     # FIXME: peut sûrement mieux faire
-    devoir_todo_item_dataset.where(eleve_id: eleve_id).count > 0
+    devoir_todo_items_dataset.where(eleve_id: eleve_id).count > 0
   end
   def plus_fait_par( eleve_id )
     # FIXME: peut sûrement mieux faire
-    devoir_todo_item_dataset.where(eleve_id: eleve_id).destroy
+    devoir_todo_items_dataset.where(eleve_id: eleve_id).destroy
   end
 end
 
