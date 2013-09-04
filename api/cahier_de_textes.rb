c201cef3cd203e62e5739805ee02b0dac06a8d62
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-04 15:03:46 +0200

    assemblage de l'API dans app.rb plutôt que dans la définition de l'API elle-même

--- api/cahier_de_textes.rb
+++ api/cahier_de_textes.rb
@@ -3,25 +3,23 @@
 module CahierDeTextesAPI
   class CahierDeTextesAPI < Grape::API
 
-    resource :cahier_de_textes do
-      desc 'contenu combiné des cahiers de textes concernant l\'utilisateur durant l\'intervalle de dates donné'
-      params {
-        optional :debut, type: Time
-        optional :fin, type: Time
-      }
-      get do
-        # TODO: get this from actual (Élève) user
-        regroupements_ids = [ 1, 2, 3, 4, 5, 12 ]
+    desc 'contenu combiné des cahiers de textes concernant l\'utilisateur durant l\'intervalle de dates donné'
+    params {
+      optional :debut, type: Time
+      optional :fin, type: Time
+    }
+    get do
+      # TODO: get this from actual (Élève) user
+      regroupements_ids = [ 1, 2, 3, 4, 5, 12 ]
 
-        regroupements_ids.map {
-          |regroupement_id|
-          cdt = CahierDeTextes[ regroupement_id: regroupement_id ]
-          unless cdt.nil?
-            CahierDeTextes[ regroupement_id: regroupement_id ].content( params[:debut] ? params[:debut] : Time.now,
-                                                                        params[:fin] ? params[:fin] : Time.now )
-          end
-        }.to_json
-      end
+      regroupements_ids.map {
+        |regroupement_id|
+        cdt = CahierDeTextes[ regroupement_id: regroupement_id ]
+        unless cdt.nil?
+          CahierDeTextes[ regroupement_id: regroupement_id ].content( params[:debut] ? params[:debut] : Time.now,
+                                                                      params[:fin] ? params[:fin] : Time.now )
+        end
+      }.to_json
     end
 
   end
