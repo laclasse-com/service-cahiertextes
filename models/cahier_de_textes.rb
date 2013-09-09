9dbf22f820d85dfe163f4cfa7dc5b1e5ca8b416e
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-09 16:12:50 +0200

    nom des méthodes francisés

--- models/cahier_de_textes.rb
+++ models/cahier_de_textes.rb
@@ -1,12 +1,26 @@
 # -*- coding: utf-8 -*-
 
 class CahierDeTextes < Sequel::Model( :cahiers_de_textes )
-  def statistics
-    # TODO: return statistics about this Cahier de textes
-    { name: '6⁰Q' }
+  def statistiques
+    cours = Cours.where(cahier_de_textes_id: id).where(:date_cours)
+    {
+      regroupement_id: regroupement_id,
+      filled: cours.count,
+      validated: cours.where( :date_validation ).count,
+      par_mois: (1..12).map do
+        |month|
+
+        tmp_cours = cours.where( 'extract( month from date_cours ) = ' + month.to_s )
+        {
+          filled: tmp_cours.count,
+          validated: tmp_cours.where( :date_validation ).count
+            # matiere_id: CreneauEmploiDuTemps[ cours.creneau_emploi_du_temps_id ].matiere_id,
+        }
+      end
+    }
   end
 
-  def content( debut, fin )
+  def contenu( debut, fin )
     # TODO: return the content of this Cahier de textes during the given dates interval
     {}
   end
