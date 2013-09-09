478bd0aa2933af0ae32caeb2092fb172669977d8
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-09 17:08:41 +0200

    API /classe/:id plus bavarde

--- models/cahier_de_textes.rb
+++ models/cahier_de_textes.rb
@@ -7,14 +7,31 @@ class CahierDeTextes < Sequel::Model( :cahiers_de_textes )
       regroupement_id: regroupement_id,
       filled: cours.count,
       validated: cours.where( :date_validation ).count,
-      par_mois: (1..12).map do
-        |month|
+      par_matiere:
+      cours.join(:creneaux_emploi_du_temps, id: :creneau_emploi_du_temps_id).select(:matiere_id).group_by(:matiere_id).map do
+        |record|
+        {
+          matiere_id: record.values[:matiere_id],
+          par_mois:
+          (1..12).map do
+            |month|
+            tmp_cours = cours.join(:creneaux_emploi_du_temps, id: :creneau_emploi_du_temps_id).where(matiere_id: record.values[:matiere_id]).where( 'extract( month from date_cours ) = ' + month.to_s )
 
+            {
+              filled: tmp_cours.count,
+              validated: tmp_cours.where( :date_validation ).count
+            }
+          end
+        }
+      end,
+      par_mois:
+      (1..12).map do
+        |month|
         tmp_cours = cours.where( 'extract( month from date_cours ) = ' + month.to_s )
+
         {
           filled: tmp_cours.count,
           validated: tmp_cours.where( :date_validation ).count
-            # matiere_id: CreneauEmploiDuTemps[ cours.creneau_emploi_du_temps_id ].matiere_id,
         }
       end
     }
