0fd1c811863b4bab4614ef49904c41761445b134
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-10 16:29:52 +0200

    un peu de code illustratif pour pas tout oublier

--- lib/annuaire.rb
+++ lib/annuaire.rb
@@ -1,12 +1,24 @@
 # -*- coding: utf-8 -*-
 
-require 'rest-client'
+require 'rest-client'           # https://github.com/rest-client/rest-client
 
 # Module d'interfaçage avec l'annuaire
 module Annuaire
   module_function
 
+  SERVICE_ANNUAIRE_USER = 'http://www.dev.laclasse.com/api'
+
   def get_matiere_id( code_uai, label )
+    RestClient.get( SERVICE_ANNUAIRE_USER + '/matieres',
+                    { accept: 'json', params: { uai: code_uai, libelle: label } } ) do
+      |response, request, result|
+      if response.code == 200
+        # TODO: use response
+      else
+        STDERR.puts response.code.to_s
+      end
+    end
+
     rand(100_000..999_999)
   end
 
