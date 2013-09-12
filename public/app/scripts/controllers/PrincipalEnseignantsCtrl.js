c64306058702c9ca65241751d66e7432ba2c39e4
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-12 18:07:33 +0200

    consomme les données enseignants de l'API (+ indentation (!))

--- public/app/scripts/controllers/PrincipalEnseignantsCtrl.js
+++ public/app/scripts/controllers/PrincipalEnseignantsCtrl.js
@@ -1,55 +1,62 @@
 'use strict';
 
 angular.module('cahierDeTexteApp')
-.controller('PrincipalEnseignantsCtrl', function ($scope, $http, EmploiDuTemps) {
-  $scope.enseignants = [];
-  $scope.annee = [];
+    .controller('PrincipalEnseignantsCtrl', function ($scope, $http, EmploiDuTemps) {
+	$scope.enseignants = [];
+	$scope.annee = [];
 
-  $scope.radar = {
-    data: [],
-    renderer: RadarChart.draw
-  };
-  $scope.gridEnseignants = {
-    data: 'enseignants',
-    enableCellEdit: true,
-    plugins: [new ngGridFlexibleHeightPlugin()],
-    columnDefs: [
-      { field: 'name', displayName: 'Nom', cellTemplate: '<div><a href="#/principal/enseignant/{{row.getProperty(\'id\')}}">{{row.entity[col.field]}}</a></div>' },
-      { field: 'discipline', displayName: 'Discipline' },
-      { field: 'stats', displayName: 'Statistiques' }]
-  };
+	$scope.radar = {
+	    data: [],
+	    renderer: RadarChart.draw
+	};
+	$scope.gridEnseignants = {
+	    data: 'enseignants',
+	    enableCellEdit: true,
+	    plugins: [new ngGridFlexibleHeightPlugin()],
+	    columnDefs: [
+		{ field: 'name', displayName: 'Nom', cellTemplate: '<div><a href="#/principal/enseignant/{{row.getProperty(\'id\')}}">{{row.entity[col.field]}}</a></div>' },
+		{ field: 'discipline', displayName: 'Discipline' },
+		{ field: 'stats', displayName: 'Statistiques' }]
+	};
 
-  $http({
-    method: 'GET',
-    url: 'http://localhost:9292/api/v0/etablissement/0134567A/enseignant'
-  }).
-    success( function( response ) {
-               $scope.enseignants = response;
-             }).
-    error( function (data, status) {
-             if (status === 404) {
-               $scope.error = 'it does not exist';
-             } else {
-               $scope.error = 'Error: ' + status + '\nData: ' + data;
-             }
-           });
-  // EmploiDuTemps.getEnseignants().success(
-  // 	function( response ) {
-  // 	    $scope.enseignants = response;
-  // 	    $scope.radar.data.push( $scope.enseignants.map( function( e ) {
-  // 		return { axis: e.name, value: e.total };
-  // 	    } ) );
-  // 	    $scope.radar.data.push( $scope.enseignants.map( function( e ) {
-  // 		return { axis: e.name, value: e.stats };
-  // 	    } ) );
-  // 	});
-  // $scope.enseignant = $scope.enseignants[0];
+	$http({
+	    method: 'GET',
+	    url: 'http://localhost:9292/api/v0/etablissement/0134567A/enseignant'
+	}).
+	    success( function( response ) {
+		$scope.data = response;
+		$scope.enseignants = $scope.data.map( function( e ) {
+                    return { id: e.enseignant_id,
+                             name: e.enseignant_id,
+                             discipline: '<void>',
+                             stats: e.statistiques.reduce( function(total, monthly_stat) {
+                                 return { validated: total.validated + monthly_stat.validated } ;
+                             }, { validated: 0 } ).validated + '/' + e.statistiques.reduce( function(total, monthly_stat) {
+                                 return { filled: total.filled + monthly_stat.filled } ;
+                             }, { filled: 0 } ).filled
+			   };
+                } );
 
-  EmploiDuTemps.getMois().success(
-    function( response ) {
-      $scope.annee = response.mois;
+		// $scope.radar.data.push( $scope.enseignants.map( function( e ) {
+		//                           return { axis: e.name, value: e.total };
+		//                         } ) );
+		// $scope.radar.data.push( $scope.enseignants.map( function( e ) {
+		//                           return { axis: e.name, value: e.stats };
+		//                         } ) );
+	    }).
+	    error( function (data, status) {
+		if (status === 404) {
+		    $scope.error = 'it does not exist';
+		} else {
+		    $scope.error = 'Error: ' + status + '\nData: ' + data;
+		}
+	    });
+
+	EmploiDuTemps.getMois().success(
+	    function( response ) {
+		$scope.annee = response.mois;
+	    });
+	$scope.mois = $scope.annee[0];
+
+	console.log($scope.data);
     });
-  $scope.mois = $scope.annee[0];
-
-  console.log($scope.data);
-});
