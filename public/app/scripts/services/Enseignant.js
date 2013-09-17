bca5f15ca5cdbe8a9dd6c4c85cd07f324fe2197b
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-17 15:26:32 +0200

    mention du serveur superflue

--- public/app/scripts/services/Enseignant.js
+++ public/app/scripts/services/Enseignant.js
@@ -2,7 +2,7 @@
 
 angular.module('cahierDeTexteApp')
   .factory('EnseignantAPI', [ '$resource', function Enseignant($resource) {
-      return $resource( 'http://localhost:9292/api/v0/etablissement/:etablissement_id/enseignant/:enseignant_id',
+      return $resource( '/api/v0/etablissement/:etablissement_id/enseignant/:enseignant_id',
 			{ etablissement_id: '@etablissement_id',
 			  enseignant_id: '@enseignant_id' } );
   } ] );
