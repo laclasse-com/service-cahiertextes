c38ca47ea52aab2a6071e73894baa16dc46923a2
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-23 17:36:09 +0200

    filtrage des données

--- public/app/scripts/controllers/PrincipalClassesCtrl.js
+++ public/app/scripts/controllers/PrincipalClassesCtrl.js
@@ -1,22 +1,66 @@
 'use strict';
 
 angular.module('cahierDeTexteApp')
-    .controller('PrincipalClassesCtrl', [ '$scope', 'EmploiDuTemps', function ($scope, EmploiDuTemps) {
-	$scope.classes = [];
-	EmploiDuTemps.getClasses().success( function( response ) {
-	    $scope.classes = response.classes;
-	});
-	$scope.classeCourante = $scope.classes[1];
-	
-	$scope.mois = [];
-	EmploiDuTemps.getMois().success( function( response ) {
-	    $scope.mois = response.mois;
-	});
-	$scope.moisCourant = $scope.mois[1];
-	
-	$scope.matieres = [];
-	EmploiDuTemps.getMatieres().success( function( response ) {
-	    $scope.matieres = response.matieres;
-	});
-	$scope.matiereCourante = $scope.matieres[1];
-    } ] );
+    .controller('PrincipalClassesCtrl',
+		[ '$scope', 'EmploiDuTemps', 'ClasseAPI',
+		  function ($scope, EmploiDuTemps, ClasseAPI) {
+		      $scope.uai = '0134567A';
+		      
+		      $scope.raw_data = [];
+
+		      $scope.classes = [];
+		      $scope.matieres = [];
+		      $scope.mois = [];
+		      $scope.classeCourante = '';
+		      $scope.moisCourant = -1;
+		      $scope.matiereCourante = -1;
+
+		      $scope.process_data = function(  ) {
+			  $scope.data = $scope.raw_data;
+			  if ( $scope.classeCourante !== '' ) {
+			      $scope.data = _.filter( $scope.data,
+						      function( r ) {
+							  return r.regroupement_id == $scope.classeCourante;
+						      });
+			  }
+
+			  if ( $scope.matiereCourante != -1 ) {
+			      $scope.data = $scope.data.map( function( regroupement ) {
+				  var par_matiere = _.filter( regroupement.par_matiere,
+						       function( r ) {
+							   return r.matiere_id == $scope.matiereCourante;
+						       });
+				  var stats = par_matiere[0].par_mois.reduce( function(total, monthly_stat) {
+					  return { filled: total.filled + monthly_stat.filled,
+						   validated: total.validated + monthly_stat.validated } ;
+				  }, { filled: 0, validated: 0 } );
+				  return {
+				      "regroupement_id": regroupement.regroupement_id,
+				      "par_matiere": par_matiere,
+				      "filled":  stats.filled,
+				      "validated": stats.validated
+				  };
+			      });			      
+			  }
+			  
+			  $scope.classes = $scope.raw_data.map( function( r ) {
+			      return r.regroupement_id;
+			  });
+
+			  $scope.matieres = _.uniq( _.flatten( $scope.raw_data.map( function( r ) {
+			      return r.par_matiere.map( function( m ) {
+				  return m.matiere_id;
+			      });
+			  }) ) );
+		      };
+
+		      EmploiDuTemps.getMois().success( function( response ) {
+			  $scope.mois = response.mois;
+		      });
+
+		      ClasseAPI.query( { uai: $scope.uai, id: '' },
+				       function( response ) {
+					   $scope.raw_data = response;
+					   $scope.process_data();
+				       });
+		  } ] );
