c24973b4fbedb4b30469fbad0fca4c64a3abbb32
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-24 11:40:19 +0200

    refactoring par_* -> *s

--- models/cahier_de_textes.rb
+++ models/cahier_de_textes.rb
@@ -5,12 +5,12 @@ class CahierDeTextes < Sequel::Model( :cahiers_de_textes )
     cours = Cours.where(cahier_de_textes_id: id).where(:date_cours)
     {
       regroupement_id: regroupement_id,
-      par_matiere:
+      matieres:
       cours.join(:creneaux_emploi_du_temps, id: :creneau_emploi_du_temps_id).select(:matiere_id).group_by(:matiere_id).map do
         |record|
         {
           matiere_id: record.values[:matiere_id],
-          par_mois:
+          mois:
           (1..12).map do
             |month|
             tmp_cours = cours.join(:creneaux_emploi_du_temps, id: :creneau_emploi_du_temps_id).where(matiere_id: record.values[:matiere_id]).where( 'extract( month from date_cours ) = ' + month.to_s )
