03ed00851fc0c095428c8b2e3ad41b5be49fd5ec
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-26 16:26:29 +0200

    filtrage (buggé) sur les mois

--- public/app/scripts/controllers/PrincipalEnseignantCtrl.js
+++ public/app/scripts/controllers/PrincipalEnseignantCtrl.js
@@ -8,97 +8,17 @@ angular.module('cahierDeTexteApp')
 		      $scope.classe = -1;
 		      $scope.mois = [];
 		      $scope.moisCourant = -1;
+		      $scope.raw_data = [];
+		      $scope.data = [];
+		      
 
-		      $scope.process_data = function(  ) {
-			  $scope.saisies = [];
-			  if ( typeof $scope.raw_data !== 'undefined' ) {
-			      _($scope.raw_data.saisies).each( function( mois ) {
-				  _(mois).each( function ( saisie ) {
-				      if ( ( $scope.classe == -1 ) || ( saisie.classe_id == $scope.classe ) ) {
-					  $scope.saisies.push( { classe: saisie.classe_id,
-								 matiere: saisie.matiere_id,
-								 cours: saisie.cours,
-								 devoir: saisie.devoir,
-								 valide: saisie.valide,
-								 cours_id: saisie.cours_id,
-								 devoir_id: saisie.devoir_id } );
-				      }
-				  } );
-			      } );
-
-			      $scope.graphiques = {
-				  pieChart: { options: $rootScope.globalPieChartOptions,
-					      data: [ { color : "#00ff00",
-							value: 0 },
-						      { color : "#aaffaa",
-							value: 0 } ] },
-				  barChart: { options: $rootScope.globalLineChartOptions,
-					      data: { labels: [],
-						      datasets: [
-							  { fillColor : "#aaffaa", pointColor : "#aaffaa",
-							    strokeColor : "#88aa88", pointStrokeColor : "#88aa88",
-							    data: []
-							  },
-							  { fillColor : "#00ff00", pointColor : "#00ff00",
-							    strokeColor : "#00aa00", pointStrokeColor : "#00aa00",
-							    data: []
-							  } ] } }
-			      };
-
-			      _.chain($scope.saisies)
-				  .groupBy('classe')
-				  .map( function( classe ) {
-				      return { classe: classe[0].classe,
-					       filled: _(classe).size(),
-					       validated: _(classe).where({valide: true}).length };
-				  })
-				  .each( function( classe ) {
-				      $scope.graphiques.barChart.data.labels.push( classe.classe );
-				      $scope.graphiques.barChart.data.datasets[0].data.push( classe.filled );
-				      $scope.graphiques.barChart.data.datasets[1].data.push( classe.validated );
-				  });
-			      _($scope.graphiques.pieChart.data.length).times( function( i ) {
-				  $scope.graphiques.pieChart.data[i].value = $scope.graphiques.barChart.data.datasets[i].data.reduce( function( compteur, valeur ) {
-				      return compteur + valeur;
-				  }, 0);
-			      });
-
-			      if ( $scope.classe == -1 ) {
-				  $scope.classes = _.chain( $scope.raw_data )
-				      .flatten()
-				      .map( function( e ) {
-					  return e.classe_id;
-				      } )
-				      .reject( function( e ) { // FIXME: shouldn't have null in hte first place
-					  return e === null;
-				      })
-				      .uniq()
-				      .value();
-			      }
-			      if ( $scope.matiere == -1 ) {
-				  $scope.matieres = _.chain( $scope.raw_data )
-				      .flatten()
-				      .map( function( e ) {
-					  return e.matiere_id;
-				      } )
-				      .reject( function( e ) { // FIXME: shouldn't have null in hte first place
-					  return e === null;
-				      })
-				      .uniq()
-				      .value();
-			      }
-
-			  }
-
-			  $scope.gridEntries.columnDefs[0].visible = $scope.classe == -1; // FIXME: nggrid ne prends pas ceci en compte, affecté trop tard...
-		      };
 		      EmploiDuTemps.getMois().success( function( response ) {
 			  $scope.mois = response.mois;
 		      });
 
 		      // Tableau
 		      $scope.gridEntries = {
-			  data: 'saisies',
+			  data: 'gridSaisies',
 			  enableCellEdit: false,
 			  plugins: [new ngGridFlexibleHeightPlugin()],
 			  rowHeight: 60,
@@ -123,6 +43,82 @@ angular.module('cahierDeTexteApp')
 			      CoursAPI.valide({ id: e.cours_id }, {});
 			      e.valide = true;
 			  });
+		      };
+
+		      $scope.process_data = function(  ) {
+			  $scope.gridSaisies = [];
+
+			  // Extraction des classes
+			  if ( $scope.classe == -1 ) {
+			      $scope.classes = _.chain( $scope.raw_data.saisies )
+				  .flatten()
+				  .groupBy('classe_id')
+				  .keys()
+				  .value();
+			  }
+
+			  // Filtrage des données
+			  $scope.data = $scope.raw_data; //??????
+			  
+			  if ( $scope.moisCourant != -1 ) {
+			      $scope.data.saisies = $scope.data.saisies[ $scope.moisCourant - 1 ];
+			  }
+			  console.log(_($scope.data.saisies).size());
+			  $scope.data.saisies = _($scope.data.saisies).flatten();
+
+			  // population de gridSaisies
+			  _($scope.data.saisies).each( function ( saisie ) {
+			      if ( ( $scope.classe == -1 ) || ( saisie.classe_id == $scope.classe ) ) {
+				  $scope.gridSaisies.push( { classe: saisie.classe_id,
+							     matiere: saisie.matiere_id,
+							     cours: saisie.cours,
+							     devoir: saisie.devoir,
+							     valide: saisie.valide,
+							     cours_id: saisie.cours_id,
+							     devoir_id: saisie.devoir_id } );
+			      }
+			  } );
+
+			  // population des données des graphiques
+			  $scope.graphiques = {
+			      pieChart: { options: $rootScope.globalPieChartOptions,
+					  data: [ { color : "#00ff00",
+						    value: 0 },
+						  { color : "#aaffaa",
+						    value: 0 } ] },
+			      barChart: { options: $rootScope.globalLineChartOptions,
+					  data: { labels: [],
+						  datasets: [
+						      { fillColor : "#aaffaa", pointColor : "#aaffaa",
+							strokeColor : "#88aa88", pointStrokeColor : "#88aa88",
+							data: []
+						      },
+						      { fillColor : "#00ff00", pointColor : "#00ff00",
+							strokeColor : "#00aa00", pointStrokeColor : "#00aa00",
+							data: []
+						      } ] } }
+			  };
+
+			  _.chain($scope.data.saisies)
+			      .groupBy('classe')
+			      .map( function( classe ) {
+				  return { classe: classe[0].classe,
+					   filled: _(classe).size(),
+					   validated: _(classe).where({valide: true}).length };
+			      })
+			      .each( function( classe ) {
+				  $scope.graphiques.barChart.data.labels.push( classe.classe );
+				  $scope.graphiques.barChart.data.datasets[0].data.push( classe.filled );
+				  $scope.graphiques.barChart.data.datasets[1].data.push( classe.validated );
+			      });
+			  _($scope.graphiques.pieChart.data.length).times( function( i ) {
+			      $scope.graphiques.pieChart.data[i].value = $scope.graphiques.barChart.data.datasets[i].data.reduce( function( compteur, valeur ) {
+				  return compteur + valeur;
+			      }, 0);
+			  });
+			  
+			  // FIXME: nggrid ne prends pas ceci en compte, affecté trop tard...
+			  // $scope.gridEntries.columnDefs[0].visible = $scope.classe == -1;
 		      };
 
 		      // Récupération et consommation des données
