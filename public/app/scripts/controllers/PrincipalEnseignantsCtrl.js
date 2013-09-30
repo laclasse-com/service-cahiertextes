8d5fcced57d45a24daba33a8c2216f2f7ca4e02c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-09-30 17:29:08 +0200

    filtrage par classe dans le dashboard par enseignant

--- public/app/scripts/controllers/PrincipalEnseignantsCtrl.js
+++ public/app/scripts/controllers/PrincipalEnseignantsCtrl.js
@@ -2,48 +2,84 @@
 
 angular.module('cahierDeTexteApp')
     .controller('PrincipalEnseignantsCtrl', [ '$scope', '$rootScope', '$http', 'EmploiDuTemps', function ($scope, $rootScope, $http, EmploiDuTemps) {
+	$scope.classe = -1;
+	$scope.mois = -1;
+	
 	$scope.process_data = function(  ) {
-	    if ( typeof $scope.raw_data !== 'undefined' ) {
+	    if ( $scope.raw_data !== undefined ) {
 		$scope.enseignants = [];
 		$scope.radar = { options: $rootScope.globalRadarChartOptions,
 				 data: { labels: [],
 					 datasets: [
-					     // 0: saisies validées
-					     { fillColor : "#00ff00", pointColor : "#00ff00",
-					       strokeColor : "#00aa00", pointStrokeColor : "#00aa00",
-					       data: []
-					     },
-					     // 1: saisies totales
+					     // saisies totales
 					     { fillColor : "#aaffaa", pointColor : "#aaffaa",
 					       strokeColor : "#88aa88", pointStrokeColor : "#88aa88",
 					       data: []
+					     },
+					     // saisies validées
+					     { fillColor : "#00ff00", pointColor : "#00ff00",
+					       strokeColor : "#00aa00", pointStrokeColor : "#00aa00",
+					       data: []
 					     } ] } };
 
-		_.each( $scope.raw_data, function( e ) {
-		    var stats = { filled: 0,
-				  validated: 0};
-		    if ( $scope.mois < 0 ) {
-			stats.validated =  e.statistiques.reduce(
-			    function(total, monthly_stat) {
-				return { validated: total.validated + monthly_stat.validated } ;
-			    }, { validated: 0 } ).validated;
-			stats.filled = e.statistiques.reduce(
-			    function(total, monthly_stat) {
-				return { filled: total.filled + monthly_stat.filled } ;
-			    }, { filled: 0 } ).filled;
-		    } else {
-			stats.validated = e.statistiques[ $scope.mois - 1 ].validated;
-			stats.filled = e.statistiques[ $scope.mois - 1 ].filled;
-		    }
+		$scope.classes = _.chain($scope.raw_data)
+		    .pluck('classes')
+		    .flatten()
+		    .pluck('regroupement')
+		    .uniq()
+		    .value();
 
-		    $scope.enseignants.push( { id: e.enseignant_id,
-					       name: e.enseignant_id,
-					       discipline: '<void>',
-					       stats: stats.validated + '/' + stats.filled } );
-		    $scope.radar.data.labels.push( e.enseignant_id );
-		    $scope.radar.data.datasets[0].data.push( stats.validated );
-		    $scope.radar.data.datasets[1].data.push( stats.filled );
-		} );
+		var data = $scope.raw_data;
+		
+		if ( $scope.classe != -1 ) {
+		    data = _.chain(data)
+			.map( function( enseignant ) {
+			    return { enseignant_id: enseignant.enseignant_id,
+				     classes: _(enseignant.classes).reject( function( classe ) {
+					 return classe.regroupement != $scope.classe;
+				     })
+				   };
+		        })
+			.reject( function( enseignant ) {
+			    return enseignant.classes.length === 0;
+			})
+			.value();
+		}
+
+		if ( $scope.mois != -1 ) {
+		    data = _(data).map( function( enseignant ) {
+			return { enseignant_id: enseignant.enseignant_id,
+				 classes: _(enseignant.classes).map( function( classe ) {
+				     return { regroupement: classe.regroupement,
+					      statistiques: _(classe.statistiques).reject( function( mois ) {
+						  return mois.month != $scope.mois;
+					      })
+					    };
+				 })
+			       };
+		    });
+		}
+
+		$scope.enseignants = _(data).map( function( enseignant ) {
+		    var stats = _(enseignant.classes).reduce( function( totaux, classe ) {
+			var stats = _(classe.statistiques).reduce( function( totaux, mois ) {
+			    return { filled: totaux.filled + mois.filled,
+				     validated: totaux.validated + mois.validated};
+			}, { filled: 0, validated: 0});
+			
+			return { filled: totaux.filled + stats.filled,
+				 validated: totaux.validated + stats.validated};
+		    }, { filled: 0, validated: 0});
+
+		    $scope.radar.data.labels.push( enseignant.enseignant_id );
+		    $scope.radar.data.datasets[0].data.push( stats.filled );
+		    $scope.radar.data.datasets[1].data.push( stats.validated );
+
+		    return { id: enseignant.enseignant_id,
+			     name: enseignant.enseignant_id,
+			     discipline: '<void>',
+			     stats: stats.validated + '/' + stats.filled };
+		});
 	    }
 	};
 
@@ -65,7 +101,6 @@ angular.module('cahierDeTexteApp')
 		function( response ) {
 		    $scope.annee = response.mois;
 		});
-	$scope.mois = -1;
 
 	$scope.enseignant = -1;
 
