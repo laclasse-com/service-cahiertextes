df6ecc313e56c67c39ddbe80ce4cf9562f9130de
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-10-02 15:27:05 +0200

    données de test encore meilleurs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -78,18 +78,20 @@ def generate_test_data
         creneau = CreneauEmploiDuTempsEnseignant.all.sample
 
         cours = Cours.create(cahier_de_textes_id: cahier_de_textes.id,
-                             creneau_emploi_du_temps_id: creneau.id,
+                             creneau_emploi_du_temps_id: creneau.creneau_emploi_du_temps_id,
                              date_cours: '2013-' + (month + 1).to_s + '-29',
                              contenu: 'Exemple de séquence pédagogique.',
                              enseignant_id: creneau.enseignant_id )
         STDERR.putc '.'
 
-        Devoir.create(cours_id: cours.id,
-                      type_devoir_id: TypeDevoir.all.sample.id,
-                      date_due: Time.now,
-                      contenu: 'Exemple de devoir.',
-                      temps_estime: rand(0..120) )
-        STDERR.putc '.'
+        if ( rand > 0.25 ) then
+          Devoir.create(cours_id: cours.id,
+                        type_devoir_id: TypeDevoir.all.sample.id,
+                        date_due: Time.now,
+                        contenu: 'Exemple de devoir.',
+                        temps_estime: rand(0..120) )
+          STDERR.putc '.'
+        end
       }
     }
   }
