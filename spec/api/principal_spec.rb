5c122253a4e85a1829e0823a2c2c3ca839df1dde
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-10-02 12:25:10 +0200

    factorisation de la génération des données de test

--- spec/api/principal_spec.rb
+++ spec/api/principal_spec.rb
@@ -11,40 +11,7 @@ describe CahierDeTextesAPI::API do
     xml_filename = 'spec/fixtures/Edt_To_LaclasseCom_0134567A_Enclair.xml'
     post '/api/v0/pronote/xml', xml_file: Rack::Test::UploadedFile.new(xml_filename, 'text/xml')
 
-    STDERR.puts 'Remplissage des Cahiers de textes'
-    [ [ 'DS', 'Devoir surveillé' ],
-      [ 'DM', 'Devoir à la maison' ],
-      [ 'Leçon', 'Leçon à apprendre' ],
-      [ 'Exposé', 'Exposé à préparer' ],
-      [ 'Recherche', 'Recherche à faire' ],
-      [ 'Exercice', 'Exercice à faire' ] ].each { |type|
-      TypeDevoir.create(label: type[0],
-                        description: type[1] )
-      STDERR.putc '.'
-    }
-
-    12.times {
-      |month|
-      rand(2..4).times {
-        CreneauEmploiDuTempsEnseignant.select( :creneau_emploi_du_temps_id, :enseignant_id ).limit( 32 ).each {
-          |item|
-          cours = Cours.create(cahier_de_textes_id: CahierDeTextes.all.sample.id,
-                               creneau_emploi_du_temps_id: item.values[ :creneau_emploi_du_temps_id ],
-                               date_cours: '2013-' + (month + 1).to_s + '-29',
-                               contenu: 'Exemple de séquence pédagogique.',
-                               enseignant_id: item.values[:enseignant_id] )
-          STDERR.putc '.'
-          Devoir.create(cours_id: cours.id,
-                        type_devoir_id: TypeDevoir.all.sample.id,
-                        date_due: Time.now,
-                        contenu: 'Exemple de devoir.',
-                        temps_estime: rand(0..120) )
-          STDERR.putc '.'
-        }
-      }
-    }
-    STDERR.puts
-
+    generate_test_data
   end
 
   def app
