a99551ce7da8749327996dd6acb9ef0bd2e65f8d
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-10-02 10:49:26 +0200

    ajout des cours éventuels dans la réponse & construction de l'objet event côté client

--- api/emploi_du_temps.rb
+++ api/emploi_du_temps.rb
@@ -35,24 +35,12 @@ module CahierDeTextesAPI
         lundi = date_of_last 'monday' # FIXME: pas forcément un lundi ?
         jour = lundi + ( creneau.jour_de_la_semaine - 2)
 
-        { title: 'test',
+        cours = Cours.where(creneau_emploi_du_temps_id: creneau.id).where(cahier_de_textes_id: CahierDeTextes.where( regroupement_id: dummy_regroupement_id ).first.id )
+
+        { matiere_id: creneau.matiere_id,
           start: Time.new( jour.year, jour.month, jour.mday, plage_debut.hour, plage_debut.min ).iso8601,
           end: Time.new( jour.year, jour.month, jour.mday, plage_fin.hour, plage_fin.min ).iso8601,
-          allDay: false,
-          url: 'http://laclasse.com',
-          color: '#778899' }
-
-        # Cours
-        #   .where(creneau_emploi_du_temps_id: creneau.id)
-        #   .map {
-        #   |cours|
-        #   { title: cours ? cours.contenu : '',
-        #     start: Time.new( jour.year, jour.month, jour.mday, plage_debut.hour, plage_debut.min ).iso8601,
-        #     end: Time.new( jour.year, jour.month, jour.mday, plage_fin.hour, plage_fin.min ).iso8601,
-        #     allDay: false,
-        #     url: 'http://laclasse.com',
-        #     color: '#778899' }
-        # }
+          cours_id: cours.count > 0 ? cours.first.id : -1 }
       }.flatten
     end
 
