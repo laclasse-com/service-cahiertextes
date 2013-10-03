51076740d6fb98f8b7a84751e7b0e290e15eab32
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-10-03 14:37:36 +0200

    l'API envoie les cours et devoirs complets qui sont affichés dans une popup

--- public/app/scripts/controllers/EleveCalendarCtrl.js
+++ public/app/scripts/controllers/EleveCalendarCtrl.js
@@ -2,9 +2,31 @@
 
 angular.module('cahierDeTexteApp')
     .controller('EleveCalendarCtrl',
-		[ '$scope', '$rootScope', 'APIEmploiDuTemps',
-		  function ( $scope, $rootScope, APIEmploiDuTemps ) {
-		      /* config object */
+		[ '$scope', '$rootScope', '$modal', 'APIEmploiDuTemps',
+		  function ( $scope, $rootScope, $modal, APIEmploiDuTemps ) {
+		      // popup d'affichage des détails
+		      $scope.cours = {};
+		      $scope.devoir = {};
+		      $scope.affiche_details = function(  ) {
+			  // var modalInstance = 
+			  $modal.open({ templateUrl: 'views/modals/eleve.detail_emploi_du_temps.html',
+					controller: modalInstanceCtrl,
+					resolve: {
+					    cours: function() { return $scope.cours; },
+					    devoir: function() { return $scope.devoir; }
+					}
+				      });
+		      };
+		      var modalInstanceCtrl = function( $scope, $modalInstance, cours, devoir ) {
+			  $scope.cours = cours;
+			  $scope.devoir = devoir;
+
+			  $scope.close = function() {
+			      $modalInstance.close();
+			  };
+		      };
+
+		      // configuration du composant calendrier
 		      $scope.calendar = {
 			  options: $rootScope.globalCalendarOptions,
 			  events: [  ]
@@ -15,15 +37,27 @@ angular.module('cahierDeTexteApp')
 		      $scope.calendar.options.header = { left: 'title',
 							 center: 'agendaDay agendaWeek month',
 							 right: 'today prev,next' };
+		      $scope.calendar.options.eventClick = function( event ) {
+			  var event_data = _(event.source.events).findWhere({_id: event._id});
+			  console.log( event_data.details );
+			  $scope.cours = event_data.details.cours;
+			  $scope.devoir = event_data.details.devoir;
+			  $scope.affiche_details(  );
+		      };
 
+		      
+		      // population des créneaux d'emploi du temps avec les cours et devoirs éventuels
 		      APIEmploiDuTemps.query( function( response ) {
 			  $scope.calendar.events.push( response.map( function( event ) {
-			      return { allDay: false,
+			      
+			      // TODO: mettre les détails cours et devoir dans un tableau à part pour popup
+			      return { details: { cours: event.cours,
+						  devoir: event.devoir },
+				       allDay: false,
 				       title: ''+event.matiere_id,
 				       start: new Date( event.start ),
 				       end: new Date( event.end ),
-				       url: (event.cours_id > 0) ? 'javascript:alert(' + event.cours_id + ')' : '',
-				       color: (event.devoir_id > 0) ? $rootScope.theme.calendar.devoir : (event.cours_id > 0) ? $rootScope.theme.calendar.saisie : $rootScope.theme.calendar.vide };
+				       color: (_(event.devoir).size() > 0) ? $rootScope.theme.calendar.devoir : (_(event.cours).size() > 0) ? $rootScope.theme.calendar.saisie : $rootScope.theme.calendar.vide };
 			  } ) );
 		      });
 		  }
