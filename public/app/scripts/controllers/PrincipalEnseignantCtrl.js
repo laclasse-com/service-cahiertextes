4f51d3e0ae57baf2418d0ba7a250a7266878e264
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-10-08 11:58:49 +0200

    refactoring massif, refonte de l'API Annuaire

--- public/app/scripts/controllers/PrincipalEnseignantCtrl.js
+++ public/app/scripts/controllers/PrincipalEnseignantCtrl.js
@@ -2,147 +2,181 @@
 
 angular.module('cahierDeTexteApp')
     .controller('PrincipalEnseignantCtrl',
-		[ '$scope', '$rootScope', '$stateParams', 'APIEnseignant', 'APICours', 'APIAnnuaire', 'Etablissement',
-		  function( $scope, $rootScope, $stateParams, APIEnseignant, APICours, APIAnnuaire, Etablissement ) {
-		      $scope.enseignant_id = $stateParams.enseignant_id;
+		[ '$scope', '$rootScope', '$stateParams', 'APIEnseignant', 'APICours', 'APIMatieres', 'APIRegroupements', 'APIUsers',
+		  function( $scope, $rootScope, $stateParams, APIEnseignant, APICours, APIMatieres, APIRegroupements, APIUsers ) {
+		      // FIXME: dummy enseignant_id
+		      $scope.enseignant_id = 'VAA60462';
+		      console.log($stateParams.enseignant_id)
 		      $scope.classe = -1;
 		      $scope.mois = $rootScope.mois;
 		      $scope.moisCourant = -1;
-		      $scope.raw_data = [];
-		      $scope.data = [];
-		      $scope.selectedSaisies = [];
-
-		      APIAnnuaire.getUser( $scope.enseignant_id )
-			  .then( function( response ) {
-			      $scope.enseignant = response.data;
-			      $scope.enseignant.matieres = _.chain($scope.enseignant.classes)
-				  .pluck( 'matiere_libelle' )
-				  .uniq()
-				  .value();
-			  });
 
 		      // Tableau
 		      $scope.grid = {
 			  data: 'gridSaisies',
-			  selectedItems: $scope.selectedSaisies,
+			  selectedItems: 'selectedSaisies',
 			  enableCellEdit: false,
 			  plugins: [new ngGridFlexibleHeightPlugin()],
 			  rowHeight: 60,
 			  columnDefs: [
-			      { field: 'classe', displayName: 'Classe' },
-			      { field: 'matiere', displayName: 'Matière' },
+			      { field: 'classe.libelle', displayName: 'Classe' },
+			      { field: 'matiere.libelle_long', displayName: 'Matière' },
 			      { field: 'cours', displayName: 'Cours',
 				cellTemplate: '<span style="overflow-y:auto" ng-bind-html-unsafe="row.entity.cours">{{row.entity.cours}}</span>' },
 			      { field: 'devoir', displayName: 'Travail à faire',
 				cellTemplate: '<span style="overflow-y:auto" ng-bind-html-unsafe="row.entity.devoir">{{row.entity.devoir}}</span>' },
 			      { field: 'validated', displayName: 'Validé',
-				cellTemplate: '<div class="ngSelectionCell"><input tabindex="-1" class="ngSelectionCheckbox" type="checkbox" ng-model="row.entity.valide" ng-show="!row.entity.valide" ng-click="valide( {{row.entity.cours_id}} )" /><input tabindex="-1" class="ngSelectionCheckbox" type="checkbox" disabled checked ng-show="row.entity.valide" /></div>'}
-			  ]
-		      };
-		      $scope.valide = function( cours_id ) {
-			  APICours.valide({ id: cours_id }, {});
-			  $scope.populate_graphs( $scope.gridSaisies );
-		      };
-		      $scope.valideSelection = function() {
-			  _($scope.selectedSaisies).each( function( saisie ) {
-			      APICours.valide({ id: saisie.cours_id }, {});
-			      saisie.valide = true;
-			  });
-			  $scope.populate_graphs( $scope.gridSaisies );
-		      };
-		      $scope.selectionneNonValides = function() {
-			  _($scope.gridSaisies).each( function( saisie, index ){
-			      if( saisie.valide === false ) {
-				  $scope.grid.selectItem( index, true );
-			      }
-			  });
-		      };
-
-		      $scope.populate_graphs = function( saisies ) {
-			  $scope.graphiques = {
-			      pieChart: { options: $rootScope.globalPieChartOptions,
-					  data: [ { color : $rootScope.theme.filled.base,
-						    value: 0 },
-						  { color : $rootScope.theme.validated.base,
-						    value: 0 } ] },
-			      barChart: { options: $rootScope.globalLineChartOptions,
-					  data: { labels: [],
-						  datasets: [
-						      { fillColor : $rootScope.theme.filled.base,
-							pointColor : $rootScope.theme.filled.base,
-							strokeColor : $rootScope.theme.filled.stroke,
-							pointStrokeColor : $rootScope.theme.filled.stroke,
-							data: []
-						      },
-						      { fillColor : $rootScope.theme.validated.base,
-							pointColor : $rootScope.theme.validated.base,
-							strokeColor : $rootScope.theme.validated.stroke,
-							pointStrokeColor : $rootScope.theme.validated.stroke,
-							data: []
-						      } ] } }
-			  };
-
-			  _.chain(saisies)
-			      .groupBy('classe')
-			      .map( function( classe ) {
-				  return { classe: classe[0].classe,
-					   filled: classe.length,
-					   validated: _(classe).where({valide: true}).length };
-			      })
-			      .each( function( classe ) {
-				  $scope.graphiques.barChart.data.labels.push( classe.classe );
-				  $scope.graphiques.barChart.data.datasets[0].data.push( classe.filled );
-				  $scope.graphiques.barChart.data.datasets[1].data.push( classe.validated );
-
-				  $scope.graphiques.pieChart.data[0].value += classe.filled - classe.validated;
-				  $scope.graphiques.pieChart.data[1].value = classe.validated;
+				cellTemplate: '<div class="ngSelectionCell"><input tabindex="-1" class="ngSelectionCheckbox" type="checkbox" ng-model="row.entity.valide" ng-show="!row.entity.valide" ng-click="grid.valide( {{row.entity.cours_id}} )" /><input tabindex="-1" class="ngSelectionCheckbox" type="checkbox" disabled checked ng-show="row.entity.valide" /></div>'}
+			  ],
+			  valide: function( cours_id ) {
+			      APICours.valide({ id: cours_id }, {});
+			      $scope.populate_graphs( $scope.gridSaisies );
+			  },
+			  valideSelection: function() {
+			      _($scope.selectedSaisies).each( function( saisie ) {
+				  APICours.valide({ id: saisie.cours_id }, {});
+				  saisie.valide = true;
 			      });
+			      $scope.populate_graphs( $scope.gridSaisies );
+			  },
+			  selectionneNonValides: function() {
+			      _($scope.gridSaisies).each( function( saisie, index ){
+				  if( saisie.valide === false ) {
+				      $scope.grid.selectItem( index, true );
+				  }
+			      });
+			  },
+			  populate: function( saisies ) {
+			      $scope.gridSaisies = [];
+			      $scope.selectedSaisies = [];
+
+			      _(saisies).each( function ( saisie ) {
+				  if ( ( $scope.classe == -1 ) || ( saisie.classe_id == $scope.classe ) ) {
+				      var record = { classe: '',
+						     matiere: '',
+						     cours: saisie.cours,
+						     devoir: saisie.devoir,
+						     valide: saisie.valide,
+						     cours_id: saisie.cours_id,
+						     devoir_id: saisie.devoir_id };
+				      APIMatieres.get({ matiere_id: saisie.matiere_id },
+						      function success( response ) {
+							  record.matiere = response.libelle_long;
+						      },
+						      function error(  ) {
+							  record.matiere = 'Matière inconnue';
+						      });
+				      APIRegroupements.get({ regroupement_id: saisie.classe_id },
+							   function success( response ) {
+							       record.regroupement = response.libelle;
+							   },
+							   function error(  ) {
+							       record.regroupement = 'Classe inconnue';
+							   });
+				      
+				      $scope.gridSaisies.push( record );
+				  }
+			      } );
+			  }
 		      };
 
-		      $scope.process_data = function(  ) {
-			  $scope.gridSaisies = [];
+		      // Graphiques
+		      $scope.graphiques = {
+			  pieChart: { options: $rootScope.globalPieChartOptions,
+				      data: [ { color : $rootScope.theme.filled.base,
+						value: 0 },
+					      { color : $rootScope.theme.validated.base,
+						value: 0 } ] },
+			  barChart: { options: $rootScope.globalLineChartOptions,
+				      data: { labels: [],
+					      datasets: [
+						  { fillColor : $rootScope.theme.filled.base,
+						    pointColor : $rootScope.theme.filled.base,
+						    strokeColor : $rootScope.theme.filled.stroke,
+						    pointStrokeColor : $rootScope.theme.filled.stroke,
+						    data: []
+						  },
+						  { fillColor : $rootScope.theme.validated.base,
+						    pointColor : $rootScope.theme.validated.base,
+						    strokeColor : $rootScope.theme.validated.stroke,
+						    pointStrokeColor : $rootScope.theme.validated.stroke,
+						    data: []
+						  } ] } },
+			  populate: function( saisies ) {
+			      $scope.graphiques.barChart.data.labels = [];
+			      $scope.graphiques.barChart.data.datasets[0].data = [];
+			      $scope.graphiques.barChart.data.datasets[1].data = [];
+			      $scope.graphiques.pieChart.data[0].value = 0;
+			      $scope.graphiques.pieChart.data[1].value = 0;
 
-			  // Extraction des classes
-			  if ( $scope.classe == -1 ) {
-			      $scope.classes = _.chain( $scope.raw_data.saisies )
-				  .flatten()
-				  .groupBy('classe_id')
-				  .keys()
-				  .value();
+			      _.chain(saisies)
+				  .groupBy('classe')
+				  .map( function( classe ) {
+				      return { classe: classe[0].classe,
+					       filled: classe.length,
+					       validated: _(classe).where({valide: true}).length };
+				  })
+				  .each( function( classe ) {
+				      $scope.graphiques.barChart.data.labels.push( classe.classe );
+				      $scope.graphiques.barChart.data.datasets[0].data.push( classe.filled );
+				      $scope.graphiques.barChart.data.datasets[1].data.push( classe.validated );
+
+				      $scope.graphiques.pieChart.data[0].value += classe.filled - classe.validated;
+				      $scope.graphiques.pieChart.data[1].value = classe.validated;
+				  });
 			  }
+		      };
 
-			  // Filtrage des données
-			  var saisies = [];
-			  if ( $scope.moisCourant != -1 ) {
-			      saisies = _($scope.raw_data.saisies[ $scope.moisCourant - 1 ]).flatten();
-			  } else {
-			      saisies = _($scope.raw_data.saisies).flatten();
-			  }
-
-			  // population de gridSaisies
-			  _(saisies).each( function ( saisie ) {
-			      if ( ( $scope.classe == -1 ) || ( saisie.classe_id == $scope.classe ) ) {
-				  $scope.gridSaisies.push( { classe: saisie.classe_id,
-							     matiere: saisie.matiere_id,
-							     cours: saisie.cours,
-							     devoir: saisie.devoir,
-							     valide: saisie.valide,
-							     cours_id: saisie.cours_id,
-							     devoir_id: saisie.devoir_id } );
+		      $scope.process_data = function( data ) {
+			  if ( data !== undefined ) {
+			      // Extraction des classes
+			      if ( $scope.classe == -1 ) {
+				  $scope.classes = _.chain( data.saisies )
+				      .flatten()
+				      .groupBy('classe_id')
+				      .keys()
+				      .map( function( key ) {
+					  var regroupement = APIRegroupements.get({ regroupement_id: key });
+					  return regroupement.libelle;
+				      })
+				      .value();
 			      }
-			  } );
 
-			  $scope.populate_graphs( $scope.gridSaisies );
+			      // Filtrage des données
+			      var saisies = [];
+			      if ( $scope.moisCourant != -1 ) {
+				  saisies = _(data.saisies[ $scope.moisCourant - 1 ]).flatten();
+			      } else {
+				  saisies = _(data.saisies).flatten();
+			      }
 
-			  // FIXME: nggrid ne prends pas ceci en compte, affecté trop tard...
-			  // $scope.grid.columnDefs[0].visible = $scope.classe == -1;
+			      $scope.grid.populate( saisies );
+			      $scope.graphiques.populate( $scope.gridSaisies );
+
+			      // FIXME: nggrid ne prends pas ceci en compte, affecté trop tard...
+			      // $scope.grid.columnDefs[0].visible = $scope.classe == -1;
+			  }
 		      };
 
 		      // Récupération et consommation des données
-		      APIEnseignant.get( { enseignant_id: $scope.enseignant_id,
-					   etablissement_id: '0134567A' },
-					 function( response ) {
-					     $scope.raw_data = response;
-					     $scope.process_data();
-					 } );
+		      APIUsers.get({ user_id: $scope.enseignant_id },
+				   function( response ) {
+				       $scope.enseignant = response;
+				       $scope.enseignant.matieres = _.chain($scope.enseignant.classes)
+					   .pluck( 'matiere_libelle' )
+					   .uniq()
+					   .value();
+				   },
+				   function error() {
+				       console.log( 'Erreur d\'apppel de l\'API Users' );
+				   });
+
+		      APIEnseignant.get({ enseignant_id: $stateParams.enseignant_id, //$scope.enseignant_id,
+					  etablissement_id: '0134567A' },
+					function success( response ) {
+					    $scope.process_data( response );
+					},
+					function error() {
+					    console.log( 'Erreur d\'apppel de l\'API Enseignant' );
+					});
 		  } ] );
--- public/app/scripts/controllers/PrincipalEnseignantsCtrl.js
+++ public/app/scripts/controllers/PrincipalEnseignantsCtrl.js
@@ -2,40 +2,105 @@
 
 angular.module('cahierDeTexteApp')
     .controller('PrincipalEnseignantsCtrl',
-		[ '$scope', '$rootScope', 'Etablissement', 'APIEnseignants',
-		  function ($scope, $rootScope, Etablissement, APIEnseignants) {
+		[ '$scope', '$rootScope', 'APIEnseignants', 'APIUsers',
+		  function( $scope, $rootScope, APIEnseignants, APIUsers ) {
+		      $scope.radar = { options: $rootScope.globalRadarChartOptions,
+				       data: { labels: [],
+					       datasets: [
+						   { fillColor : $rootScope.theme.filled.base,
+						     pointColor : $rootScope.theme.filled.base,
+						     strokeColor : $rootScope.theme.filled.stroke,
+						     pointStrokeColor : $rootScope.theme.filled.stroke,
+						     data: []
+						   },
+						   { fillColor : $rootScope.theme.validated.base,
+						     pointColor : $rootScope.theme.validated.base,
+						     strokeColor : $rootScope.theme.validated.stroke,
+						     pointStrokeColor : $rootScope.theme.validated.stroke,
+						     data: []
+						   } ] },
+				       populate: function( enseignants ) {
+					   $scope.radar.data.datasets[0].data = [];
+					   $scope.radar.data.datasets[1].data = [];
+
+					   _(enseignants).each( function( enseignant ) {
+					       var stats = _(enseignant.classes).reduce( function( totaux, classe ) {
+						   var stats = _(classe.statistiques).reduce( function( totaux, mois ) {
+						       return { filled: totaux.filled + mois.filled,
+								validated: totaux.validated + mois.validated};
+						   }, { filled: 0, validated: 0});
+
+						   return { filled: totaux.filled + stats.filled,
+							    validated: totaux.validated + stats.validated};
+					       }, { filled: 0, validated: 0});
+
+					       $scope.radar.data.datasets[0].data.push( stats.filled );
+					       $scope.radar.data.datasets[1].data.push( stats.validated );
+
+					       $scope.radar.data.labels.push( 'UNK' );
+					   });
+				       }
+				     };
+		      $scope.grid = { data: 'enseignants',
+				      enableCellEdit: true,
+				      plugins: [new ngGridFlexibleHeightPlugin()],
+				      columnDefs: [
+					  { field: 'name', displayName: 'Nom',
+					    cellTemplate: '<div><a href="#/principal/enseignant/{{row.getProperty(\'id\')}}">{{row.entity[col.field]}}</a></div>' },
+					  { field: 'discipline', displayName: 'Discipline' },
+					  { field: 'stats', displayName: 'Statistiques' }],
+				      populate: function( enseignants ) {
+					  $scope.enseignants = _(enseignants).map( function( enseignant ) {
+					      var stats = _(enseignant.classes).reduce( function( totaux, classe ) {
+						  var stats = _(classe.statistiques).reduce( function( totaux, mois ) {
+						      return { filled: totaux.filled + mois.filled,
+							       validated: totaux.validated + mois.validated};
+						  }, { filled: 0, validated: 0});
+
+						  return { filled: totaux.filled + stats.filled,
+							   validated: totaux.validated + stats.validated};
+					      }, { filled: 0, validated: 0});
+
+					      var row = { id: enseignant.enseignant_id,
+							  name: 'UNK',
+							  discipline: [],
+							  stats: stats.validated + '/' + stats.filled };
+
+					      // FIXME: debug data
+					      APIUsers.get({ user_id: 'VAA60462' },
+							   // APIUsers.get({ user_id: enseignant.enseignant_id },
+							   function( response ) {
+							       row.name = response.full_name;
+							       row.discipline = _.chain(response.classes)
+								   .pluck( 'matiere_libelle' )
+								   .uniq()
+								   .value();
+							   },
+							   function error() {
+							       console.log( 'Erreur d\'apppel de l\'API Users' );
+							   });
+
+					      return row;
+					  });
+				      }
+				    };
 		      $scope.annee = $rootScope.mois;
 		      $scope.classe = -1;
 		      $scope.mois = -1;
-		      
-		      $scope.process_data = function(  ) {
-			  if ( $scope.raw_data !== undefined ) {
-			      $scope.enseignants = [];
-			      $scope.radar = { options: $rootScope.globalRadarChartOptions,
-					       data: { labels: [],
-						       datasets: [
-							   { fillColor : $rootScope.theme.filled.base,
-							     pointColor : $rootScope.theme.filled.base,
-							     strokeColor : $rootScope.theme.filled.stroke,
-							     pointStrokeColor : $rootScope.theme.filled.stroke,
-							     data: []
-							   },
-							   { fillColor : $rootScope.theme.validated.base,
-							     pointColor : $rootScope.theme.validated.base,
-							     strokeColor : $rootScope.theme.validated.stroke,
-							     pointStrokeColor : $rootScope.theme.validated.stroke,
-							     data: []
-							   } ] } };
+		      $scope.enseignant = -1;
 
-			      $scope.classes = _.chain($scope.raw_data)
+		      $scope.process_data = function( data ) {
+			  console.log('$scope.process_data called !')
+			  if ( data !== undefined ) {
+			      // extraction des classes
+			      $scope.classes = _.chain(data)
 				  .pluck('classes')
 				  .flatten()
 				  .pluck('regroupement')
 				  .uniq()
 				  .value();
 
-			      var data = $scope.raw_data;
-			      
+			      // filtrage sur la classe sélectionnée
 			      if ( $scope.classe != -1 ) {
 				  data = _.chain(data)
 				      .map( function( enseignant ) {
@@ -45,12 +110,13 @@ angular.module('cahierDeTexteApp')
 						   })
 						 };
 				      })
-				      .reject( function( enseignant ) {
+				      .reject( function( enseignant ) { // TODO: chercher une meilleure solution
 					  return enseignant.classes.length === 0;
 				      })
 				      .value();
 			      }
 
+			      // filtrage sur le mois sélectionné
 			      if ( $scope.mois != -1 ) {
 				  data = _(data).map( function( enseignant ) {
 				      return { enseignant_id: enseignant.enseignant_id,
@@ -65,51 +131,14 @@ angular.module('cahierDeTexteApp')
 				  });
 			      }
 
-			      $scope.enseignants = _(data).map( function( enseignant ) {
-				  var stats = _(enseignant.classes).reduce( function( totaux, classe ) {
-				      var stats = _(classe.statistiques).reduce( function( totaux, mois ) {
-					  return { filled: totaux.filled + mois.filled,
-						   validated: totaux.validated + mois.validated};
-				      }, { filled: 0, validated: 0});
-				      
-				      return { filled: totaux.filled + stats.filled,
-					       validated: totaux.validated + stats.validated};
-				  }, { filled: 0, validated: 0});
-
-				  $scope.radar.data.labels.push( enseignant.enseignant_id );
-				  $scope.radar.data.datasets[0].data.push( stats.filled );
-				  $scope.radar.data.datasets[1].data.push( stats.validated );
-
-				  return { id: enseignant.enseignant_id,
-					   name: enseignant.enseignant_id,
-					   discipline: '<void>',
-					   stats: stats.validated + '/' + stats.filled };
-			      });
+			      $scope.radar.populate( data );
+			      $scope.grid.populate( data );
 			  }
 		      };
-
-		      // Initialisation
-		      $scope.gridEnseignants = {
-			  data: 'enseignants',
-			  enableCellEdit: true,
-			  plugins: [new ngGridFlexibleHeightPlugin()],
-			  columnDefs: [
-			      { field: 'name', displayName: 'Nom',
-				cellTemplate: '<div><a href="#/principal/enseignant/{{row.getProperty(\'id\')}}">{{row.entity[col.field]}}</a></div>' },
-			      { field: 'discipline', displayName: 'Discipline' },
-			      { field: 'stats', displayName: 'Statistiques' }]
-		      };
-		      $scope.annee = [];
-
-		      $scope.enseignant = -1;
 
 		      // Récupération et consommation des données
 		      APIEnseignants.query( { etablissement_id: '0134567A' },
 					    function success( response ) {
-						$scope.raw_data = response;
-						$scope.process_data();
-					    },
-					    function error(  ) {
-						
+						$scope.process_data( response );
 					    } );
 		  } ] );
