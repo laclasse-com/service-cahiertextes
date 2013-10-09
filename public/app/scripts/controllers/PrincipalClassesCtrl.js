6f9e99783b11f9004aa86276f165fd585f21cdfa
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-10-09 17:10:07 +0200

    refactoring massif, séparation caulculs et consommation ; asynchronisme

--- public/app/scripts/controllers/PrincipalClassesCtrl.js
+++ public/app/scripts/controllers/PrincipalClassesCtrl.js
@@ -2,16 +2,16 @@
 
 angular.module('cahierDeTexteApp')
     .controller('PrincipalClassesCtrl',
-		[ '$scope', '$rootScope', 'APIClasse', 'APIRegroupements', 'APIMatieres',
-		  function ($scope, $rootScope, APIClasse, APIRegroupements, APIMatieres) {
+		[ '$scope', '$rootScope', '$q', 'APIClasse', 'APIRegroupements', 'APIMatieres',
+		  function ($scope, $rootScope, $q, APIClasse, APIRegroupements, APIMatieres) {
 		      $scope.uai = '0134567A';
 
 		      $scope.raw_data		=   [];
-		      $scope.data		=  [  ];
-		      $scope.classes		= [    ];
+		      $scope.displayed_data     =  [  ];
+		      $scope.classes		= {    };
 		      $scope.matieres		=   []; // bientôt Noël !
 
-		      $scope.classeCourante	= '';
+		      $scope.classe		= -1;
 		      $scope.moisCourant	= -1;
 		      $scope.matiereCourante	= -1;
 
@@ -24,13 +24,8 @@ angular.module('cahierDeTexteApp')
 						  { color : $rootScope.theme.filled.base,
 						    value: 0 } ],
 					  populate: function( data ) {
-					      $scope.global_stats = data.reduce( function( totaux, regroupement ) {
-						  return { filled: totaux.filled + regroupement.filled,
-							   validated: totaux.validated + regroupement.validated };
-					      }, { filled: 0, validated: 0 });
-
-					      $scope.pieChart.data[0].value = $scope.global_stats.validated;
-					      $scope.pieChart.data[1].value = $scope.global_stats.filled - $scope.global_stats.validated;
+					      $scope.pieChart.data[0].value = data.validated;
+					      $scope.pieChart.data[1].value = data.filled - data.validated;
 					  } };
 
 		      $scope.monthlyLineChart = { options: $rootScope.globalLineChartOptions,
@@ -66,100 +61,25 @@ angular.module('cahierDeTexteApp')
 
 		      $scope.individualCharts = { classes: [],
 						  populate: function( data, classes ) {
-						      // Calcul des statistiques
-						      _(data).each( function( regroupement ) {
-							  // stats mensuelles
-							  regroupement.mensuel = regroupement.matieres.reduce( function( monthly_stats, matiere ) {
-							      _(matiere.mois).each( function( mois ) {
-								  monthly_stats.filled[ mois.mois - 1 ] += mois.filled;
-								  monthly_stats.validated[ mois.mois - 1 ] += mois.validated;
-							      });
-							      return monthly_stats;
-							  }, { filled: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-							       validated:  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
-
-							  // sommes
-							  regroupement.filled = regroupement.mensuel.filled.reduce( function(total, mensuel) {
-							      return total + mensuel;
-							  }, 0);
-							  regroupement.validated = regroupement.mensuel.validated.reduce( function(total, mensuel) {
-							      return total + mensuel;
-							  }, 0);
-						      });
-
 						      $scope.individualCharts.classes = _.chain(data)
 							  .map( function( regroupement ) {
-							      var classe = _(classes).findWhere({ id: regroupement.regroupement_id });
-							      return {
-								  regroupement: classe,
-								  pieChart: { options: $rootScope.globalPieChartOptions,
-									      data: [ { color : $rootScope.theme.validated.base,
-											value: regroupement.validated },
-										      { color : $rootScope.theme.filled.base,
-											value: regroupement.filled - regroupement.validated } ] },
-								  lineChart: { options: $rootScope.globalLineChartOptions,
-									       data: { labels: $rootScope.mois,
-										       datasets: [
-											   { fillColor : $rootScope.theme.filled.base,
-											     pointColor : $rootScope.theme.filled.base,
-											     strokeColor : $rootScope.theme.filled.stroke,
-											     pointStrokeColor : $rootScope.theme.filled.stroke,
-											     data: regroupement.mensuel.filled
-											   },
-											   { fillColor : $rootScope.theme.validated.base,
-											     pointColor : $rootScope.theme.validated.base,
-											     strokeColor : $rootScope.theme.validated.stroke,
-											     pointStrokeColor : $rootScope.theme.validated.stroke,
-											     data: regroupement.mensuel.validated
-											   } ] } } };
+							      return { libelle: classes[ regroupement.regroupement_id ].libelle,
+								       pieChart: { options: $rootScope.globalPieChartOptions,
+										   data: [ { color : $rootScope.theme.validated.base,
+											     value: regroupement.validated },
+											   { color : $rootScope.theme.filled.base,
+											     value: regroupement.filled - regroupement.validated } ] } };
 							  })
-							  .reject(function( classe ) {
+							  .reject( function( classe ) {
 							      return _(classe.pieChart.data).reduce(function( useless, slice ) {
 								  return useless && slice.value === 0;
 							      }, true);
-							  })
+							  } )
 							  .value();
 						  } };
 
-		      $scope.process_data = function(  ) {
-			  $scope.data = $scope.raw_data;
-
-			  // Extraction des classes
-			  $scope.classes = _.chain($scope.data)
-			      .pluck( 'regroupement_id' )
-			      .map( function( regroupement_id ) {
-				  var regroupement = { id: regroupement_id,
-						       libelle: 'classe inconnue !' };
-				  APIRegroupements.get({ regroupement_id: regroupement_id },
-						       function success( response ) {
-							   regroupement.libelle = response.libelle_aaf;
-						       });
-				  return regroupement;
-			      })
-			      .value();
-
-			  // filtrage sur un mois
-			  if ( $scope.moisCourant != -1 ) {
-			      $scope.data = $scope.data.map( function( regroupement ) {
-				  return { regroupement_id: regroupement.regroupement_id,
-					   matieres: regroupement.matieres.map( function( matiere ) {
-					       return { matiere_id: matiere.matiere_id,
-							mois: _(matiere.mois).filter( function( mois ) {
-							    return mois.mois == $scope.moisCourant;
-							}) };
-					   }) };
-			      });
-			  }
-
-			  // Filtrage sur une classe
-			  if ( $scope.classeCourante !== '' ) {
-			      $scope.data = _($scope.data).filter( function( r ) {
-				  return r.regroupement_id == $scope.classeCourante;
-			      });
-			  }
-
-			  // Extraction des matières
-			  $scope.matieres = _.chain($scope.data)
+		      $scope.extract_matieres = function( data ) {
+			  return _.chain(data)
 			      .pluck( 'matieres' )
 			      .flatten()
 			      .pluck( 'matiere_id' )
@@ -168,33 +88,110 @@ angular.module('cahierDeTexteApp')
 				  var matiere = { id: matiere_id,
 						  libelle: 'Matière inconnue !' };
 				  APIMatieres.get({ matiere_id: matiere_id },
-						       function success( response ) {
-							   matiere.libelle = response.libelle_long;
-						       });
+						  function success( response ) {
+						      matiere.libelle = response.libelle_long;
+						  });
 				  return matiere;
 			      })
 			      .value();
+		      };
 
-			  // Filtrage sur une matière
-			  if ( $scope.matiereCourante != -1 ) {
-			      $scope.data = $scope.data.map( function( regroupement ) {
-				  var matieres = _(regroupement.matieres).filter( function( r ) {
-				      return r.matiere_id == $scope.matiereCourante;
+		      $scope.extract_classes_promises = function( data ) {
+			  return _.chain( data )
+			      .pluck( 'regroupement_id' )
+			      .map( function( regroupement_id ) {
+				  return APIRegroupements.get({ regroupement_id: regroupement_id }).$promise;
+			      })
+			      .value();
+		      };
+
+		      $scope.process_data = function(  ) {
+			  if ( $scope.raw_data.length > 0 ) {
+			      $scope.displayed_data = $scope.raw_data;
+
+			      // Filtrage par classe
+			      if ( $scope.classe != -1 ) {
+				  // .invert() suppose que les valeurs sont uniques
+				  var id = _($scope.classes).invert()[$scope.classe];
+				  $scope.displayed_data = _($scope.displayed_data).filter( function( r ) {
+				      return r.regroupement_id == id;
 				  });
+			      }
 
-				  return { regroupement_id: regroupement.regroupement_id,
-					   matieres: matieres };
+			      // Filtrage sur une matière
+			      if ( $scope.matiereCourante != -1 ) {
+				  $scope.displayed_data = $scope.displayed_data.map( function( regroupement ) {
+				      var matieres = _(regroupement.matieres).filter( function( r ) {
+					  return r.matiere_id == $scope.matiereCourante;
+				      });
+
+				      return { regroupement_id: regroupement.regroupement_id,
+					       matieres: matieres };
+				  });
+			      }
+
+			      // filtrage sur un mois
+			      if ( $scope.moisCourant != -1 ) {
+				  $scope.displayed_data = $scope.displayed_data.map( function( regroupement ) {
+				      return { regroupement_id: regroupement.regroupement_id,
+					       matieres: regroupement.matieres.map( function( matiere ) {
+						   return { matiere_id: matiere.matiere_id,
+							    mois: _(matiere.mois).filter( function( mois ) {
+								return mois.mois == $scope.moisCourant;
+							    }) };
+					       }) };
+				  });
+			      }
+
+			      // Calcul des statistiques mensuelles et par regroupement
+			      _($scope.displayed_data).each( function( regroupement ) {
+				  // stats mensuelles
+				  regroupement.mensuel = regroupement.matieres.reduce( function( monthly_stats, matiere ) {
+				      _(matiere.mois).each( function( mois ) {
+					  monthly_stats.filled[ mois.mois - 1 ] += mois.filled;
+					  monthly_stats.validated[ mois.mois - 1 ] += mois.validated;
+				      });
+				      return monthly_stats;
+				  }, { filled: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+				       validated:  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0] });
+
+				  // sommes
+				  regroupement.filled = regroupement.mensuel.filled.reduce( function(total, mensuel) {
+				      return total + mensuel;
+				  }, 0);
+				  regroupement.validated = regroupement.mensuel.validated.reduce( function(total, mensuel) {
+				      return total + mensuel;
+				  }, 0);
 			      });
-			  }
 
-			  $scope.individualCharts.populate( $scope.data, $scope.classes );
-			  $scope.monthlyLineChart.populate( $scope.data );
-			  $scope.pieChart.populate( $scope.data );
+			      var global_stats = $scope.displayed_data.reduce( function( totaux, regroupement ) {
+				  return { filled: totaux.filled + regroupement.filled,
+					   validated: totaux.validated + regroupement.validated };
+			      }, { filled: 0, validated: 0 });
+
+			      $scope.displayed_data.filled = global_stats.filled;
+			      $scope.displayed_data.validated = global_stats.validated;
+
+			      $scope.individualCharts.populate( $scope.displayed_data, $scope.classes );
+			      $scope.pieChart.populate( $scope.displayed_data );
+			      $scope.monthlyLineChart.populate( $scope.displayed_data );
+			  }
 		      };
 
 		      APIClasse.query( { uai: $scope.uai, id: '' },
 				       function( response ) {
 					   $scope.raw_data = response;
-					   $scope.process_data();
+
+					   // Extraction des matières
+					   $scope.matieres = $scope.extract_matieres( $scope.raw_data );
+
+					   // Extraction des classes
+					   $q.all( $scope.extract_classes_promises( $scope.raw_data ) )
+					       .then( function( classes ) {
+						   _(classes).each(function( classe ) {
+						       $scope.classes[classe.id] = classe.libelle !== null ? classe.libelle : classe.libelle_aaf;
+						   });
+						   $scope.process_data();
+					       });
 				       });
 		  } ] );
