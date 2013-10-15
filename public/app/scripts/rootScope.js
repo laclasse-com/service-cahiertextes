dcd82778c1f8894a4ccf537f19a7ad037cd1d2b6
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-10-15 16:40:01 +0200

    meilleur organisation de la configuration de l'application

---
+++ public/app/scripts/rootScope.js
@@ -0,0 +1,55 @@
+'use strict';
+
+angular.module( 'cahierDeTexteApp' )
+    .run( [ '$rootScope', function ($rootScope) {
+	$rootScope.mois = [ 'Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre' ];
+	// définition des couleurs
+	$rootScope.theme = {
+	    filled: { base: '#aaffaa',
+		      stroke: '#88aa88' },
+	    validated: { base: '#00ff00',
+			 stroke: '#00aa00' },
+	    calendar: { vide: '#a0a0a0',
+			saisie: '#aaaaff',
+			devoir: '#ff6666',
+			devoir_fait: '#66cc66'}
+	};
+
+	// options des calendriers
+	$rootScope.calendar = {	cours_max_length: 16,
+				devoir_max_length: 16,
+				couleurs: {
+				    cours: '#0000ff',
+				    devoir: '#ff00ff'
+				}};
+	$rootScope.globalCalendarOptions = {
+	    timeFormat: 'H:mm{ - H:mm}',
+	    columnFormat: { month: 'ddd',
+			    week: 'ddd d/M',
+			    day: 'dddd d MMMM' },
+	    titleFormat: { month: 'MMMM yyyy',
+			   week: "d MMM[ yyyy]{ '&#8212;' d [ MMM] yyyy}",
+			   day: 'dddd d MMMM yyyy' },
+	    monthNames: $rootScope.mois,
+	    monthNamesShort: [ 'Jan.', 'Fév.', 'Mar.', 'Avr.', 'Mai', 'Juin', 'Juil.', 'Août', 'Sep.', 'Oct.', 'Nov.', 'Déc.' ],
+	    dayNames: [ 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi', 'Dimanche' ],
+	    dayNamesShort: [ 'Lun.', 'Mar.', 'Mer.', 'Jeu.', 'Ven.', 'Sam.', 'Dim.' ],
+	    buttonText: { prev:     '&lsaquo;',
+			  next:     '&rsaquo;',
+			  prevYear: '&laquo;',
+			  nextYear: '&raquo;',
+			  today:    'aujourd\'hui',
+			  month:    'mois',
+			  week:     'semaine',
+			  day:      'jour' }
+	};
+
+	// options de tinyMCE
+	$rootScope.tinyMCEOptions = { menubar: false,
+				      statusbar: false };
+
+	// options des graphiques
+	$rootScope.globalPieChartOptions = { animation : false };
+	$rootScope.globalLineChartOptions = { animation : false };
+	$rootScope.globalRadarChartOptions = { animation : false };
+    } ] );
---
+++ public/app/scripts/routes.js
@@ -0,0 +1,67 @@
+'use strict';
+
+angular.module( 'cahierDeTexteApp' )
+    .config( [ '$stateProvider', '$urlRouterProvider', function ( $stateProvider, $urlRouterProvider ) {
+	$urlRouterProvider.otherwise('/');
+
+	$stateProvider
+	    .state('racine', {
+		url: '/',
+		templateUrl: 'views/main.html',
+		controller: 'MainCtrl'
+	    })
+	    .state('principal', {
+		url: '/principal',
+		templateUrl: 'views/principal/index.html',
+		controller: 'PrincipalCtrl'
+	    })
+	    .state('principal.classes', { //imbriquée sous principal
+		parent: 'principal',
+		url: '/classes',
+		views: {
+		    'titre': {
+			template: 'Validation des saisies par Classe/Groupe'
+		    },
+		    'content': {
+			templateUrl: 'views/principal/classes.html',
+			controller: 'PrincipalClassesCtrl'
+		    }
+		}
+	    })
+	    .state('principal.enseignants', { //imbriquée sous principal
+		parent: 'principal',
+		url: '/enseignants',
+		views: {
+		    'titre': {
+			template: 'Validation des saisies par Enseignants'
+		    },
+		    'content': {
+			templateUrl: 'views/principal/enseignants.html',
+			controller: 'PrincipalEnseignantsCtrl'
+		    }
+		}
+	    })
+	    .state('principal.enseignant', { //imbriquée sous principal
+		parent: 'principal',
+		url: '/enseignant/:enseignant_id',
+		views: {
+		    'titre': {
+			template: 'Validation des saisies par Enseignant'
+		    },
+		    'content': {
+			templateUrl: 'views/principal/enseignant.html',
+			controller: 'PrincipalEnseignantCtrl'
+		    }
+		}
+	    })
+	    .state('eleve', {
+		url: '/eleve',
+		templateUrl: 'views/emploi_du_temps.html',
+		controller: 'EleveCtrl'
+	    })
+	    .state('enseignant', {
+		url: '/enseignant',
+		templateUrl: 'views/emploi_du_temps.html',
+		controller: 'EnseignantCtrl'
+	    });
+    } ] );
