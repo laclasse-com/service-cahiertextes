cde61bd3423d1b569e647f42cda200bfada21a68
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-10-29 11:53:40 +0100

    fix marquage fait

--- public/app/scripts/controllers/EleveDevoirsCtrl.js
+++ public/app/scripts/controllers/EleveDevoirsCtrl.js
@@ -4,18 +4,20 @@ angular.module('cahierDeTexteApp')
     .controller('EleveDevoirsCtrl',
 		[ '$scope', 'Devoirs', 'TypesDeDevoir',
 		  function( $scope, Devoirs, TypesDeDevoir ) {
+		      var eleve_id = 1;
 		      $scope.fait = function( id ) {
-			  Devoirs.fait({ id: id },
-				       function() {
-					   _($scope.devoirs).where({ id: id }).fait = true;
-				       });
+			  Devoirs.fait({ id: id }).$promise
+			      .then( function() {
+				  _($scope.devoirs).findWhere({ id: id }).fait = true;
+			      });
 		      };
 
 		      $scope.types_de_devoir = TypesDeDevoir.query();
-		      Devoirs.query({ eleve_id: 1 },
+		      Devoirs.query({ eleve_id: eleve_id },
 				    function( response ) {
 					$scope.devoirs = _(response).map( function( devoir ) {
-					    devoir.type_devoir = _($scope.types_de_devoir).findWhere({id: devoir.type_devoir_id}).label;
+					    devoir.type_devoir = _($scope.types_de_devoir)
+						.findWhere({id: devoir.type_devoir_id}).label;
 					    return devoir;
 					});
 				    });
