7cc0eefe1a4683aa3b90b8cf22329cb378471708
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-11-19 17:51:36 +0100

    le client n'interroge plus l'annuaire directement mais passe par le relais de l'application serveur

---
+++ api/annuaire.rb
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+require_relative '../lib/annuaire'
+
+module CahierDeTextesAPI
+  # API d'interfaçage avec l'annuaire
+  class AnnuaireAPI < Grape::API
+    format :json
+
+    desc ''
+    params {
+      requires :id, desc: 'id de la matière'
+    }
+    get '/matieres/:id' do
+      Annuaire.get_matiere( params[:id] )
+    end
+
+    desc ''
+    params {
+      requires :id, desc: 'id du regroupement'
+    }
+    get '/regroupements/:id' do
+      Annuaire.get_regroupement( params[:id] )
+    end
+
+    desc ''
+    params {
+      requires :id, desc: 'id de l\'utilisateur'
+    }
+    get '/users/:id' do
+      Annuaire.get_user( params[:id] )
+    end
+
+  end
+end
--- public/app/scripts/services/API/Annuaire.js
+++ public/app/scripts/services/API/Annuaire.js
@@ -4,7 +4,7 @@ angular.module('cahierDeTexteApp')
   .factory('Matieres',
 	   [ '$resource',
 	     function( $resource ) {
-		 return $resource( 'http://www.dev.laclasse.com/api/app/matieres/:matiere_id',
+		 return $resource( '/api/v0/annuaire/matieres/:matiere_id',
 				   { matiere_id: '@matiere_id' } );
 	     } ] );
 
@@ -12,7 +12,7 @@ angular.module('cahierDeTexteApp')
   .factory('Regroupements',
 	   [ '$resource',
 	     function( $resource ) {
-		 return $resource( 'http://www.dev.laclasse.com/api/app/regroupements/:regroupement_id',
+		 return $resource( '/api/v0/annuaire/regroupements/:regroupement_id',
 				   { regroupement_id: '@regroupement_id' } );
 	     } ] );
 
@@ -20,6 +20,6 @@ angular.module('cahierDeTexteApp')
   .factory('Users',
 	   [ '$resource',
 	     function( $resource ) {
-		 return $resource( 'http://www.dev.laclasse.com/api/app/users/:user_id?expand=true',
+		 return $resource( '/api/v0/annuaire/users/:user_id',
 				   { user_id: '@user_id' } );
 	     } ] );
