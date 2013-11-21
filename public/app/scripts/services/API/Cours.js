6ad14f4f27cd1e04e44baaa77b3391dabc8219c8
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-11-21 11:40:43 +0100

    préparation de la duplication de cours

--- public/app/scripts/services/API/Cours.js
+++ public/app/scripts/services/API/Cours.js
@@ -4,8 +4,12 @@ angular.module('cahierDeTexteApp')
     .factory('Cours',
 	     [ '$resource', function($resource) {
 		 return $resource( '/api/v0/cours/:id',
-				   { id: '@id' },
+				   { id: '@id',
+				     regroupement_id: '@regroupement_id',
+				     creneau_emploi_du_temps_id: '@creneau_emploi_du_temps_id' },
 				   { update: { method: 'PUT' },
 				     valide: { method: 'PUT',
-					       url: '/api/v0/cours/:id/valide' } } );
+					       url: '/api/v0/cours/:id/valide' },
+				     copie: { method: 'PUT',
+					      url: '/api/v0/cours/:id/copie/regroupement/:regroupement_id/creneau_emploi_du_temps/:creneau_emploi_du_temps_id' } } );
 	     } ] );
