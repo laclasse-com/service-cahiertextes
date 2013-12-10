12ce8d91e1626ce7dbfe701ca454f653cc6d3a17
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-12-10 11:30:28 +0100

    fausse APIKey pour de rire

--- config/CASLaclasseCom.rb
+++ config/CASLaclasseCom.rb
@@ -9,5 +9,5 @@ module CASLaclasseCom
               login_url: '/sso-mysql/login',
               service_validate_url: '/sso-mysql/serviceValidate',
               logout_url: '/sso-mysql/logout' }
-  APIKEY = ''
+  APIKEY = 'f3f5efb72a0661e19564bacfa3dcc908b1712133637989a0825748ab9938c243'
 end
