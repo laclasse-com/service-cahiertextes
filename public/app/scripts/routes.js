c4a78209acf8d84cfa2a84018c7f066a3cf604f0
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-12-10 11:30:03 +0100

    indentation

--- public/app/scripts/routes.js
+++ public/app/scripts/routes.js
@@ -2,92 +2,93 @@
 
 angular.module( 'cahierDeTexteApp' )
     .config( [ '$stateProvider', '$urlRouterProvider', function ( $stateProvider, $urlRouterProvider ) {
-	$urlRouterProvider.otherwise('/');
+        $urlRouterProvider.otherwise('/');
 
-	$stateProvider
-	    .state('racine', {
-		url: '/',
-		templateUrl: 'views/main.html',
-		controller: 'MainCtrl'
-	    })
+        $stateProvider
+            .state('racine', {
+                url: '/',
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
 
-	// Principal ///////////////////////////////////////////////////////////
-	    .state('principal', {
-		url: '/principal',
-		templateUrl: 'views/principal/index.html',
-		controller: 'PrincipalCtrl'
-	    })
-	    .state('principal.classes', { //imbriquée sous principal
-		parent: 'principal',
-		url: '/classes',
-		views: {
-		    'titre': {
-			template: 'Validation des saisies par Classe/Groupe'
-		    },
-		    'content': {
-			templateUrl: 'views/principal/classes.html',
-			controller: 'PrincipalClassesCtrl'
-		    }
-		}
-	    })
-	    .state('principal.enseignants', { //imbriquée sous principal
-		parent: 'principal',
-		url: '/enseignants',
-		views: {
-		    'titre': {
-			template: 'Validation des saisies par Enseignants'
-		    },
-		    'content': {
-			templateUrl: 'views/principal/enseignants.html',
-			controller: 'PrincipalEnseignantsCtrl'
-		    }
-		}
-	    })
-	    .state('principal.enseignant', { //imbriquée sous principal
-		parent: 'principal',
-		url: '/enseignant/:enseignant_id',
-		views: {
-		    'titre': {
-			template: 'Validation des saisies par Enseignant'
-		    },
-		    'content': {
-			templateUrl: 'views/principal/enseignant.html',
-			controller: 'PrincipalEnseignantCtrl'
-		    }
-		}
-	    })
 
-	// Élève ///////////////////////////////////////////////////////////////
-	    .state('eleve', {
-		url: '/eleve',
-		templateUrl: 'views/eleve/index.html',
-		controller: 'EleveCtrl'
-	    })
-	    .state('eleve.emploi_du_temps', {
-		parent: 'eleve',
-		url: '/emploi_du_temps',
-		views: {
-		    'content': {
-			templateUrl: 'views/eleve/emploi_du_temps.html',
-			controller: 'EleveEmploiDuTempsCtrl'
-		    }
-		}
-	    })
-	    .state('eleve.devoirs', {
-		parent: 'eleve',
-		url: '/devoirs',
-		views: {
-		    'content': {
-			templateUrl: 'views/eleve/devoirs.html',
-			controller: 'EleveDevoirsCtrl'
-		    }
-		}
-	    })
+        // Principal ///////////////////////////////////////////////////////////
+            .state('principal', {
+                url: '/principal',
+                templateUrl: 'views/principal/index.html',
+                controller: 'PrincipalCtrl'
+            })
+            .state('principal.classes', { //imbriquée sous principal
+                parent: 'principal',
+                url: '/classes',
+                views: {
+                    'titre': {
+                        template: 'Validation des saisies par Classe/Groupe'
+                    },
+                    'content': {
+                        templateUrl: 'views/principal/classes.html',
+                        controller: 'PrincipalClassesCtrl'
+                    }
+                }
+            })
+            .state('principal.enseignants', { //imbriquée sous principal
+                parent: 'principal',
+                url: '/enseignants',
+                views: {
+                    'titre': {
+                        template: 'Validation des saisies par Enseignants'
+                    },
+                    'content': {
+                        templateUrl: 'views/principal/enseignants.html',
+                        controller: 'PrincipalEnseignantsCtrl'
+                    }
+                }
+            })
+            .state('principal.enseignant', { //imbriquée sous principal
+                parent: 'principal',
+                url: '/enseignant/:enseignant_id',
+                views: {
+                    'titre': {
+                        template: 'Validation des saisies par Enseignant'
+                    },
+                    'content': {
+                        templateUrl: 'views/principal/enseignant.html',
+                        controller: 'PrincipalEnseignantCtrl'
+                    }
+                }
+            })
 
-	// Enseignant //////////////////////////////////////////////////////////
-	    .state('enseignant', {
-		url: '/enseignant',
-		templateUrl: 'views/enseignant.html',
-		controller: 'EnseignantCtrl'
-	    });
+        // Élève ///////////////////////////////////////////////////////////////
+            .state('eleve', {
+                url: '/eleve',
+                templateUrl: 'views/eleve/index.html',
+                controller: 'EleveCtrl'
+            })
+            .state('eleve.emploi_du_temps', {
+                parent: 'eleve',
+                url: '/emploi_du_temps',
+                views: {
+                    'content': {
+                        templateUrl: 'views/eleve/emploi_du_temps.html',
+                        controller: 'EleveEmploiDuTempsCtrl'
+                    }
+                }
+            })
+            .state('eleve.devoirs', {
+                parent: 'eleve',
+                url: '/devoirs',
+                views: {
+                    'content': {
+                        templateUrl: 'views/eleve/devoirs.html',
+                        controller: 'EleveDevoirsCtrl'
+                    }
+                }
+            })
+
+        // Enseignant //////////////////////////////////////////////////////////
+            .state('enseignant', {
+                url: '/enseignant',
+                templateUrl: 'views/enseignant.html',
+                controller: 'EnseignantCtrl'
+            });
     } ] );
