c272c3825d1e35d4e11ff4e139a59eb44dda1a94
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2013-12-16 11:15:39 +0100

    rends le client conscient de l'utilisateur connecté

---
+++ public/app/scripts/services/API/CurrentUser.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('cahierDeTexteApp')
+    .service('CurrentUser',
+	     [ '$http', 'Users',
+	       function( $http, Users ) {
+		   this.getCurrentUser = function() {
+		       var current_user = $http.get( '/api/v0/current_user' ).success(function( response ) {
+			   Users.get({ user_id: response.info.uid }).$promise.then(function( details ) {
+			       response.details = details;
+			   });
+			   return response;
+		       });
+		       return current_user;
+		   };
+	       } ] );
