afe7a2f236bf32ec11cb55af2d2e2cdba6d6f31d
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-01-27 15:21:49 +0100

    seul un prof à le droit d'écriture sur les cours

--- lib/UserHelpers.rb
+++ lib/UserHelpers.rb
@@ -6,4 +6,14 @@ module UserHelpers
       env['rack.session'][:current_user]
    end
 
+   def user_is_prof?( uai )
+      # if user['ENTPersonProfils'].index("ENS:#{uai}")
+      # FIXME: DEBUG: remove right for DIR
+      if user['ENTPersonProfils'].index("ENS:#{uai}") || user['ENTPersonProfils'].index("DIR:#{uai}")
+         true
+      else
+         false
+      end
+   end
+
 end
