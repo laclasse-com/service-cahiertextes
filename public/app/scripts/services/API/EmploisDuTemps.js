81a2ebf21614646781559ee43520b85c4567e548
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-01-28 14:46:22 +0100

    consolide les services d'accès aux API CdT dans un fichier unique

--- public/app/scripts/services/API/EmploisDuTemps.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('cahierDeTexteApp')
-  .factory('EmploisDuTemps',
-	   [ '$resource',
-	     function($resource) {
-		 return $resource( '/api/v0/emplois_du_temps' );
-	     } ] );
--- public/app/scripts/services/API/TypesDeDevoir.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('cahierDeTexteApp')
-  .factory('TypesDeDevoir',
-	   [ '$resource',
-	     function($resource) {
-		 return $resource( '/api/v0/types_de_devoir/:id',
-				   { id: '@id' });
-	     } ] );
