90fcf1006bf7adbda01aa936b8c9531be817e283
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-01-29 17:25:31 +0100

    hardcode prefix

--- public/app/scripts/services/API/CahierDeTextes.js
+++ public/app/scripts/services/API/CahierDeTextes.js
@@ -5,7 +5,7 @@ angular.module('cahierDeTexteApp')
 	     [ '$http', 'Users',
 	       function( $http, Users ) {
 		   this.getCurrentUser = _.memoize( function() {
-		       var current_user = $http.get( '/api/v0/current_user' ).success(function( response ) {
+		       var current_user = $http.get( '/ct/api/v0/current_user' ).success(function( response ) {
 			   Users.get({ user_id: response.uid }).$promise.then(function( details ) {
 			       response.details = details;
 			   });
@@ -17,7 +17,7 @@ angular.module('cahierDeTexteApp')
 
 angular.module('cahierDeTexteApp')
   .factory('Classes', [ '$resource', function($resource) {
-      return $resource( '/api/v0/etablissements/:uai/classes/:id',
+      return $resource( '/ct/api/v0/etablissements/:uai/classes/:id',
 			{ uai: '@uai',
 			  id: '@id' } );
   } ] );
@@ -25,21 +25,21 @@ angular.module('cahierDeTexteApp')
 angular.module('cahierDeTexteApp')
     .factory('Cours',
 	     [ '$resource', function($resource) {
-		 return $resource( '/api/v0/cours/:id',
+		 return $resource( '/ct/api/v0/cours/:id',
 				   { id: '@id',
 				     regroupement_id: '@regroupement_id',
 				     creneau_emploi_du_temps_id: '@creneau_emploi_du_temps_id' },
 				   { update: { method: 'PUT' },
 				     valide: { method: 'PUT',
-					       url: '/api/v0/cours/:id/valide' },
+					       url: '/ct/api/v0/cours/:id/valide' },
 				     copie: { method: 'PUT',
-					      url: '/api/v0/cours/:id/copie/regroupement/:regroupement_id/creneau_emploi_du_temps/:creneau_emploi_du_temps_id' } } );
+					      url: '/ct/api/v0/cours/:id/copie/regroupement/:regroupement_id/creneau_emploi_du_temps/:creneau_emploi_du_temps_id' } } );
 	     } ] );
 
 angular.module('cahierDeTexteApp')
     .factory('CreneauEmploiDuTemps',
 	     [ '$resource', function($resource) {
-		 return $resource( '/api/v0/creneau_emploi_du_temps/:id',
+		 return $resource( '/ct/api/v0/creneau_emploi_du_temps/:id',
 				   { id: '@id',
 				     regroupement_id: '@regroupement_id',
 				     jour_de_la_semaine: '@jour_de_la_semaine',
@@ -52,25 +52,25 @@ angular.module('cahierDeTexteApp')
 angular.module('cahierDeTexteApp')
     .factory('Devoirs',
 	     [ '$resource', function($resource) {
-		 return $resource( '/api/v0/devoirs/:id',
+		 return $resource( '/ct/api/v0/devoirs/:id',
 				   { id: '@id' },
 				   { update: { method: 'PUT' },
 				     fait: { method: 'PUT',
-					     url: '/api/v0/devoirs/:id/fait' }});
+					     url: '/ct/api/v0/devoirs/:id/fait' }});
 	     } ] );
 
 angular.module('cahierDeTexteApp')
   .factory('EmploisDuTemps',
 	   [ '$resource',
 	     function($resource) {
-		 return $resource( '/api/v0/emplois_du_temps' );
+		 return $resource( '/ct/api/v0/emplois_du_temps' );
 	     } ] );
 
 angular.module('cahierDeTexteApp')
   .factory('Enseignants',
 	   [ '$resource',
 	     function($resource) {
-		 return $resource( '/api/v0/etablissements/:etablissement_id/enseignants/:enseignant_id',
+		 return $resource( '/ct/api/v0/etablissements/:etablissement_id/enseignants/:enseignant_id',
 				   { etablissement_id: '@etablissement_id',
 				     enseignant_id: '@enseignant_id' } );
 	     } ] );
@@ -79,7 +79,7 @@ angular.module('cahierDeTexteApp')
   .factory('TypesDeDevoir',
 	   [ '$resource',
 	     function($resource) {
-		 return $resource( '/api/v0/types_de_devoir/:id',
+		 return $resource( '/ct/api/v0/types_de_devoir/:id',
 				   { id: '@id' });
 	     } ] );
 
