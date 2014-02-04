ea07bc6129ff330841d5ba0aabadbf12bdb72264
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-02-04 11:16:25 +0100

    style

--- public/app/scripts/controllers/HeaderCtrl.js
+++ public/app/scripts/controllers/HeaderCtrl.js
@@ -3,7 +3,7 @@
 angular.module('cahierDeTexteApp')
     .controller('HeaderCtrl',
 		[ '$scope', '$location', 'CurrentUser',
-		  function ($scope, $location, CurrentUser) {
+		  function ( $scope, $location, CurrentUser ) {
 		      CurrentUser.getCurrentUser().success( function( response ) {
 			  $scope.current_user = response;
 		      });
