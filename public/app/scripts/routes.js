f2dd14f2d8c48506bde9aa35a54f0b9471547807
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-02-20 14:23:12 +0100

    enlève la partie Titre

--- public/app/scripts/routes.js
+++ public/app/scripts/routes.js
@@ -22,9 +22,6 @@ angular.module( 'cahierDeTexteApp' )
 			   url: '/enseignants',
 			   data: { auth: [ 'DIR' ] },
 			   views: {
-			       'titre': {
-				   template: 'Validation des saisies par Enseignants'
-			       },
 			       'content': {
 				   templateUrl: 'app/views/principal/enseignants.html',
 				   controller: 'PrincipalEnseignantsCtrl'
@@ -36,9 +33,6 @@ angular.module( 'cahierDeTexteApp' )
 			   url: '/enseignant/:enseignant_id',
 			   data: { auth: [ 'DIR' ] },
 			   views: {
-			       'titre': {
-				   template: 'Validation des saisies par Enseignant'
-			       },
 			       'content': {
 				   templateUrl: 'app/views/principal/enseignant.html',
 				   controller: 'PrincipalEnseignantCtrl'
@@ -50,9 +44,6 @@ angular.module( 'cahierDeTexteApp' )
 			   url: '/classes',
 			   data: { auth: [ 'DIR' ] },
 			   views: {
-			       'titre': {
-				   template: 'Validation des saisies par Classe/Groupe'
-			       },
 			       'content': {
 				   templateUrl: 'app/views/principal/classes.html',
 				   controller: 'PrincipalClassesCtrl'
