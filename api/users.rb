1e165c143c79eb3dc78221573e782b61742fdd40
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-02-27 17:42:45 +0100

    récupère les infos complètes de l'annuaire

--- api/users.rb
+++ api/users.rb
@@ -6,7 +6,19 @@ module CahierDeTextesAPI
     desc 'renvoi les infos de l\'utilisateur identifié'
     get '/current' do
        utilisateur = env['rack.session'][:current_user]
-       utilisateur['classes'] = [] unless utilisateur.has_key? 'classes'
+       # utilisateur['classes'] = [] unless utilisateur.has_key? 'classes'
+       utilisateur[ 'extra' ] = Annuaire.get_user( utilisateur[ 'uid' ] )
+       utilisateur['classes'] = utilisateur[ 'extra' ][ 'classes' ]
+       utilisateur['classes'] = utilisateur['classes'].map { |classe|
+          classe['regroupement_id'] = classe['classe_id']
+
+          classe
+       }
+
+       # FIXME: DEBUG
+       p utilisateur['ENTPersonProfils']
+       utilisateur['ENTPersonProfils'] = 'ENS:0699999Z' if utilisateur['uid'] == 'VAA62559'
+       #utilisateur['classes'] = [{regroupement_id: 23}, {regroupement_id: 24} ] if utilisateur['uid'] == 'VAA62559'
 
        utilisateur
     end
