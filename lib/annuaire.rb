f884f3f360b115b42b7c94556ea55352354b80b3
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-03-11 11:12:38 +0100

    signature correcte des requêtes vers annuaire v3

--- lib/annuaire.rb
+++ lib/annuaire.rb
@@ -9,143 +9,150 @@ require_relative '../config/options'
 
 # Module d'interfaçage avec l'annuaire
 module Annuaire
-  module_function
-  
-    @coordination = nil
-    @liaison = nil
-    
-    def coordination
-      @coordination
-    end
-  
-    def liaison
-      @liaison
-    end
-  
-  # Fonction de vérification du mode d'api paramétrée dans la conf et init des paramètres
-  def init
-   @coordination = '?'
-   @liaison = '/'
-   if ANNUAIRE[:api_mode] == 'v2'
-      @coordination = "&"
-      @liaison = ""
-    end
-  end
-  
-  # Construire la chaine de paramètres à encoder
-  def build_canonical_string(args)
-    init
-    s = Hash[ args.sort ].map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( '&' )
-    coordination + "#{s};"
-  end
-  
-  # Construction de la signature 
-  def build_signature(canonical_string, ts)
-    digest = OpenSSL::Digest.new( 'sha1' )
-    digested_message = Base64.encode64( OpenSSL::HMAC.digest( digest, ANNUAIRE[:secret], canonical_string ) )
-    { app_id: ANNUAIRE[:app_id], 
-      timestamp: ts,
-      signature: digested_message }.map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( ';' ).chomp
-  end
-  
-  # Compatibilité api V2, rectification du service 
-  # qui ne doit pas être au format REST, mais au format URL
-  def compat_service (srv)
-    if ANNUAIRE[:api_mode] == 'v2'
-      srv.sub! "matieres/libelle", "&searchmatiere="
-      srv.sub! "matieres", "&mat_code="
-      srv.sub! "etablissements", "&etb_code="
-      srv.sub! "regroupements", "&grp_id="
-      srv.sub! "users", ""
-      srv.sub! "regroupement", "&searchgrp="
-      srv.sub! "/", ""
-    end
-    srv
-  end
-  
-  # construction de la requete signée
-  def sign( uri, service, args )
-    init
-    timestamp = Time.now.getutc.strftime('%FT%T')
-    canonical_string = build_canonical_string args    
-    query = args.map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( '&' )
-    signature = build_signature(canonical_string, timestamp)
+   module_function
 
-    # Compatibilité avec les api laclasse v2 (pl/sql): pas de mode REST, en fait.
-    service = compat_service service
-    # Fin patch compat.
-puts "#{uri}"+liaison+"#{service}"+coordination+"#{query};#{signature}"
-    "#{uri}"+liaison+"#{service}"+coordination+"#{query};#{signature}"
-  end
+   @coordination = nil
+   @liaison = nil
 
-  # Envoie de la requete à l'annuaire
-  def send_request (service, param, expandvalue, error_msg)
-    RestClient.get( sign( ANNUAIRE[:url], "#{service}/#{param}", { expand: expandvalue } ) ) do
-      |response, request, result|
-      if response.code == 200
-        return JSON.parse( response )
-      else
-        STDERR.puts "#{error_msg} : #{param}"
+   # Fonction de vérification du mode d'api paramétrée dans la conf et init des paramètres
+   def init
+      @coordination = '?'
+      @liaison = '/'
+      if ANNUAIRE[:api_mode] == 'v2'
+         @coordination = '&'
+         @liaison = ''
       end
-    end
    end
 
-  # service de recherche d'une matière
-  def search_matiere( label )
-    parsed_response = send_request "matieres/libelle", CGI.escape( label ), "false", "Matière inconnue"
-  end
+   # Construire la chaine de paramètres à encoder
+   def build_canonical_string(args)
+      init
+      s = Hash[ args.sort ].map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( '&' )
 
-  # Service de recherche d'une classe ou d'un groupe d'élèves
-  def search_regroupement( code_uai, nom )
-    code_uai = CGI.escape( code_uai )
-    nom = CGI.escape( nom )
+      "#{@coordination}#{s};"
+   end
 
-    RestClient.get( sign( ANNUAIRE[:url], 'regroupement', { etablissement: code_uai, nom: nom }) ) do
-      |response, request, result|
-      if response.code == 200
-        return JSON.parse( response )[0]
-      else
-        STDERR.puts "Regroupement inconnu : #{nom}"
-        return { 'id' => rand( 1 .. 59 ) } # nil }
+   # Construction de la signature
+   def build_signature(canonical_string, ts)
+      digest = OpenSSL::Digest.new( 'sha1' )
+      digested_message = Base64.encode64( OpenSSL::HMAC.digest( digest, ANNUAIRE[:secret], canonical_string ) )
+      { app_id: ANNUAIRE[:app_id],
+            timestamp: ts,
+            signature: digested_message }.map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( ';' ).chomp
+   end
+
+   # Compatibilité api V2, rectification du service
+   # qui ne doit pas être au format REST, mais au format URL
+   def compat_service (srv)
+      if ANNUAIRE[:api_mode] == 'v2'
+         srv.sub! 'matieres/libelle', '&searchmatiere='
+         srv.sub! 'matieres', '&mat_code='
+         srv.sub! 'etablissements', '&etb_code='
+         srv.sub! 'regroupements', '&grp_id='
+         srv.sub! 'users', ''
+         srv.sub! 'regroupement', '&searchgrp='
+         srv.sub! '/', ''
       end
-    end
-  end
+      srv
+   end
 
-  # Service de recherche d'un utilisateur
-  def search_utilisateur( code_uai, nom, prenom )
-    code_uai = CGI.escape( code_uai )
-    nom = CGI.escape( nom )
-    prenom = CGI.escape( prenom )
+   def sign( uri, service, args )
+      init
+      timestamp = Time.now.getutc.strftime('%FT%T')
+      canonical_string = ANNUAIRE[:api_mode] == 'v3' ? "#{uri}#{@liaison}#{service}" : ''
+      canonical_string += build_canonical_string( args )
+      canonical_string += "#{timestamp};#{ANNUAIRE[:app_id]}" if ANNUAIRE[:api_mode] == 'v3'
 
-    RestClient.get( sign( ANNUAIRE[:url], 'users', { nom: nom, prenom: prenom, etablissement: code_uai } ) ) do
-      |response, request, result|
-      if response.code == 200
-        return JSON.parse( response )[0]
-      else
-        STDERR.puts "Utilisateur inconnu : #{prenom} #{nom}"
-        return { 'id_ent' => 'VAA' + rand( 60_400 .. 60_500 ).to_s } # nil }
+      signature = build_signature( canonical_string, timestamp )
+
+      # Compatibilité avec les api laclasse v2 (pl/sql): pas de mode REST, en fait.
+      service = compat_service( service )
+      # Fin patch compat.
+
+      query = args.map { |key, value| "#{key}=#{CGI.escape(value)}" }.join( '&' )
+
+      "#{uri}#{@liaison}#{service}#{@coordination}#{query};#{signature}"
+   end
+
+   def send_request( service, param, expand, error_msg )
+      RestClient.get( sign( ANNUAIRE[:url], "#{service}/#{CGI.escape( param )}", { expand: expand } ) ) do
+         |response, request, result|
+         if response.code == 200
+            return JSON.parse( response )
+         else
+            STDERR.puts "#{error_msg} : #{CGI.escape( param )}"
+         end
       end
-    end
-  end
+   end
 
-  # API d'interfaçage avec l'annuaire à destination du client
-  def get_matiere( id )
-    parsed_response = send_request "matieres", CGI.escape( id ), "false", "Matière inconnue"
-  end
-  
-  # Service classes et groupes d'élèves
-  def get_regroupement( id )
-    parsed_response = send_request "regroupements", CGI.escape( id ), "false", "Regroupement inconnu"
-  end
+   def search_matiere( label )
+      label = URI.escape( label )
 
-  # Service Utilisateur : init de la session et de son environnement
-  def get_user( id )
-    parsed_response = send_request "users", CGI.escape( id ), "true", "User inconnu"
-  end
+      RestClient.get( sign( ANNUAIRE[:url], "matieres/libelle/#{label}", {}, ANNUAIRE[:secret], ANNUAIRE[:app_id] ) ) do
+         |response, request, result|
+         if response.code == 200
+            return JSON.parse( response )
+         else
+            STDERR.puts "Matière inconnue : #{label}"
+            STDERR.puts 'URL fautive: ' + sign( ANNUAIRE[:url], "/matieres/libelle/#{label}", {}, ANNUAIRE[:secret], ANNUAIRE[:app_id] )
+            return { 'id' => nil }
+         end
+      end
+   end
 
-  # Service etablissement
-  def get_etablissement( uai )
-    parsed_response = send_request "etablissements", CGI.escape( uai ), "true", "Etablissement inconnu"
-  end
+   def search_regroupement( code_uai, nom )
+      code_uai = URI.escape( code_uai )
+      nom = URI.escape( nom )
+
+      RestClient.get( sign( ANNUAIRE[:url], 'regroupement', { etablissement: code_uai, nom: nom }, ANNUAIRE[:secret], ANNUAIRE[:app_id] ) ) do
+         |response, request, result|
+         if response.code == 200
+            return JSON.parse( response )[0]
+         else
+            STDERR.puts "Regroupement inconnu : #{nom}"
+            return { 'id' => rand( 1 .. 59 ) } # nil }
+         end
+      end
+   end
+
+   def search_utilisateur( code_uai, nom, prenom )
+      code_uai = URI.escape( code_uai )
+      nom = URI.escape( nom )
+      prenom = URI.escape( prenom )
+
+      RestClient.get( sign( ANNUAIRE[:url], 'users', { nom: nom, prenom: prenom, etablissement: code_uai }, ANNUAIRE[:secret], ANNUAIRE[:app_id] ) ) do
+         |response, request, result|
+         if response.code == 200
+            return JSON.parse( response )[0]
+         else
+            STDERR.puts "Utilisateur inconnu : #{prenom} #{nom}"
+            return { 'id_ent' => nil }
+         end
+      end
+   end
+
+   # API d'interfaçage avec l'annuaire à destination du client
+   def get_matiere( id )
+      parsed_response = send_request 'matieres', CGI.escape( id ), 'false', 'Matière inconnue'
+      parsed_response
+   end
+
+   # Service classes et groupes d'élèves
+   def get_regroupement( id )
+      parsed_response = send_request 'regroupements', CGI.escape( id ), 'false', 'Regroupement inconnu'
+      parsed_response
+   end
+
+   # Service Utilisateur : init de la session et de son environnement
+   def get_user( id )
+      parsed_response = send_request 'users', CGI.escape( id ), 'true', 'User inconnu'
+      parsed_response
+   end
+
+   # Service etablissement
+   def get_etablissement( uai )
+      parsed_response = send_request 'etablissements', CGI.escape( uai ), 'true', 'Etablissement inconnu'
+      parsed_response
+   end
 
 end
