7f59a2bba45ffc1fe57872fc461cfafd06f774d3
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-03-12 14:58:22 +0100

    erreur si l'utilisateur n'appartient pas à l'établissement

--- api/v0/etablissements.rb
+++ api/v0/etablissements.rb
@@ -5,11 +5,19 @@ module CahierDeTextesAPI
       class EtablissementsAPI < Grape::API
          format :json
 
+         helpers do
+            def restrict_on_etablissement( uai )
+                           error!( '401 Unauthorized', 401 ) unless user.extra['profils'].map { |p| p['etablissement_code_uai'] }.sort.uniq.include?( uai )
+            end
+         end
+
          desc 'statistiques des cahiers de textes par classes/mois/matières'
          params {
             requires :uai, desc: 'Code UAI de l\'établissement'
          }
          get '/:uai/classes' do
+            restrict_on_etablissement( params[:uai] )
+
             Etablissement.where(uai: params[:uai]).first.statistiques_classes
          end
 
@@ -19,6 +27,8 @@ module CahierDeTextesAPI
             requires :classe_id, desc: 'identifiant annuaire de la classe'
          }
          get '/:uai/classes/:classe_id' do
+            restrict_on_etablissement( params[:uai] )
+
             cahier_de_textes = CahierDeTextes[ regroupement_id: params[:classe_id] ]
 
             error!( 'Classe inconnue', 404 ) if cahier_de_textes.nil?
@@ -31,6 +41,8 @@ module CahierDeTextesAPI
             requires :uai, desc: 'Code UAI de l\'établissement'
          }
          get '/:uai/enseignants' do
+            restrict_on_etablissement( params[:uai] )
+
             Etablissement.where(UAI: params[:uai]).first.statistiques_enseignants
          end
 
@@ -40,6 +52,8 @@ module CahierDeTextesAPI
             requires :enseignant_id, desc: 'identifiant annuaire de l\'enseignant'
          }
          get '/:uai/enseignants/:enseignant_id' do
+            restrict_on_etablissement( params[:uai] )
+
             Etablissement.where(uai: params[:uai]).first.saisies_enseignant( params[:enseignant_id] )
          end
 
