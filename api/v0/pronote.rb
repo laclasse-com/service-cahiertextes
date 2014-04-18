036a9dc2b6534a944e6d0d5e828a5a5976b58e15
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-04-18 12:04:39 +0200

    mise en forme

--- api/v0/pronote.rb
+++ api/v0/pronote.rb
@@ -4,25 +4,22 @@ require_relative '../../models/models'
 require_relative '../../lib/pronote'
 
 module CahierDeTextesAPI
-   module V0
-      class ProNoteAPI < Grape::API
-         format :json
+  module V0
+    class ProNoteAPI < Grape::API
+      format :json
 
-         desc 'Upload a XML file and load it in DB.'
-         post '/xml' do
-            # Consommation du fichier reçu
-            ProNote.load_xml( File.open( params[:xml_file][:tempfile] ) )
+      desc 'Upload a XML file and load it in DB.'
+      post '/xml' do
+        # Consommation du fichier reçu
+        ProNote.load_xml( File.open( params[:xml_file][:tempfile] ) )
 
-            # on retourne un log succint des infos chargées
-            {
-               filename: params[:xml_file][:filename],
-                  size: params[:xml_file][:tempfile].size,
-                  nb_salles: Salle.count,
-                  nb_plages_horaires: PlageHoraire.count,
-                  nb_creneau_emploi_du_temps: CreneauEmploiDuTemps.count,
-            }
-         end
-
+        # on retourne un log succint des infos chargées
+        { filename: params[:xml_file][:filename],
+          size: params[:xml_file][:tempfile].size,
+          nb_salles: Salle.count,
+          nb_plages_horaires: PlageHoraire.count,
+          nb_creneau_emploi_du_temps: CreneauEmploiDuTemps.count }
       end
-   end
+    end
+  end
 end
