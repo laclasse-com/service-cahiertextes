ea9e133b1bb1ebc128c71a150fb751170fee57a1
Gwenhael Le Moine <glemoine@erasme.org>
2014-04-22 17:24:27 +0200

    semi-réparation de la récupération des classes

--- api/v0/emplois_du_temps.rb
+++ api/v0/emplois_du_temps.rb
@@ -12,14 +12,16 @@ module CahierDeTextesAPI
             requires :fin, type: Date
          }
          get '/du/:debut/au/:fin' do
-            if # ( user.is?( 'ENS', user.ENTPersonStructRattachRNE ) ||
-               #     user.is?( 'ELV', user.ENTPersonStructRattachRNE ) ||
-               #     user.is?( 'DIR', user.ENTPersonStructRattachRNE ) ) &&
-              user.methods.include? :classes
+            # if # ( user.is?( 'ENS', user.ENTPersonStructRattachRNE ) ||
+            #    #     user.is?( 'ELV', user.ENTPersonStructRattachRNE ) ||
+            #    #     user.is?( 'DIR', user.ENTPersonStructRattachRNE ) ) &&
+            #   user.methods.include? :classes
+
                params[:debut] = Date.parse( params[:debut].iso8601 )
                params[:fin] = Date.parse( params[:fin].iso8601 )
 
-               regroupements_ids = user.classes.map {
+               # FIXME
+               regroupements_ids = Annuaire.get_user( user.uid )['classes'].map {
                   |classe|
                   classe['classe_id']
                }.uniq
@@ -34,7 +36,7 @@ module CahierDeTextesAPI
 
                   # 1. récupération du cahier de textes
                   data = CahierDeTextes.where( regroupement_id: creneau[ :regroupement_id ] )
-                  raise '/!\ Incohérence dans les cahiers de textes !' unless data.count == 1
+                  #raise '/!\ Incohérence dans les cahiers de textes !' unless data.count == 1
                   cahier_de_textes = data.first
 
                   ( params[:debut] .. params[:fin] ).reject {
@@ -53,16 +55,18 @@ module CahierDeTextesAPI
                         cours = le_cours.to_hash
                         cours[:ressources] = le_cours.ressources
 
-                        devoirs = Devoir.where( cours_id: cours[:id] ).all.map { |devoir|
-                           hstart = PlageHoraire[ CreneauEmploiDuTemps[ devoir.creneau_emploi_du_temps_id ].debut ].debut
-                           hend = PlageHoraire[ CreneauEmploiDuTemps[ devoir.creneau_emploi_du_temps_id ].fin ].fin
-                           d = devoir.to_hash
-                           d[:ressources] = devoir.ressources
-                           d[:fait] = devoir.fait_par?( user.uid )
-                           d[:start] = Time.new( devoir.date_due.year, devoir.date_due.month, devoir.date_due.mday, hstart.hour, hstart.min ).iso8601
-                           d[:end] = Time.new( devoir.date_due.year, devoir.date_due.month, devoir.date_due.mday, hend.hour, hend.min ).iso8601
+                        devoirs = Devoir.where( cours_id: cours[:id] ).all.map {
+                          |devoir|
+                          hstart = PlageHoraire[ CreneauEmploiDuTemps[ devoir.creneau_emploi_du_temps_id ].debut ].debut
+                          hend = PlageHoraire[ CreneauEmploiDuTemps[ devoir.creneau_emploi_du_temps_id ].fin ].fin
+                          d = devoir.to_hash
+                          d[:ressources] = devoir.ressources
+                          d[:fait] = devoir.fait_par?( user.uid )
+                          d[:start] = Time.new( devoir.date_due.year, devoir.date_due.month, devoir.date_due.mday, hstart.hour, hstart.min ).iso8601
+                          d[:end] = Time.new( devoir.date_due.year, devoir.date_due.month, devoir.date_due.mday, hend.hour, hend.min ).iso8601
 
-                           d }
+                          d
+                        }
                      }
 
                      {
@@ -77,9 +81,9 @@ module CahierDeTextesAPI
                      }
                   }
                }.flatten
-            else
-               []
-            end
+            # else
+            #    []
+            # end
          end
 
       end
