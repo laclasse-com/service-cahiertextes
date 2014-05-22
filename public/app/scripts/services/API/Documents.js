f717bf61ece92d98c7f057789d9c48fa83dffdd4
Gwenhael Le Moine <glemoine@erasme.org>
2014-05-22 12:29:33 +0200

    factorise l'url des Documents dans une constante

--- public/app/scripts/services/API/Documents.js
+++ public/app/scripts/services/API/Documents.js
@@ -2,11 +2,11 @@
 
 angular.module('cahierDeTexteApp')
     .service('Documents',
-	     [ '$http',
-	       function( $http ) {
+	     [ '$http', 'DOCS_URL',
+	       function( $http, DOCS_URL ) {
 		   this.list_files = function( root ) {
 		       root = typeof root === 'undefined' ? '&init=1' : root;
-		       return $http.get( "http://www.dev.laclasse.com/docs-beta/api/connector?cmd=open&target=" + root );
+		       return $http.get( DOCS_URL + "/api/connector?cmd=open&target=" + root );
 		   };
 	       }
 	     ] );
