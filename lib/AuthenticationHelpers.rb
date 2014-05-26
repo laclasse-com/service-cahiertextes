83a2c6f294ed32c20574e6f7b69f556d600b0bdc
Pierre-Gilles Levallois <pgl@erasme.org>
2014-05-26 13:45:26 +0200

    change module 'CASLaclasseCom en CASAuth

--- lib/AuthenticationHelpers.rb
+++ lib/AuthenticationHelpers.rb
@@ -26,8 +26,8 @@ module AuthenticationHelpers
     env['rack.session'][:authenticated] = false
     env['rack.session'][:current_user] = nil
 
-    CASLaclasseCom::OPTIONS[:ssl] ? protocol = 'https://' : protocol = 'http://'
-    redirect protocol + CASLaclasseCom::OPTIONS[:host] + CASLaclasseCom::OPTIONS[:logout_url] + '?url=' + URI.encode( url )
+    CASAuth::OPTIONS[:ssl] ? protocol = 'https://' : protocol = 'http://'
+    redirect protocol + CASAuth::OPTIONS[:host] + CASAuth::OPTIONS[:logout_url] + '?url=' + URI.encode( url )
   end
 
   def provisionning
