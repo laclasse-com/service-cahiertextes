d5cd2b3f38507b5b3364d4fea7843ee8c7fdb1b1
Gwenhael Le Moine <glemoine@erasme.org>
2014-05-27 12:17:40 +0200

    upload sans ng-file-upload

--- public/app/scripts/controllers/EnseignantCtrl.js
+++ public/app/scripts/controllers/EnseignantCtrl.js
@@ -114,16 +114,7 @@ angular.module('cahierDeTexteApp')
 							  };
 
 							  $scope.upload_and_add_ressource = function( item, fichiers ) {
-							      Documents.upload_dans_cahier_de_textes( $scope.classe.id, fichiers )
-								  .progress(function(evt) {
-								      console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
-								  }).success(function(data, status, headers, config) {
-								      // file is uploaded successfully
-								      console.log(data);
-								  }).error(function(data, status, headers, config) {
-								      console.log(status);
-								      console.log(data);
-								  });
+							      Documents.upload_dans_cahier_de_textes( $scope.classe.id, fichiers );
 							  };
 
 							  $scope.remove_ressource = function( item, hash ) {
