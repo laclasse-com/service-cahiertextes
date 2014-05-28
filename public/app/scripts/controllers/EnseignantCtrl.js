5230ab9cedec681453c994125b9dfda74d2b012a
Gwenhael Le Moine <glemoine@erasme.org>
2014-05-28 11:39:01 +0200

    ajout de ressources par upload de fichiers totalement fonctionnel

--- public/app/scripts/controllers/EnseignantCtrl.js
+++ public/app/scripts/controllers/EnseignantCtrl.js
@@ -133,14 +133,22 @@ angular.module('cahierDeTexteApp')
 							      if ( item.ressources === undefined ) {
 								  item.ressources = [];
 							      }
-							      Documents.upload_dans_cahier_de_textes( scope_popup.classe.id, fichiers )
-								  .success( function( response ) {
-								      _(response.added).each( function( doc ) {
-									  item.ressources.push( { name: doc.name,
-												  hash: doc.hash } );
-									  scope_popup.is_dirty();
+							      var responses = Documents.upload_dans_cahier_de_textes( scope_popup.classe.id, fichiers );
+							      for ( var i = 0 ; i < responses.length ; i++ ) {
+								  responses[ i ]
+								      .success( function( response ) {
+									  _(response.added).each( function( doc ) {
+									      if ( _(item.ressources).findWhere( { hash: doc.hash } ) === undefined ) {
+								  		  item.ressources.push( { name: doc.name,
+								  					  hash: doc.hash } );
+								  		  scope_popup.is_dirty();
+									      }
+									  } )
+									      } )
+								      .error( function( response ) {
+									  console.debug( response.error );
 								      } );
-								  } );
+							      }
 							  };
 
 							  scope_popup.remove_ressource = function( item, hash ) {
--- public/app/scripts/services/API/Documents.js
+++ public/app/scripts/services/API/Documents.js
@@ -2,8 +2,8 @@
 
 angular.module('cahierDeTexteApp')
     .service('Documents',
-	     [ '$http', '$upload', 'DOCS_URL',
-	       function( $http, $upload, DOCS_URL ) {
+	     [ '$http', 'DOCS_URL',
+	       function( $http, DOCS_URL ) {
 		   this.list_files = function( root ) {
 		       root = typeof root === 'undefined' ? '&init=1' : root;
 		       return $http.get( DOCS_URL + "/api/connector?cmd=open&target=" + root );
@@ -14,22 +14,23 @@ angular.module('cahierDeTexteApp')
 		   };
 
 		   this.upload_dans_cahier_de_textes = function( share_id, fichiers ) {
-		       var form_data = new FormData();
-		       form_data.append( 'upload[]', fichiers );
-		       form_data.append( 'id', 'upload' );
-		       form_data.append( 'cmd', 'upload' );
-		       form_data.append( 'current', 0 );
-		       form_data.append( 'attachment', 'CAHIERTXT' );
-		       form_data.append( 'share', share_id );
-		       // return $http.post( DOCS_URL + '/api/ctxt/add',
-		       //			  form_data,
-		       //			  { headers: {'Content-Type': undefined },
-		       //			    transformRequest: function( data ) { return data; }
-		       //			  } );
-		       return upload.upload( { url: DOCS_URL + '/api/ctxt/add',
-					       data: form_data,
-					       file: fichiers,
-					       fileFormDataName: 'upload[]' } );
+		       var responses = [];
+		       for ( var i = 0 ; i < fichiers.length ; i++ ) {
+			   var form_data = new FormData();
+			   form_data.append( 'upload[]', fichiers.item( i ) );
+			   form_data.append( 'id', 'upload' );
+			   form_data.append( 'cmd', 'upload' );
+			   form_data.append( 'current', 0 );
+			   form_data.append( 'attachment', 'CAHIERTXT' );
+			   form_data.append( 'share', share_id );
+			   responses.push( $http.post( DOCS_URL + '/api/ctxt/add',
+						       form_data,
+						       { headers: {'Content-Type': undefined },
+							 transformRequest: angular.identity }
+						     ) );
+		       }
+
+		       return responses;
 		   };
 	       }
 	     ] );
