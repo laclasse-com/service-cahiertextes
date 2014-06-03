6631619a4b94e41adce5485d49ce5d6228f60845
Gwenhael Le Moine <glemoine@erasme.org>
2014-06-03 11:44:27 +0200

    style

--- public/app/scripts/services/Redirection.js
+++ public/app/scripts/services/Redirection.js
@@ -1,36 +1,36 @@
 'use strict';
 
 angular.module('cahierDeTexteApp')
-        .service('Redirection',
-                ['$location', '$state', 'User',
-                    function($location, $state, User) {
-                        this.doorman = function(allowed_types) {
-                            User.get_user().then(function(response) {
-                                if (_(allowed_types).indexOf(response.data['profil_actif']['type']) == -1
-                                     && !response.data['profil_actif']['admin']   
-                                   ) {
-                                    // traiter le raffraichissement de l'app en fonction du changement de profil actif
-                                    var reloadStatus = true;
-                                    var stateName = '';
-                                    
-                                    switch (response.data['profil_actif']['type']) {
-                                        case 'DIR':
-                                            stateName = 'principal.enseignants';
-                                             break;
-                                        case'ENS':
-                                            stateName = 'enseignant.emploi_du_temps';
-                                            break;
-                                        case 'ELV':
-                                            stateName = 'eleve.emploi_du_temps';
-                                            break;
-                                        default:
-                                            $location.url('/logout');
-                                            $location.replace();
-                                    }
-                                   reloadStatus = ($state.current.name == 'index' || $state.current.name == stateName) ? true : false;
-                                   $state.transitionTo(stateName, $state.params, {reload: reloadStatus, inherit: false, notify: reloadStatus});
-                                }
-                            });
-                        };
-                    }
-                ]);
+    .service( 'Redirection',
+	      [ '$location', '$state', 'User',
+		function( $location, $state, User ) {
+		    this.doorman = function( allowed_types ) {
+			User.get_user().then(function( response ) {
+			    if (_(allowed_types).indexOf( response.data['profil_actif']['type'] ) == -1
+			      && !response.data['profil_actif']['admin']
+			     ) {
+				 // traiter le raffraichissement de l'app en fonction du changement de profil actif
+				 var reloadStatus = true;
+				 var stateName = '';
+
+				 switch ( response.data['profil_actif']['type'] ) {
+				 case 'DIR':
+				     stateName = 'principal.enseignants';
+				     break;
+				 case 'ENS':
+				     stateName = 'enseignant.emploi_du_temps';
+				     break;
+				 case 'ELV':
+				     stateName = 'eleve.emploi_du_temps';
+				     break;
+				 default:
+				     $location.url( '/logout' );
+				     $location.replace();
+				 }
+				 reloadStatus = ( $state.current.name == 'index' || $state.current.name == stateName );
+				 $state.transitionTo( stateName, $state.params, { reload: reloadStatus, inherit: false, notify: reloadStatus } );
+			     }
+		      });
+		  };
+	      }
+	     ]);
