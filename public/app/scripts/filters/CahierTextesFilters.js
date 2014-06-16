58f641dc9ad4e811304ae4b5210c50d32cb4b500
Gwenhael Le Moine <glemoine@erasme.org>
2014-06-16 14:23:16 +0200

    chainage des déclarations de filtres

--- public/app/scripts/filters/CahierTextesFilters.js
+++ public/app/scripts/filters/CahierTextesFilters.js
@@ -1,22 +1,23 @@
-/* 
+/*
  * Filtres Angulars spécifiques à l'application Cahier de Textes
  */
 
 // Tenir compte de la timezone dans les dates
-angular.module('cahierDeTexteApp').filter('correctTimeZone', function() {
-  return function(d) {
-    var timezoneOffset = new Date(d).getTimezoneOffset() * 60000;
-    return new Date(new Date(d) - timezoneOffset);
-  };
-});
+angular.module( 'cahierDeTexteApp' )
+    .filter( 'correctTimeZone', function () {
+	return function ( d ) {
+	    var timezoneOffset = new Date( d ).getTimezoneOffset() * 60000;
+	    return new Date( new Date( d ) - timezoneOffset );
+	};
+    } )
+    .filter( 'formateCreneau', [ '$filter',
+	function ( $filter ) {
+	    // Afficher un créneau : date + heure de début et heure de fin
+	    // ex : "Jeu 19 juin 15:00 - 16:00"
 
-// Afficher un créneau : date + heure de début et heure de fin
-// ex : "Jeu 19 juin 15:00 - 16:00"
-angular.module('cahierDeTexteApp').filter('formateCreneau', ['$filter', function($filter) {
-  return function(creneau) {
-    return $filter('date')(creneau.start, 'EEE dd MMM HH:mm') + ' - ' +$filter('date')(creneau.end, 'shortTime');
-  };
-
+	    return function ( creneau ) {
+		return $filter( 'date' )( creneau.start, 'EEE dd MMM HH:mm' ) + ' - ' + $filter( 'date' )( creneau.end, 'shortTime' );
+	    };
 	}
     ] )
     .filter( 'cleverTruncate', function () {
