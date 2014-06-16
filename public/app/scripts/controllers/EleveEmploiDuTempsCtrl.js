42acfc43c3b053d2bc78e3c26d42f741a368e0c0
Gwenhael Le Moine <glemoine@erasme.org>
2014-06-16 11:19:30 +0200

    désimbriquation des functions

--- public/app/scripts/controllers/EleveEmploiDuTempsCtrl.js
+++ public/app/scripts/controllers/EleveEmploiDuTempsCtrl.js
@@ -22,64 +22,65 @@ angular.module('cahierDeTexteApp')
 		      };
 
 		      // consommation des données
+		      var CalendarEvent = function( event, item ) {
+			  var fc_event = this; //pour pouvoir le référencé dans les .then()
+			  this.details = { cours: event.cours,
+					   devoirs: event.devoirs };
+			  this.allDay = false;
+			  this.title = '';
+			  this.description = '';
+			  this.color = '';
+			  this.type = ( _(item).has( 'fait' ) ) ? 'devoir': 'cours';
+			  if ( this.type === 'cours' ) {
+			      item.start = event.start;
+			      item.end = event.end;
+			      this.className = 'saisie-invalide';
+			      if ( event.matiere_id.length > 0 ) {
+				  Annuaire.get_matiere( event.matiere_id ).$promise.then( function success( response ) {
+				      fc_event.title = response.libelle_long;
+				  });
+			      }
+			  } else {
+			      this.className = item.fait ? 'saisie-devoirs-fait' : 'saisie-devoirs';
+
+			      API.get_type_de_devoir( { id: item.type_devoir_id } )
+				  .$promise.then( function success( response ) {
+				      fc_event.title = response.label;
+				  });
+			  }
+			  this.start = new Date( item.start );
+			  this.end = new Date( item.end );
+
+			  if ( _(item).has( 'contenu' ) && item.contenu.length > 0 ) {
+			      this.description += '<br><span style="color:' + CALENDAR_PARAMS.couleurs[ this.type ] + '">';
+			      this.description += item.contenu.substring( 0, CALENDAR_PARAMS.max_length );
+			      this.description += item.contenu.length > CALENDAR_PARAMS.max_length ? '…' : '';
+			      this.description += '</span>';
+			      this.className += ' clickable-event';
+			      this.id = item.id;
+			  } else {
+			      this.className = 'saisie-vide un-clickable-event';
+			  }
+
+		      };
+
+		      var fullCalendarize_event = function( item_emploi_du_temps ) {
+			  var events = [];
+
+			  // traitement du cours
+			  events.push( new CalendarEvent( item_emploi_du_temps, item_emploi_du_temps.cours ) );
+
+			  // traitement des devoirs
+			  _(item_emploi_du_temps.devoirs).each( function( devoir ) {
+			      events.push( new CalendarEvent( item_emploi_du_temps, devoir ) );
+			  });
+
+			  return events;
+		      };
+
 		      var retrieve_data = function( from_date, to_date ) {
 			  User.get_user().then( function( response ) {
 			      $scope.current_user = response.data;
-
-			      var fullCalendarize_event = function( item_emploi_du_temps ) {
-				  var Calendar_event = function( event, item ) {
-				      var fc_event = this; //pour pouvoir le référencé dans les .then()
-				      this.details = { cours: event.cours,
-						       devoirs: event.devoirs };
-				      this.allDay = false;
-				      this.title = '';
-				      this.description = '';
-				      this.color = '';
-				      this.type = ( _(item).has( 'fait' ) ) ? 'devoir': 'cours';
-				      if ( this.type === 'cours' ) {
-					  item.start = event.start;
-					  item.end = event.end;
-					  this.className = 'saisie-invalide';
-					  if ( event.matiere_id.length > 0 ) {
-					      Annuaire.get_matiere( event.matiere_id ).$promise.then( function success( response ) {
-						  fc_event.title = response.libelle_long;
-					      });
-					  }
-				      } else {
-					  this.className = item.fait ? 'saisie-devoirs-fait' : 'saisie-devoirs';
-
-					  API.get_type_de_devoir( { id: item.type_devoir_id } )
-					      .$promise.then( function success( response ) {
-						  fc_event.title = response.label;
-					      });
-				      }
-				      this.start = new Date( item.start );
-				      this.end = new Date( item.end );
-
-				      if ( _(item).has( 'contenu' ) && item.contenu.length > 0 ) {
-					  this.description += '<br><span style="color:' + CALENDAR_PARAMS.couleurs[ this.type ] + '">';
-					  this.description += item.contenu.substring( 0, CALENDAR_PARAMS.max_length );
-					  this.description += item.contenu.length > CALENDAR_PARAMS.max_length ? '…' : '';
-					  this.description += '</span>';
-					  this.className += ' clickable-event';
-					  this.id = item.id;
-				      } else {
-					  this.className = 'saisie-vide un-clickable-event';
-				      }
-
-				  };
-				  var events = [];
-
-				  // traitement du cours
-				  events.push( new Calendar_event( item_emploi_du_temps, item_emploi_du_temps.cours ) );
-
-				  // traitement des devoirs
-				  _(item_emploi_du_temps.devoirs).each( function( devoir ) {
-				      events.push( new Calendar_event( item_emploi_du_temps, devoir ) );
-				  });
-
-				  return events;
-			      };
 
 			      EmploisDuTemps.query(
 				  { debut: from_date,
