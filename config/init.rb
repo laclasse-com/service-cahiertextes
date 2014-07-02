3644ded956c20cb627a82048d9b4dba109f48784
Pierre-Gilles Levallois <pgl@erasme.org>
2014-07-02 11:00:26 +0200

    refactoring config : le serveur démarre

--- config/init.rb
+++ config/init.rb
@@ -3,6 +3,13 @@
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
+# DIR Method
+def __DIR__(*args)
+  filename = caller[0][/^(.*):/, 1]
+  dir = File.expand_path(File.dirname(filename))
+  ::File.expand_path(::File.join(dir, *args.map{|a| a.to_s}))
+end
+
 # Require settings
 puts "loading config/options"
 require __DIR__('options')
