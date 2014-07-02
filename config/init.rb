ddbcd9d0f6a4a34a3fc77ce0e9e46f9e06799952
Gwenhael Le Moine <glemoine@erasme.org>
2014-07-02 14:15:05 +0200

    require tous les gems

--- config/init.rb
+++ config/init.rb
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 
+Bundler.require( :default, ENV['RACK_ENV'].to_sym )     # require tout les gems définis dans Gemfile
+
 Encoding.default_external = Encoding::UTF_8
 Encoding.default_internal = Encoding::UTF_8
 
