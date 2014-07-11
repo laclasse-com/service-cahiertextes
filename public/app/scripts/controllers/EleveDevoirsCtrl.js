623e0ed89d9e61598234b5841ebed737f8c2b37f
Gwenhael Le Moine <glemoine@erasme.org>
2014-07-11 15:16:14 +0200

    Application du nouveau style graphique

--- public/app/scripts/controllers/EleveDevoirsCtrl.js
+++ public/app/scripts/controllers/EleveDevoirsCtrl.js
@@ -2,8 +2,8 @@
 
 angular.module('cahierDeTexteApp')
     .controller('EleveDevoirsCtrl',
-		[ '$scope', 'API', 'Devoirs',
-		  function( $scope, API, Devoirs ) {
+		[ '$scope', 'API', 'Annuaire', 'Devoirs', 'Cours', 'CreneauEmploiDuTemps',
+		  function( $scope, API, Annuaire, Devoirs, Cours, CreneauEmploiDuTemps ) {
 		      $scope.empty = false;
 
 		      $scope.affiche_faits = false;
@@ -30,7 +30,12 @@ angular.module('cahierDeTexteApp')
 				  .$promise.then(function( response ) {
 				      $scope.all_devoirs = _(response).map( function( devoir ) {
 					  devoir.type_devoir = _(types_de_devoir)
-					      .findWhere({id: devoir.type_devoir_id}).label;
+					      .findWhere({id: devoir.type_devoir_id});
+					  devoir.creneau_emploi_du_temps = CreneauEmploiDuTemps.get({ id: devoir.creneau_emploi_du_temps_id });
+					  devoir.creneau_emploi_du_temps.$promise.then( function success(  ) {
+					      devoir.matiere = Annuaire.get_matiere( devoir.creneau_emploi_du_temps.matiere_id );
+					  });
+					  devoir.cours = Cours.get({ id: devoir.cours_id });
 					  return devoir;
 				      });
 				      $scope.filtre();
