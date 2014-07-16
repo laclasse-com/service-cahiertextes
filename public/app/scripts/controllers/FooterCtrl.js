ea691bf2e473e807b0f38d3859b36b710505b0ea
Gwenhael Le Moine <glemoine@erasme.org>
2014-07-16 16:29:46 +0200

    console.debug

--- public/app/scripts/controllers/FooterCtrl.js
+++ public/app/scripts/controllers/FooterCtrl.js
@@ -4,6 +4,5 @@ angular.module('cahierDeTexteApp')
     .controller('FooterCtrl',
 		[ '$scope', 'VERSION',
 		  function ( $scope, VERSION ) {
-		      console.debug(VERSION)
 		      $scope.version = VERSION;
 		  } ] );
