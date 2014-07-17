45488a8cd145ecb61d2541fef7e958bdcb5e56da
Gwenhael Le Moine <glemoine@erasme.org>
2014-07-17 11:39:42 +0200

    indentation

--- api/v1/api.rb
+++ api/v1/api.rb
@@ -14,28 +14,28 @@ require_relative './plages_horaires'
 require_relative './cahiers_de_textes'
 
 module CahierDeTextesAPI
-   module V1
-      class API < Grape::API
-         version 'v1', using: :path, vendor: 'laclasse.com'
-         format :json
-         rescue_from :all
+  module V1
+    class API < Grape::API
+      version 'v1', using: :path, vendor: 'laclasse.com'
+      format :json
+      rescue_from :all
 
-         resource( :users                    ) { mount ::CahierDeTextesAPI::V1::UsersAPI }
+      resource( :users                    ) { mount ::CahierDeTextesAPI::V1::UsersAPI }
 
-         resource( :pronote                  ) { mount ::CahierDeTextesAPI::V1::ProNoteAPI }
-         resource( :etablissements           ) { mount ::CahierDeTextesAPI::V1::EtablissementsAPI }
-         resource( :cours                    ) { mount ::CahierDeTextesAPI::V1::CoursAPI }
-         resource( :devoirs                  ) { mount ::CahierDeTextesAPI::V1::DevoirsAPI }
-         resource( :types_de_devoir          ) { mount ::CahierDeTextesAPI::V1::TypesDeDevoirAPI }
-         resource( :emplois_du_temps         ) { mount ::CahierDeTextesAPI::V1::EmploisDuTempsAPI }
-         resource( :annuaire                 ) { mount ::CahierDeTextesAPI::V1::AnnuaireAPI }
-         resource( :creneaux_emploi_du_temps ) { mount ::CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI }
-         resource( :plages_horaires          ) { mount ::CahierDeTextesAPI::V1::PlagesHorairesAPI }
-         resource( :cahiers_de_textes        ) { mount ::CahierDeTextesAPI::V1::CahiersDeTextesAPI }
+      resource( :pronote                  ) { mount ::CahierDeTextesAPI::V1::ProNoteAPI }
+      resource( :etablissements           ) { mount ::CahierDeTextesAPI::V1::EtablissementsAPI }
+      resource( :cours                    ) { mount ::CahierDeTextesAPI::V1::CoursAPI }
+      resource( :devoirs                  ) { mount ::CahierDeTextesAPI::V1::DevoirsAPI }
+      resource( :types_de_devoir          ) { mount ::CahierDeTextesAPI::V1::TypesDeDevoirAPI }
+      resource( :emplois_du_temps         ) { mount ::CahierDeTextesAPI::V1::EmploisDuTempsAPI }
+      resource( :annuaire                 ) { mount ::CahierDeTextesAPI::V1::AnnuaireAPI }
+      resource( :creneaux_emploi_du_temps ) { mount ::CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI }
+      resource( :plages_horaires          ) { mount ::CahierDeTextesAPI::V1::PlagesHorairesAPI }
+      resource( :cahiers_de_textes        ) { mount ::CahierDeTextesAPI::V1::CahiersDeTextesAPI }
 
-         add_swagger_documentation base_path: "#{APP_PATH}/api",
-            api_version: 'v1',
-            hide_documentation_path: true
-      end
-   end
+      add_swagger_documentation base_path: "#{APP_PATH}/api",
+                                api_version: 'v1',
+                                hide_documentation_path: true
+    end
+  end
 end
