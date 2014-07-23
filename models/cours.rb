7bb844a584cfe484a4c0a559efd9c882e92c5060
Gwenhael Le Moine <glemoine@erasme.org>
2014-07-23 11:54:01 +0200

    associations entre cours et devoirs

--- models/cours.rb
+++ models/cours.rb
@@ -8,6 +8,7 @@ class Cours < Sequel::Model( :cours )
   many_to_many :ressources
   many_to_one :creneau_emploi_du_temps
   many_to_one :cahier_de_textes
+  one_to_many :devoirs
 end
 
 class CoursRessource < Sequel::Model( :cours_ressources )
