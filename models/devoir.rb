e2fdc0e35b9436e8daabbb148181b935279319cb
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-21 15:26:02 +0200

    to_deep_hash spécifique pour exclude les attributs issus de jointures

--- models/devoir.rb
+++ models/devoir.rb
@@ -1,15 +1,15 @@
 # -*- coding: utf-8 -*-
 
-require_relative './ToDeepHashMixin'
-
 class Devoir < Sequel::Model( :devoirs )
-  include ToDeepHashMixin
-
   many_to_many :ressources
   many_to_one :creneau_emploi_du_temps
   many_to_one :type_devoir
   one_to_many :devoir_todo_items
   many_to_one :cours
+
+  def to_deep_hash
+    JSON.parse( to_json( include: self.class.associations, except: [ :regroupement_id, :semaines_de_presence ] ), symbolize_names: true )
+  end
 
   def fait_par!( eleve_id )
     add_devoir_todo_item( eleve_id: eleve_id, date_fait: Time.now )
