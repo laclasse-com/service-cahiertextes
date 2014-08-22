30eac1f618f1c0a0fcea6d746f08f58c96fa7378
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-22 16:03:44 +0200

    affichage SP et devoirs associés dans les popups

--- public/app/scripts/controllers/EmploiDuTempsPopupDisplayCtrl.js
+++ public/app/scripts/controllers/EmploiDuTempsPopupDisplayCtrl.js
@@ -2,18 +2,29 @@
 
 angular.module('cahierDeTexteApp')
     .controller('EmploiDuTempsPopupDisplayCtrl',
-		[ '$scope', '$modalInstance', 'Devoirs', 'titre', 'cours', 'devoirs',
-  function( $scope, $modalInstance, Devoirs, titre, cours, devoirs ) {
-      $scope.titre = titre;
-      $scope.cours = cours;
-      $scope.devoirs = devoirs;
-      $scope.tab_SP_active = $scope.devoirs.length == 0;
+		[ '$scope', '$modalInstance', 'Cours', 'Devoirs', 'titre', 'cours', 'devoirs',
+		  function( $scope, $modalInstance, Cours, Devoirs, titre, cours, devoirs ) {
+		      $scope.titre = titre;
+		      $scope.cours = Cours.get( { id: cours.id } );
+		      $scope.devoirs = devoirs.map( function( devoir ) {
+			  return Devoirs.get( { id: devoir.id } );
+		      } );
+		      $scope.tab_SP_active = $scope.devoirs.length == 0;
 
-      $scope.fait = function( id ) {
-	  Devoirs.fait({ id: id });
-      };
+		      $scope.fait = function( id ) {
+			  Devoirs.fait({ id: id });
+		      };
 
-      $scope.fermer = function() {
-	  $modalInstance.close( $scope );
-      };
-  } ] );
+		      $scope.fermer = function() {
+			  $modalInstance.close( $scope );
+		      };
+
+		      $scope.cours.$promise.then( function( cours ) {
+			  _(cours.devoirs).each( function( devoir ) {
+			      devoir.tooltip = devoir.contenu;
+			      if ( devoir.temps_estime > 0 ) {
+				  devoir.tooltip = '<span><i class="picto temps"></i>' + devoir.temps_estime * 5 + ' minutes</span><hr>' + devoir.tooltip;
+			      }
+			  } );
+		      } );
+		  } ] );
