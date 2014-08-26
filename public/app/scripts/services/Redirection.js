31290161a0339581ffc7d40c41f8656e4fd517ec
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-26 10:36:30 +0200

    active la redirection même pour les admins

--- public/app/scripts/services/Redirection.js
+++ public/app/scripts/services/Redirection.js
@@ -7,26 +7,27 @@ angular.module('cahierDeTexteApp')
 		    this.doorman = function( allowed_types ) {
 			User.get_user().then(function( response ) {
 			    if (_(allowed_types).size() === 0 ||
-				   ( _(allowed_types).indexOf( response.data['profil_actif']['type'] ) === -1
-			      && !response.data['profil_actif']['admin'] )
-			     ) {
-				 // traiter le raffraichissement de l'app en fonction du changement de profil actif
-				 var stateName = '404';
+				( _(allowed_types).indexOf( response.data['profil_actif']['type'] ) === -1
+				  // && !response.data['profil_actif']['admin']
+				)
+			       ) {
+				   // traiter le raffraichissement de l'app en fonction du changement de profil actif
+				   var stateName = '404';
 
-				 switch ( response.data['profil_actif']['type'] ) {
-				 case 'DIR':
-				     stateName = 'principal.enseignants';
-				     break;
-				 case 'ENS':
-				     stateName = 'enseignant.emploi_du_temps';
-				     break;
-				 case 'ELV':
-				     stateName = 'eleve.emploi_du_temps';
-				     break;
-				 }
-				 $state.go( stateName, $state.params, { reload: true, inherit: true, notify: true } );
-			     }
-		      });
-		  };
-	      }
-	     ]);
+				   switch ( response.data['profil_actif']['type'] ) {
+				   case 'DIR':
+				       stateName = 'principal.enseignants';
+				       break;
+				   case 'ENS':
+				       stateName = 'enseignant.emploi_du_temps';
+				       break;
+				   case 'ELV':
+				       stateName = 'eleve.emploi_du_temps';
+				       break;
+				   }
+				   $state.go( stateName, $state.params, { reload: true, inherit: true, notify: true } );
+			       }
+			});
+		    };
+		}
+	      ]);
