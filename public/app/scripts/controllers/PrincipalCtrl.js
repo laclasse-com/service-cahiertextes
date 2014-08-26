178123e883a0f5df3e2d0dff4947bce28954bded
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-26 17:48:32 +0200

    tab enseignants en premier

--- public/app/scripts/controllers/PrincipalCtrl.js
+++ public/app/scripts/controllers/PrincipalCtrl.js
@@ -4,11 +4,11 @@ angular.module('cahierDeTexteApp')
     .controller('PrincipalCtrl',
 		[ '$scope', '$state',
 		  function ( $scope, $state ) {
-		      $scope.tabs = [ { heading: 'Tableaux de bord par classe',
-					uisref: 'principal.classes',
-					active: false },
-				      { heading: 'Validation des saisies par enseignant',
+		      $scope.tabs = [ { heading: 'Validation des saisies par enseignant',
 					uisref: 'principal.enseignants',
+					active: false },
+				      { heading: 'Tableaux de bord par classe',
+					uisref: 'principal.classes',
 					active: false } ];
 		      _($scope.tabs).each( function( tab ) {
 			  tab.active = tab.uisref == $state.current.name;
