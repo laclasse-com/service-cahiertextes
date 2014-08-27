c2e8937e9003372626c504d62977dcf3ea9da401
Gwenhael Le Moine <glemoine@erasme.org>
2014-08-27 12:03:31 +0200

    style

--- public/app/scripts/filters/CahierTextesFilters.js
+++ public/app/scripts/filters/CahierTextesFilters.js
@@ -5,27 +5,30 @@
 
 // Tenir compte de la timezone dans les dates
 angular.module( 'cahierDeTexteApp' )
-    .filter( 'correctTimeZone', function () {
-	return function ( d ) {
-	    var timezoneOffset = new Date( d ).getTimezoneOffset() * 60000;
-	    return new Date( new Date( d ) - timezoneOffset );
-	};
-    } )
-    .filter( 'correctTimeZoneToGMT', function () {
-	return function ( d ) {
-	    var timezoneOffset = new Date( d ).getTimezoneOffset() / 60;
+    .filter( 'correctTimeZone',
+	     function() {
+		 return function( d ) {
+		     var timezoneOffset = new Date( d ).getTimezoneOffset() * 60000;
+		     return new Date( new Date( d ) - timezoneOffset );
+		 };
+	     } )
+    .filter( 'correctTimeZoneToGMT',
+	     function() {
+		 return function( d ) {
+		     var timezoneOffset = new Date( d ).getTimezoneOffset() / 60;
 
-	    d.setHours( d.getHours() + timezoneOffset );
+		     d.setHours( d.getHours() + timezoneOffset );
 
-	    return d;
-	};
-    } )
-    .filter( 'formateCreneau', [ '$filter',
-	function ( $filter ) {
-	    // Afficher un créneau : date + heure de début et heure de fin
-	    // ex : "Jeu 19 juin 15:00 - 16:00"
-	    return function ( creneau ) {
-		return $filter( 'date' )( creneau.start, 'EEE dd MMM HH:mm' ) + ' - ' + $filter( 'date' )( creneau.end, 'shortTime' );
-	    };
-	}
-			       ] );
+		     return d;
+		 };
+	     } )
+    .filter( 'formateCreneau',
+	     [ '$filter',
+	       function( $filter ) {
+		   // Afficher un créneau : date + heure de début et heure de fin
+		   // ex : "Jeu 19 juin 15:00 - 16:00"
+		   return function( creneau ) {
+		       return $filter( 'date' )( creneau.start, 'EEE dd MMM HH:mm' ) + ' - ' + $filter( 'date' )( creneau.end, 'shortTime' );
+		   };
+	       }
+	     ] );
