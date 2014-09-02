f435513a3c3718aabb1f1a31cae98814fb9ee94b
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-02 11:51:42 +0200

    configure textAngular comme on aime

--- public/app/scripts/defaults.js
+++ public/app/scripts/defaults.js
@@ -95,4 +95,52 @@ angular.module( 'cahierDeTexteApp' )
 							yFunction: function(){ return function(d) { return d.value; }; },
 							colorFunction: CHART_COLORS_FUNCTION };
 					   };
-				       } ] );
+				       } ] )
+    .config(['$provide', function($provide){
+	// this demonstrates how to register a new tool and add it to the default toolbar
+	$provide.decorator('taOptions', ['$delegate', function(taOptions){
+	    // $delegate is the taOptions we are decorating
+	    // here we override the default toolbars and classes specified in taOptions.
+	    // taOptions.toolbar = [
+	    //     ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
+	    //     ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
+	    //     ['justifyLeft','justifyCenter','justifyRight'],
+	    //     ['html', 'insertImage', 'insertLink', 'unlink']
+	    // ];
+	    taOptions.toolbar = [
+		[ 'bold', 'italics', 'underline', 'ul', 'ol', 'quote', 'justifyLeft', 'justifyCenter', 'justifyRight', 'insertLink', 'redo', 'undo', 'clear' ]
+	    ];
+	    taOptions.classes = {
+		focussed: 'focussed',
+		toolbar: 'btn-toolbar',
+		toolbarGroup: 'btn-group',
+		toolbarButton: 'btn btn-default',
+		toolbarButtonActive: 'active',
+		disabled: 'disabled',
+		textEditor: 'form-control',
+		htmlEditor: 'form-control'
+	    };
+	    return taOptions; // whatever you return will be the taOptions
+	}]);
+	// this demonstrates changing the classes of the icons for the tools for font-awesome v3.x
+	// $provide.decorator('taTools', ['$delegate', function(taTools){
+	//     taTools.bold.iconclass = 'icon-bold';
+	//     taTools.italics.iconclass = 'icon-italic';
+	//     taTools.underline.iconclass = 'icon-underline';
+	//     taTools.ul.iconclass = 'icon-list-ul';
+	//     taTools.ol.iconclass = 'icon-list-ol';
+	//     taTools.undo.iconclass = 'icon-undo';
+	//     taTools.redo.iconclass = 'icon-repeat';
+	//     taTools.justifyLeft.iconclass = 'icon-align-left';
+	//     taTools.justifyRight.iconclass = 'icon-align-right';
+	//     taTools.justifyCenter.iconclass = 'icon-align-center';
+	//     taTools.clear.iconclass = 'icon-ban-circle';
+	//     taTools.insertLink.iconclass = 'icon-link';
+	//     // taTools.unlink.iconclass = 'icon-link red';
+	//     taTools.insertImage.iconclass = 'icon-picture';
+	//     // there is no quote icon in old font-awesome so we change to text as follows
+	//     delete taTools.quote.iconclass;
+	//     taTools.quote.buttontext = 'quote';
+	//     return taTools;
+	// }]);
+    } ] );
