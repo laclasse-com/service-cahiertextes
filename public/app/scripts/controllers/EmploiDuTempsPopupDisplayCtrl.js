315234ab24f4f6db835be8fa3afdbf4c5d93e113
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-09 16:47:30 +0200

    URL des ressources correctes pour l'élève

--- public/app/scripts/controllers/EmploiDuTempsPopupDisplayCtrl.js
+++ public/app/scripts/controllers/EmploiDuTempsPopupDisplayCtrl.js
@@ -2,14 +2,23 @@
 
 angular.module('cahierDeTexteApp')
     .controller('EmploiDuTempsPopupDisplayCtrl',
-		[ '$scope', '$modalInstance', 'APP_PATH', 'Cours', 'Devoirs', 'titre', 'cours', 'devoirs',
-		  function( $scope, $modalInstance, APP_PATH, Cours, Devoirs, titre, cours, devoirs ) {
+		[ '$scope', '$sce', '$modalInstance', 'APP_PATH', 'DOCS_URL', 'Cours', 'Devoirs', 'titre', 'cours', 'devoirs',
+		  function( $scope, $sce, $modalInstance, APP_PATH, DOCS_URL, Cours, Devoirs, titre, cours, devoirs ) {
 		      $scope.app_path = APP_PATH;
 		      $scope.titre = titre;
 		      $scope.cours = Cours.get( { id: cours.id } );
 		      $scope.devoirs = devoirs.map( function( devoir ) {
 			  return Devoirs.get( { id: devoir.id } );
 		      } );
+		      _($scope.cours.ressources).each( function( ressource ) {
+			  ressource.url = $sce.trustAsResourceUrl( DOCS_URL + '/api/connector?cmd=file&target=' + ressource.hash );
+		      } );
+		      _($scope.devoirs).each( function( devoir ) {
+			  _(devoir.ressources).each( function( ressource ) {
+			      ressource.url = $sce.trustAsResourceUrl( DOCS_URL + '/api/connector?cmd=file&target=' + ressource.hash );
+			  } );
+		      } );
+
 		      $scope.tab_SP_active = $scope.devoirs.length == 0;
 
 		      $scope.fait = function( id ) {
