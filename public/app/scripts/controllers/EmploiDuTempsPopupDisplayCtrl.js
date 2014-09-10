6a6ec1ff40cd847115df016583fab17a34997d5f
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-10 09:57:18 +0200

    cours peut être null

--- public/app/scripts/controllers/EmploiDuTempsPopupDisplayCtrl.js
+++ public/app/scripts/controllers/EmploiDuTempsPopupDisplayCtrl.js
@@ -6,12 +6,23 @@ angular.module('cahierDeTexteApp')
 		  function( $scope, $sce, $modalInstance, APP_PATH, DOCS_URL, Cours, Devoirs, titre, cours, devoirs ) {
 		      $scope.app_path = APP_PATH;
 		      $scope.titre = titre;
-		      $scope.cours = Cours.get( { id: cours.id } );
+		      if ( ! _(cours).isNull() ) {
+			  $scope.cours = Cours.get( { id: cours.id } );
+			  _($scope.cours.ressources).each( function( ressource ) {
+			      ressource.url = $sce.trustAsResourceUrl( DOCS_URL + '/api/connector?cmd=file&target=' + ressource.hash );
+			  } );
+
+			  $scope.cours.$promise.then( function( cours ) {
+			      _(cours.devoirs).each( function( devoir ) {
+				  devoir.tooltip = devoir.contenu;
+				  if ( devoir.temps_estime > 0 ) {
+				      devoir.tooltip = '<span><i class="picto temps"></i>' + devoir.temps_estime * 5 + ' minutes</span><hr>' + devoir.tooltip;
+				  }
+			      } );
+			  } );
+		      }
 		      $scope.devoirs = devoirs.map( function( devoir ) {
 			  return Devoirs.get( { id: devoir.id } );
-		      } );
-		      _($scope.cours.ressources).each( function( ressource ) {
-			  ressource.url = $sce.trustAsResourceUrl( DOCS_URL + '/api/connector?cmd=file&target=' + ressource.hash );
 		      } );
 		      _($scope.devoirs).each( function( devoir ) {
 			  _(devoir.ressources).each( function( ressource ) {
@@ -28,13 +39,4 @@ angular.module('cahierDeTexteApp')
 		      $scope.fermer = function() {
 			  $modalInstance.close( $scope );
 		      };
-
-		      $scope.cours.$promise.then( function( cours ) {
-			  _(cours.devoirs).each( function( devoir ) {
-			      devoir.tooltip = devoir.contenu;
-			      if ( devoir.temps_estime > 0 ) {
-				  devoir.tooltip = '<span><i class="picto temps"></i>' + devoir.temps_estime * 5 + ' minutes</span><hr>' + devoir.tooltip;
-			      }
-			  } );
-		      } );
 		  } ] );
