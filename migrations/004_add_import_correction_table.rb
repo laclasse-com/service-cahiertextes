0e5df881abae85e6bec4c12771a58411118c26d2
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-11 11:16:20 +0200

    collecte des erreurs d'identification par l'annuaire

---
+++ migrations/004_add_import_correction_table.rb
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+Sequel.migration do
+  change do
+    create_table!(:failed_identifications) {
+      primary_key :id
+
+      Date :date_creation, null: false, default: Time.now
+      String :sha256, null: false, unique: true
+      String :id_annuaire, null: true, unique: false
+    }
+  end
+end
---
+++ models/failed_identification.rb
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+class FailedIdentification < Sequel::Model( :failed_identifications )
+
+end
