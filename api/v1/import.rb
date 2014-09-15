c1142600c40432e3783550bac601f4a9e304eea2
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-09-15 10:34:49 +0200

    API import accessible uniquement aux Personnels de direction

--- api/v1/import.rb
+++ api/v1/import.rb
@@ -9,6 +9,13 @@ module CahierDeTextesAPI
     class ImportAPI < Grape::API
       format :json
 
+      before do
+        # pas de gestion restriction d'accès sur les get
+        next if request.get?
+
+        error!( '401 Unauthorized', 401 ) unless user.is?( 'DIR' )
+      end
+
       desc 'Receive a Pronote XML file and load it in DB.'
       post '/pronote' do
         # on retourne un log succint des infos chargées
