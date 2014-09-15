e0a7ca23c9a585d964d0d0098ca3231935f61eeb
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2014-09-15 17:41:23 +0200

    POC reconciliation Import <-> Annuaire

--- public/app/scripts/controllers/ImportCtrl.js
+++ public/app/scripts/controllers/ImportCtrl.js
@@ -2,8 +2,8 @@
 
 angular.module('cahierDeTexteApp')
     .controller('ImportCtrl',
-		[ '$scope', '$upload', 'APP_PATH',
-		  function ( $scope, $upload, APP_PATH ) {
+		[ '$scope', '$http', '$upload', 'APP_PATH', 'Annuaire', 'User',
+		  function ( $scope, $http, $upload, APP_PATH, Annuaire, User ) {
 		      $scope.in_progress = false;
 		      $scope.result = false;
 
@@ -20,12 +20,35 @@ angular.module('cahierDeTexteApp')
 				      $scope.in_progress = true;
 				      console.log('percent: ' + parseInt(100.0 * evt.loaded / evt.total));
 				  })
+				  .error( function() {
+				      $scope.in_progress = false;
+				  })
 				  .success( function( data, status, headers, config ) {
 				      $scope.in_progress = false;
 				      $scope.result = data;
-				  })
-				  .error( function() {
-				      $scope.in_progress = false;
+
+				      $scope.identifie_objet = function( sha256, id_annuaire ) {
+					  $http.post( APP_PATH + '/api/v1/import/mrpni/' + sha256 + '/est/' + id_annuaire );
+				      };
+
+				      if ( !_($scope.result.rapport.matieres.error).isEmpty() ) {
+					  // TODO
+					  $scope.matieres = Annuaire.get_all_matieres();
+				      }
+				      if ( !_($scope.result.rapport.enseignants.error).isEmpty() ) {
+					  // TODO
+					  User.get_user().success( function ( response ) {
+					      $scope.current_user = response;
+					      $scope.enseignants = Annuaire.get_all_enseignants( { uai: $scope.current_user.profil_actif.uai } );
+					  } );
+				      }
+				      if ( !_($scope.result.rapport.regroupements.error).isEmpty() ) {
+					  // TODO
+					  User.get_user().success( function ( response ) {
+					      $scope.current_user = response;
+					      $scope.enseignants = Annuaire.get_all_regroupements( { uai: $scope.current_user.profil_actif.uai } );
+					  } );
+				      }
 				  });
 			  }
 		      };
