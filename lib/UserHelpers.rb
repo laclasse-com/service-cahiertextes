ebc0191142498d269f61e735ceca106aeb860614
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-24 17:33:56 +0200

    style

--- lib/UserHelpers.rb
+++ lib/UserHelpers.rb
@@ -6,7 +6,7 @@ module UserHelpers
       hash.each do |k, v|
         instance_variable_set( "@#{k}", v )
         # create the getter
-        self.class.send( :define_method, k, proc{ instance_variable_get( "@#{k}" ) } )
+        self.class.send( :define_method, k, proc { instance_variable_get( "@#{k}" ) } )
         # create the setter
         # self.class.send( :define_method, "#{k}=", proc{ |val| instance_variable_set( "@#{k}", val ) } )
       end
