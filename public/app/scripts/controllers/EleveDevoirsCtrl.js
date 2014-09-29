1fabf996b4cbfd6b5a578636686dbd190c861a35
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-29 17:08:02 +0200

    filtre par matière dans la liste des devoirs, #101

--- public/app/scripts/controllers/EleveDevoirsCtrl.js
+++ public/app/scripts/controllers/EleveDevoirsCtrl.js
@@ -30,21 +30,38 @@ angular.module('cahierDeTexteApp')
 						      fin: $scope.to_date,
 						      uid: $scope.current_user.profil_actif.type == 'TUT' ? $scope.current_user.enfant_actif.enfant.id_ent : null })
 				      .$promise.then(function( response ) {
-					  $scope.devoirs = _(response).map( function( devoir ) {
+					  $scope.matieres = {};
+
+					  $scope.all_devoirs = _(response).map( function( devoir ) {
 					      devoir.type_devoir = _(types_de_devoir).findWhere({id: devoir.type_devoir_id});
+
 					      CreneauEmploiDuTemps.get({ id: devoir.creneau_emploi_du_temps_id })
 						  .$promise.then( function success( response ) {
 						      devoir.creneau_emploi_du_temps = response;
-						      devoir.matiere = Annuaire.get_matiere( devoir.creneau_emploi_du_temps.matiere_id );
+						      $scope.matieres[ devoir.creneau_emploi_du_temps.matiere_id ] = Annuaire.get_matiere( devoir.creneau_emploi_du_temps.matiere_id );
+						      devoir.matiere = $scope.matieres[ devoir.creneau_emploi_du_temps.matiere_id ];
 						  });
+
 					      devoir.cours = _(devoir.cours_id).isNull() ? null : Cours.get({ id: devoir.cours_id });
+
 					      _(devoir.ressources).each( function( ressource ) {
 						  ressource.url = $sce.trustAsResourceUrl( DOCS_URL + '/api/connector?cmd=file&target=' + ressource.hash );
 					      } );
 
 					      return devoir;
 					  });
+					  $scope.devoirs = $scope.all_devoirs;
 				      });
+			      };
+
+			      $scope.filter_data = function( matiere_id ) {
+				  if ( _(matiere_id).isNull() ) {
+				      $scope.devoirs = $scope.all_devoirs;
+				  } else {
+				      $scope.devoirs = _($scope.all_devoirs).select( function( devoir ) {
+					  return devoir.creneau_emploi_du_temps.matiere_id == matiere_id;
+				      } );
+				  }
 			      };
 
 			      $scope.month_offset = 0;
