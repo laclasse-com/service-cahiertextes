a7ea9fe638e2d5f5e3ee99d628acac24666b7eb6
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-30 15:35:59 +0200

    ajout date de dernière connexion

--- migrations/005_add_user_parameters_table.rb
+++ migrations/005_add_user_parameters_table.rb
@@ -8,6 +8,7 @@ Sequel.migration do
       String :uid, null: false, unique: true
       String :parameters, null: false, default: { affichage_types_de_devoir: true,
                                                   affichage_week_ends: false }.to_json
+      DateTime :date_connexion,  null: false, default: Time.now
     }
   end
 end
