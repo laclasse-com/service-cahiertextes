e328168d69e0deca0ba5dae13a6d610e6fa49e64
Gwenhael Le Moine <glemoine@erasme.org>
2014-09-30 15:35:43 +0200

    refactoring

--- api/v1/users.rb
+++ api/v1/users.rb
@@ -3,47 +3,22 @@
 module CahierDeTextesAPI
   module V1
     class UsersAPI < Grape::API
-
       desc 'renvoi les infos de l\'utilisateur identifié'
       get '/current' do
-        utilisateur = env['rack.session'][:current_user]
+        user.full( env )
+      end
 
-        extra = Annuaire.get_user( utilisateur[ 'uid' ] )
-        utilisateur[ 'profils' ] = extra['profils'].map do |profil|
-          # renommage de champs
-          profil['type'] = profil['profil_id']
-          profil['uai'] = profil['etablissement_code_uai']
-          profil['etablissement'] = profil['etablissement_nom']
-          profil['nom'] = profil['profil_nom']
-
-          # calcule du droit d'admin, true pour les TECH et les ADM
-          profil['admin'] = extra['roles'].select { |r| r['etablissement_code_uai'] == profil['etablissement_code_uai'] && ( r['role_id'] == 'TECH' || r['role_id'].match('ADM.*') ) }.length > 0
-
-          profil
-        end
-        utilisateur[ 'enfants' ] = extra [ 'enfants' ]
-
-        regroupements_annuaire = Annuaire.get_user_regroupements( utilisateur[ 'uid' ] )
-        utilisateur[ 'classes' ] = regroupements_annuaire[ 'classes' ]
-                                   .concat( regroupements_annuaire['groupes_eleves'] )
-                                   .concat( regroupements_annuaire['groupes_libres'] )
-                                   .map do |regroupement|
-          if regroupement.key? 'groupe_id'
-            regroupement['type'] = 'groupe'
-            regroupement['classe_id'] = regroupement['groupe_id']
-            regroupement['classe_libelle'] = regroupement['groupe_libelle']
-          else
-            regroupement['type'] = 'classe'
-          end
-          regroupement
-        end
-
+      desc 'met à jour les paramètres utilisateurs'
+      params {
+        requires :parametres, type: String
+      }
+      put '/current/user_parameters/?' do
         parametres = UserParameters.where( uid: utilisateur[ 'uid' ] ).first
-        parametres = UserParameters.create( uid: utilisateur[ 'uid' ] ) if parametres.nil?
 
-        utilisateur['parametrage_cahier_de_textes'] = JSON.parse( parametres[:parameters] )
+        parametres.update( parameters: params[:parametres] )
+        parametres.save
 
-        utilisateur
+        user.full( env )
       end
 
       desc 'efface toute trace de l\'utilisateur identifié'
