41ba1b753b5c2ac23f6d36099b8344b5e8207782
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-01 09:38:01 +0200

    indentation

--- api/v1/emplois_du_temps.rb
+++ api/v1/emplois_du_temps.rb
@@ -56,41 +56,41 @@ module CahierDeTextesAPI
             .select { |day| day.wday == creneau.jour_de_la_semaine } # only the same weekday as the creneau
             .map do |jour|
 
-            if creneau[:semaines_de_presence][ jour.cweek ] == 1
-              cahier_de_textes = CahierDeTextes.where( regroupement_id: creneau[:regroupement_id] ).first
-              cahier_de_textes = CahierDeTextes.create( regroupement_id: creneau[:regroupement_id] ) if cahier_de_textes.nil?
+              if creneau[:semaines_de_presence][ jour.cweek ] == 1
+                cahier_de_textes = CahierDeTextes.where( regroupement_id: creneau[:regroupement_id] ).first
+                cahier_de_textes = CahierDeTextes.create( regroupement_id: creneau[:regroupement_id] ) if cahier_de_textes.nil?
 
-              # STDERR.puts "Séquences pédagogiques multiples associées au créneau #{creneau.id}" if creneau.cours.length > 1
+                # STDERR.puts "Séquences pédagogiques multiples associées au créneau #{creneau.id}" if creneau.cours.length > 1
 
-              { regroupement_id: creneau[ :regroupement_id ],
-                enseignant_id: creneau[ :enseignant_id ],
-                creneau_emploi_du_temps_id: creneau.id,
-                matiere_id: creneau.matiere_id,
-                cahier_de_textes_id: cahier_de_textes.id,  # utilisé lors de la création d'un cours côté client
-                start: Time.new( jour.year, jour.month, jour.mday, creneau.plage_horaire_debut.debut.hour, creneau.plage_horaire_debut.debut.min ).iso8601,
-                end: Time.new( jour.year, jour.month, jour.mday, creneau.plage_horaire_fin.fin.hour, creneau.plage_horaire_fin.fin.min ).iso8601,
-                cours: creneau.cours.select { |cours| cours[:deleted] == false && cours.date_cours == jour }
-                                    .map do |cours|
-                  hcours = cours.to_hash
-                  hcours[:ressources] = cours.ressources.map { |rsrc| rsrc.to_hash }
+                { regroupement_id: creneau[ :regroupement_id ],
+                  enseignant_id: creneau[ :enseignant_id ],
+                  creneau_emploi_du_temps_id: creneau.id,
+                  matiere_id: creneau.matiere_id,
+                  cahier_de_textes_id: cahier_de_textes.id,  # utilisé lors de la création d'un cours côté client
+                  start: Time.new( jour.year, jour.month, jour.mday, creneau.plage_horaire_debut.debut.hour, creneau.plage_horaire_debut.debut.min ).iso8601,
+                  end: Time.new( jour.year, jour.month, jour.mday, creneau.plage_horaire_fin.fin.hour, creneau.plage_horaire_fin.fin.min ).iso8601,
+                  cours: creneau.cours.select { |cours| cours[:deleted] == false && cours.date_cours == jour }
+                                      .map do |cours|
+                    hcours = cours.to_hash
+                    hcours[:ressources] = cours.ressources.map { |rsrc| rsrc.to_hash }
 
-                  hcours
-                end
-                                    .first,
-                devoirs: creneau.devoirs.select { |devoir| devoir[:deleted] == false && devoir.date_due == jour }
-                                        .map do |devoir|
-                  hdevoir = devoir.to_hash
-                  hdevoir[:ressources] = devoir.ressources.map { |rsrc| rsrc.to_hash }
-                  hdevoir[:type_devoir_description] = devoir.type_devoir.description
-                  hdevoir[:fait] = devoir.fait_par?( user.uid )
+                    hcours
+                  end
+                                      .first,
+                  devoirs: creneau.devoirs.select { |devoir| devoir[:deleted] == false && devoir.date_due == jour }
+                                          .map do |devoir|
+                    hdevoir = devoir.to_hash
+                    hdevoir[:ressources] = devoir.ressources.map { |rsrc| rsrc.to_hash }
+                    hdevoir[:type_devoir_description] = devoir.type_devoir.description
+                    hdevoir[:fait] = devoir.fait_par?( user.uid )
 
-                  hdevoir
-                end
-              }
-            else
-              next
+                    hdevoir
+                  end
+                }
+              else
+                next
+              end
             end
-          end
         end
           .flatten
           .compact
