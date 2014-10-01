156244ace1b6a036c89777784b05e97cb9b26108
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-01 09:38:53 +0200

    admin fait ce qu'il veut

--- public/app/scripts/services/Redirection.js
+++ public/app/scripts/services/Redirection.js
@@ -6,10 +6,10 @@ angular.module('cahierDeTexteApp')
 		function( $state, User ) {
 		    this.doorman = function( allowed_types ) {
 			User.get_user().then(function( response ) {
-			    if (_(allowed_types).size() === 0 ||
-				( _(allowed_types).indexOf( response.data['profil_actif']['type'] ) === -1
-				  // && !response.data['profil_actif']['admin']
-				)
+			    if (_(allowed_types).size() === 0
+				|| ( _(allowed_types).indexOf( response.data['profil_actif']['type'] ) === -1
+				     && !( response.data['profil_actif']['admin'] )
+				   )
 			       ) {
 				   // traiter le raffraichissement de l'app en fonction du changement de profil actif
 				   var stateName = '404';
