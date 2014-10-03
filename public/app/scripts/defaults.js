ace65a25b0b7ca645ffa9c3306760ea554438388
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-03 14:28:10 +0200

    ménage

--- public/app/scripts/defaults.js
+++ public/app/scripts/defaults.js
@@ -106,15 +106,6 @@ angular.module( 'cahierDeTexteApp' )
 	$provide.decorator( 'taOptions',
 			    [ '$delegate', 'taRegisterTool',
 			      function( taOptions, taRegisterTool ){
-				  // $delegate is the taOptions we are decorating
-				  // here we override the default toolbars and classes specified in taOptions.
-				  // taOptions.toolbar = [
-				  //     ['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'pre', 'quote'],
-				  //     ['bold', 'italics', 'underline', 'ul', 'ol', 'redo', 'undo', 'clear'],
-				  //     ['justifyLeft','justifyCenter','justifyRight'],
-				  //     ['html', 'insertImage', 'insertLink', 'unlink']
-				  // ];
-
 				  taOptions.toolbar = [
 				      [ 'bold', 'italics', 'underline', 'ul', 'ol', 'quote', 'justifyLeft', 'justifyCenter', 'justifyRight', 'insertLink', 'redo', 'undo' ]
 				  ];
@@ -146,25 +137,4 @@ angular.module( 'cahierDeTexteApp' )
 				  };
 				  return taOptions; // whatever you return will be the taOptions
 			      }]);
-	// this demonstrates changing the classes of the icons for the tools for font-awesome v3.x
-	// $provide.decorator('taTools', ['$delegate', function(taTools){
-	//     taTools.bold.iconclass = 'icon-bold';
-	//     taTools.italics.iconclass = 'icon-italic';
-	//     taTools.underline.iconclass = 'icon-underline';
-	//     taTools.ul.iconclass = 'icon-list-ul';
-	//     taTools.ol.iconclass = 'icon-list-ol';
-	//     taTools.undo.iconclass = 'icon-undo';
-	//     taTools.redo.iconclass = 'icon-repeat';
-	//     taTools.justifyLeft.iconclass = 'icon-align-left';
-	//     taTools.justifyRight.iconclass = 'icon-align-right';
-	//     taTools.justifyCenter.iconclass = 'icon-align-center';
-	//     taTools.clear.iconclass = 'icon-ban-circle';
-	//     taTools.insertLink.iconclass = 'icon-link';
-	//     // taTools.unlink.iconclass = 'icon-link red';
-	//     taTools.insertImage.iconclass = 'icon-picture';
-	//     // there is no quote icon in old font-awesome so we change to text as follows
-	//     delete taTools.quote.iconclass;
-	//     taTools.quote.buttontext = 'quote';
-	//     return taTools;
-	// }]);
     } ] );
