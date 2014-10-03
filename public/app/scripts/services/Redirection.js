ce4b6d0c794e2956c582d6bb66caa045eb10c8d0
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-03 15:51:38 +0200

    style

--- public/app/scripts/services/Redirection.js
+++ public/app/scripts/services/Redirection.js
@@ -5,7 +5,7 @@ angular.module('cahierDeTexteApp')
 	      [ '$state', 'User',
 		function( $state, User ) {
 		    this.doorman = function( allowed_types ) {
-			User.get_user().then(function( response ) {
+			User.get_user().then( function( response ) {
 			    if (_(allowed_types).size() === 0
 				|| ( _(allowed_types).indexOf( response.data['profil_actif']['type'] ) === -1
 				     && !( response.data['profil_actif']['admin'] )
@@ -31,7 +31,7 @@ angular.module('cahierDeTexteApp')
 				   }
 				   $state.go( stateName, $state.params, { reload: true, inherit: true, notify: true } );
 			       }
-			});
+			} );
 		    };
 		}
-	      ]);
+	      ] );
