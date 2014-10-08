23f88d21dec40aaaaa27a8534aad2afb9a5daf5d
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-08 13:46:16 +0200

    pour minification

--- public/app/js/http_interceptors.js
+++ public/app/js/http_interceptors.js
@@ -3,18 +3,19 @@
 angular.module( 'cahierDeTexteApp' )
     .config( [ '$httpProvider',
 	       function( $httpProvider ) {
-		   $httpProvider.interceptors.push( function( $q ) {
-		       return {
-			   'response': function( response ) {
-			       //Will only be called for HTTP up to 300
-			       return response;
-			   },
-			   'responseError': function( rejection ) {
-			       if ( rejection.status === 401 ) {
-				   location.reload();
-			       }
-			       return $q.reject( rejection );
-			   }
-		       };
-		   });
-	       }]);
+		   $httpProvider.interceptors.push( [ '$q',
+						      function( $q ) {
+							  return {
+							      'response': function( response ) {
+								  //Will only be called for HTTP up to 300
+								  return response;
+							      },
+							      'responseError': function( rejection ) {
+								  if ( rejection.status === 401 ) {
+								      location.reload();
+								  }
+								  return $q.reject( rejection );
+							      }
+							  };
+						      } ] );
+	       } ] );
