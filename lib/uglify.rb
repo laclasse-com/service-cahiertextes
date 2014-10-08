c34bd526679abd2dad6fb8167f87b61d63527ae9
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-08 16:54:54 +0200

    retourne quelque chose

--- lib/uglify.rb
+++ lib/uglify.rb
@@ -4,14 +4,14 @@ module Uglify
   module_function
 
   def those_files( ary_files, options = nil )
-    Uglifier.compile( ary_files.map { |fichier| File.read( fichier ) }.join ) if options.nil?
-    Uglifier.compile( ary_files.map { |fichier| File.read( fichier ) }.join,
-                      options ) unless options.nil?
+    return Uglifier.compile( ary_files.map { |fichier| File.read( fichier ) }.join ) if options.nil?
+    return Uglifier.compile( ary_files.map { |fichier| File.read( fichier ) }.join,
+                             options ) unless options.nil?
   end
 
   def those_files_with_map( ary_files, options = nil )
-    Uglifier.compile_with_map( ary_files.map { |fichier| File.read( fichier ) }.join ) if options.nil?
-    Uglifier.compile_with_map( ary_files.map { |fichier| File.read( fichier ) }.join,
-                               options ) unless options.nil?
+    return Uglifier.compile_with_map( ary_files.map { |fichier| File.read( fichier ) }.join ) if options.nil?
+    return Uglifier.compile_with_map( ary_files.map { |fichier| File.read( fichier ) }.join,
+                                      options ) unless options.nil?
   end
 end
