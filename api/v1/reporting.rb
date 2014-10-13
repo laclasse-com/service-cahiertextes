119d52196344dfd6f52454af36629a3e036c4ddd
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-13 17:34:24 +0200

    ajout d'une api de reporting basique, fixes #146

---
+++ api/v1/reporting.rb
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+require_relative '../../models/models'
+require_relative '../../lib/pronote'
+require_relative '../../lib/udt'
+
+module CahierDeTextesAPI
+  module V1
+    class ReportingAPI < Grape::API
+      format :json
+
+      desc 'Return some statistics about the application usage.'
+      get '/' do
+        { nb_etablissements: Etablissement.count,
+          nb_cahiers_de_textes: CahierDeTextes.count,
+          nb_sequence_pedagogiques: Cours.count,
+          nb_devoirs: Devoir.count,
+          nb_devoirs_faits: DevoirTodoItem.count,
+          nb_ressources: Ressource.count,
+          nb_utilisateurs_actifs: UserParameters.count,
+          nb_creneaux_emploi_du_temps: CreneauEmploiDuTemps.count }
+      end
+    end
+  end
+end
