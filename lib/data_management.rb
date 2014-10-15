610ea5c14fc89c0ce4561f21b2bfb2bc49958273
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-15 10:44:19 +0200

    implémente et câble l'API delete utilisateur, fixes #149

---
+++ lib/data_management.rb
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+require_relative '../models/models'
+
+module DataManagement
+  # Fonctions de nettoyage des données
+  module Cleansing
+    module_function
+
+    def unfinished_creneaux
+      CreneauEmploiDuTemps.where( matiere_id: '' )
+                          .all
+                          .select { |c| c.regroupements.empty? && c.date_creation < 1.week.ago }
+                          .each do |c|
+        c.enseignants.each do |ce|
+          ce.destroy
+        end
+        c.destroy
+      end
+    end
+  end
+
+  # Fonctions de data management relatives aux utilisateurs
+  module User
+    module_function
+
+    def delete( uid )
+      UserParameters.where( uid: uid ).destroy
+
+      DevoirTodoItem.where( eleve_id: uid ).destroy
+
+      Cours.where( enseignant_id: uid ).each do |cours|
+        cours.devoirs.each do |devoir|
+          devoir.destroy
+        end
+        cours.destroy
+      end
+
+      CreneauEmploiDuTempsEnseignant.where( enseignant_id: uid )
+                                    .all
+                                    .each do |ce|
+        cid = ce.creneau_emploi_du_temps_id
+        ce.destroy
+        cid.destroy if cid.enseignants.empty?
+      end
+    end
+
+    def merge( target_uid, source_uid )
+      STDERR.puts "#{source_uid} --> #{target_uid}"
+    end
+  end
+end
