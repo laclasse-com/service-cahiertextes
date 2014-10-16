ada6094e57464c6b68af3897977f21736a504e9d
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-16 11:30:38 +0200

    style

--- public/app/js/controllers/PopupEditionCtrl.js
+++ public/app/js/controllers/PopupEditionCtrl.js
@@ -2,8 +2,14 @@
 
 angular.module( 'cahierDeTextesClientApp' )
     .controller( 'PopupEditionCtrl',
-		 [ '$scope', '$filter', '$q', '$sce', '$modalInstance', 'APP_PATH', 'DOCS_URL', 'SEMAINES_VACANCES', 'ZONE', 'Documents', 'API', 'CreneauEmploiDuTemps', 'Cours', 'Devoirs', 'User', 'cours', 'devoirs', 'creneau', 'raw_data', 'classes', 'matieres',
-		   function ( $scope, $filter, $q, $sce, $modalInstance, APP_PATH, DOCS_URL, SEMAINES_VACANCES, ZONE, Documents, API, CreneauEmploiDuTemps, Cours, Devoirs, User, cours, devoirs, creneau, raw_data, classes, matieres ) {
+		 [ '$scope', '$filter', '$q', '$sce', '$modalInstance',
+		   'APP_PATH', 'DOCS_URL', 'SEMAINES_VACANCES', 'ZONE',
+		   'Documents', 'API', 'CreneauEmploiDuTemps', 'Cours', 'Devoirs', 'User',
+		   'cours', 'devoirs', 'creneau', 'raw_data', 'classes', 'matieres',
+		   function ( $scope, $filter, $q, $sce, $modalInstance,
+			      APP_PATH, DOCS_URL, SEMAINES_VACANCES, ZONE,
+			      Documents, API, CreneauEmploiDuTemps, Cours, Devoirs, User,
+			      cours, devoirs, creneau, raw_data, classes, matieres ) {
 		       $scope.app_path = APP_PATH;
 		       $scope.ZONE = ZONE;
 
