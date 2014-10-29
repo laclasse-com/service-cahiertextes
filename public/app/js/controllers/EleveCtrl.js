d6b2a646ee114df14f5886acf721e717d3e522e2
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-29 11:23:31 +0100

    des icônes dans les tabs

--- public/app/js/controllers/EleveCtrl.js
+++ public/app/js/controllers/EleveCtrl.js
@@ -6,9 +6,11 @@ angular.module( 'cahierDeTextesClientApp' )
 		  function ( $scope, $state, $stateParams, current_user ) {
 		      $scope.tabs = [ { heading: 'Emploi du temps',
 					uisref: 'eleve.emploi_du_temps',
+					css_class: 'glyphicon glyphicon-calendar',
 					active: false },
 				      { heading: 'Liste des devoirs',
 					uisref: 'eleve.devoirs',
+					css_class: 'glyphicon glyphicon-list',
 					active: false } ];
 		      _($scope.tabs).each( function( tab ) {
 			  tab.active = tab.uisref == $state.current.name;
--- public/app/js/controllers/EnseignantCtrl.js
+++ public/app/js/controllers/EnseignantCtrl.js
@@ -6,12 +6,15 @@ angular.module( 'cahierDeTextesClientApp' )
 		  function ( $scope, $state, current_user ) {
 		      $scope.tabs = [ { heading: 'Emploi du temps',
 					uisref: 'enseignant.emploi_du_temps',
+					css_class: 'glyphicon glyphicon-calendar',
 					active: false },
 				      { heading: 'Cahier de textes',
 					uisref: 'enseignant.cahier_de_textes',
+					css_class: 'glyphicon glyphicon-list',
 					active: false },
 				      { heading: 'Statistiques',
 					uisref: 'enseignant.stats',
+					css_class: 'glyphicon glyphicon-stats',
 					active: false } ];
 		      _($scope.tabs).each( function( tab ) {
 			  tab.active = tab.uisref == $state.current.name;
