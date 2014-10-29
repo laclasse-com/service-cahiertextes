dfac71dca8f0895d8fc37ecf6a75513c0a73c1d5
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-29 11:23:31 +0100

    des icônes dans les tabs

--- public/app/js/controllers/VieScolaireCtrl.js
+++ public/app/js/controllers/VieScolaireCtrl.js
@@ -6,6 +6,7 @@ angular.module( 'cahierDeTextesClientApp' )
 		  function ( $scope, $state ) {
 		      $scope.tabs = [ { heading: 'Emplois du Temps',
 					uisref: 'principal.emploi_du_temps',
+					css_class: 'glyphicon glyphicon-calendar',
 					active: false } ];
 		      _($scope.tabs).each( function( tab ) {
 			  tab.active = tab.uisref == $state.current.name;
