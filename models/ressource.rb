6dbb35a10fd5bd392380788f966ee7853402dc06
Gwenhael Le Moine <glemoine@erasme.org>
2014-10-29 15:01:11 +0100

    push2dev

--- models/ressource.rb
+++ models/ressource.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 class Ressource < Sequel::Model( :ressources )
-  many_to_many :cours, class: :Cours, join_table: :cours_ressources, left_key: :cours_id, right_key: :ressource_id
+  many_to_many :cours, class: :Cours, join_table: :cours_ressources, left_key: :ressource_id, right_key: :cours_id
   many_to_many :devoirs, join_table: :devoirs_ressources
 end
