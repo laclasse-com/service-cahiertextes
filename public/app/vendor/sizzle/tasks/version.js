5738997ec6559a34955bd624d984336d2236a222
Gwenhael Le Moine <glemoine@erasme.org>
2014-12-17 13:07:44 +0100

    bower update

--- public/app/vendor/sizzle/tasks/version.js
+++ public/app/vendor/sizzle/tasks/version.js
@@ -28,7 +28,11 @@ module.exports = function( grunt ) {
 			}
 			// Commit next pre version
 			grunt.config( "pkg.version", version );
-			grunt.task.run([ "build", "uglify", "dist", "commit:'Release\\: Update version to " + version + "'" ]);
+			grunt.task.run([
+				"build",
+				"tests",
+				"commit:'Release\\: Update version to " + version + "'"
+			]);
 			done();
 		});
 	});
