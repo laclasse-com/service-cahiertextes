daf4c3ba0295788aa70ae0508ec200391589bf1d
Gwenhael Le Moine <glemoine@erasme.org>
2015-02-10 15:50:53 +0100

    not ready to kill this API endpoint yet

--- api/v1/emplois_du_temps.rb
+++ api/v1/emplois_du_temps.rb
@@ -3,29 +3,19 @@
 module CahierDeTextesAPI
   module V1
     class EmploisDuTempsAPI < Grape::API
-      # desc 'emploi du temps de l\'utilisateur durant l\'intervalle de dates donné'
-      # params {
-      #   requires :debut, type: Date
-      #   requires :fin, type: Date
+      desc 'emploi du temps de l\'utilisateur durant l\'intervalle de dates donné'
+      params {
+        requires :debut, type: Date
+        requires :fin, type: Date
 
-      #   optional :uid
-      # }
-      # get '/du/:debut/au/:fin' do
-      #   LOGGER.debug 'Params in url'
-      #   if params[:uid].nil?
-      #     LOGGER.debug 'Pas d\'uid fourni'
-      #     emploi_du_temps( Date.parse( params[:debut].iso8601 ),
-      #                      Date.parse( params[:fin].iso8601 ),
-      #                      user.regroupements_ids )
-      #   else
-      #     LOGGER.debug "uid fourni : #{params[:uid]}"
-
-      #       emploi_du_temps( Date.parse( params[:debut].iso8601 ),
-      #                        Date.parse( params[:fin].iso8601 ),
-      #                        user.regroupements_ids( params[:uid] ),
-      #                        params[:uid] )
-      #   end
-      # end
+        optional :uid
+      }
+      get '/du/:debut/au/:fin' do
+        emploi_du_temps( Date.parse( params[:debut].iso8601 ),
+                         Date.parse( params[:fin].iso8601 ),
+                         user.regroupements_ids( params[:uid] ),
+                         params[:uid] )
+      end
 
       desc 'emploi du temps de l\'utilisateur durant l\'intervalle de dates donné'
       params {
