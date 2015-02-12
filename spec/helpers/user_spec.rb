f2907dccdc5429b8f8910030f694474c2f481b9a
Gwenhael Le Moine <glemoine@erasme.org>
2015-02-12 11:28:32 +0100

    test user_verbose

--- spec/helpers/user_spec.rb
+++ spec/helpers/user_spec.rb
@@ -190,4 +190,81 @@ describe CahierDeTextesApp::Helpers::User do
 
     expect( subject.new.user_regroupements_ids ).to eq [ 1, 2, 11, 12, 101, 102 ]
   end
+
+  it 'Process the user profile as the CT needs it to be' do
+    module CahierDeTextesApp
+      module Helpers
+        module User
+          def user
+            { uid: 'VAA00000',
+              user_detailed: { 'classes' => [ { 'etablissement_code' => 'Test',
+                                                'classe_id' => 1 },
+                                              { 'etablissement_code' => 'Test',
+                                                'classe_id' => 2 },
+                                              { 'etablissement_code' => 'Invalid',
+                                                'classe_id' => 9 } ],
+                               'groupes_eleves' => [ { 'etablissement_code' => 'Test',
+                                                       'groupe_id' => 11 },
+                                                     { 'etablissement_code' => 'Invalid',
+                                                       'groupe_id' => 99 },
+                                                     { 'etablissement_code' => 'Test',
+                                                       'groupe_id' => 12 } ],
+                               'groupes_libres' => [ { 'etablissement_code' => 'Invalid',
+                                                       'groupe_id' => 999 },
+                                                     { 'etablissement_code' => 'Test',
+                                                       'groupe_id' => 101 },
+                                                     { 'etablissement_code' => 'Test',
+                                                       'groupe_id' => 102 } ],
+                               'roles' => [ { 'etablissement_code_uai' => 'Test',
+                                              'role_id' => 'TECH' } ],
+                               'profils' => [ { 'etablissement_code_uai' => 'Test',
+                                                'profil_id' => 'ENS' },
+                                              { 'etablissement_code_uai' => 'Invalid',
+                                                'profil_id' => 'DIR' } ],
+                               'profil_actif' => { 'etablissement_code_uai' => 'Test',
+                                                   'profil_id' => 'ENS' },
+                               'etablissements' => 'dummy',
+                               'applications' => 'dummy',
+                               'ressources_numeriques' => 'dummy',
+                               'enfants' => 'dummy',
+                             } }
+          end
+        end
+      end
+    end
+
+    result = subject.new.user_verbose
+
+    expect( result['profil_actif'] ).to_not be_nil
+    expect( result['profil_actif']['etablissement_code_uai'] ).to eq 'Test'
+    expect( result['profil_actif']['profil_id'] ).to eq 'ENS'
+
+    expect( result['profils'] ).to_not be_nil
+    expect( result['profils'][0]['admin'] ).to be true
+    expect( result['profils'][1]['admin'] ).to be false
+
+    expect( result['enfants'] ).to_not be_nil
+    expect( result['enfants'] ).to eq 'dummy'
+
+    expect( result['classes'] ).to_not be_nil
+    expect( result['classes'].count ).to eq 9
+    expect( result['classes'].map { |c| c['id'] }.count ).to eq 9
+    expect( result['classes'].map { |c| c['libelle'] }.count ).to eq 9
+    expect( result['classes'].map { |c| c['type'] }.count ).to eq 9
+    expect( result['classes'].map { |c| c['type'] }.select { |c| c == 'classe' }.count ).to eq 3
+    expect( result['classes'].map { |c| c['type'] }.select { |c| c == 'groupe' }.count ).to eq 6
+
+    expect( result['marqueur_xiti'] ).to eq ''
+
+    expect( result[:user_detailed]['profil_actif'] ).to be_nil
+    expect( result[:user_detailed]['etablissements'] ).to be_nil
+    expect( result[:user_detailed]['applications'] ).to be_nil
+    expect( result[:user_detailed]['ressources_numeriques'] ).to be_nil
+    expect( result[:user_detailed]['profils'] ).to be_nil
+    expect( result[:user_detailed]['enfants'] ).to be_nil
+    expect( result[:user_detailed]['classes'] ).to be_nil
+    expect( result[:user_detailed]['groupes_eleves'] ).to be_nil
+    expect( result[:user_detailed]['groupes_libres'] ).to be_nil
+    expect( result[:user_detailed]['roles'] ).to be_nil
+  end
 end
