14d352e87ae60cdf14f9d67806dbdfe7f78735f0
Gwenhael Le Moine <cycojesus@timac.erasme.org>
2015-02-24 09:26:29 +0100

    passed

--- spec/api/v1/creneaux_emploi_du_temps_spec.rb
+++ spec/api/v1/creneaux_emploi_du_temps_spec.rb
@@ -15,50 +15,28 @@ describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
     CahierDeTextesAPI::API
   end
 
-  # Enseignant
   before :all do
-    class HashedUser
-      def admin?
-        false
-      end
-    end
-
-    module Annuaire
-      module_function
-
-      def get_user( _uid )
-        MOCKED_DATA[:users][:enseignant][:annuaire]
-      end
-
-      def get_user_regroupements( uid )
-        u = get_user( uid )
-        { 'classes' => u['classes'],
-          'groupes_eleves' => u['groupes_eleves'],
-          'groupes_libres' => u['groupes_libres']
-        }
-      end
-
-      def get_etablissement_regroupements( _uai )
-        MOCKED_DATA[:etablissement][:regroupements]
-      end
-    end
-
-    # Mock d'une session Enseignant
-    module CahierDeTextesApp
+    module Laclasse
       module Helpers
-        module User
-          def user
-            HashedUser.new( MOCKED_DATA[:users][:enseignant][:rack_session] )
+        module Authentication
+          def logged?
+            LOGGER.info 'Yeah yeah you are logged *wink*'
+            true
           end
         end
       end
     end
   end
-  ############ GET ############
+
+  # Enseignant
   it 'récupère un créneau d\'emploi du temps' do
     id = CreneauEmploiDuTemps.all.sample.id
+
+    LOGGER.debug "/v1/creneaux_emploi_du_temps/#{id}"
+
     get "/v1/creneaux_emploi_du_temps/#{id}"
 
+    LOGGER.debug last_response if last_response.status == 500
     expect( last_response.status ).to eq 200
 
     response = JSON.parse last_response.body
@@ -69,6 +47,20 @@ describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
   end
 
   it 'renseigne un nouveau créneau' do
+    module CahierDeTextesApp
+      module Helpers
+        module User
+          def user
+            { uid: 'TestUser',
+              user_detailed: { 'roles' => [ { 'etablissement_code_uai' => 'Test',
+                                                              'role_id' => 'TECH' } ],
+                                               'profil_actif' => { 'etablissement_code_uai' => 'Test',
+                                                                   'profil_id' => 'ENS' } } }
+          end
+        end
+      end
+    end
+
     jour = rand 1..7
     heure_debut = Time.now.beginning_of_hour.iso8601
     heure_fin = (Time.now.beginning_of_hour + ( (rand 1..5) * 1800 )).iso8601
@@ -81,6 +73,7 @@ describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
                                             matiere_id: matiere_id,
                                             regroupement_id: regroupement_id }
 
+    LOGGER.debug last_response if last_response.status == 500
     expect( last_response.status ).to eq 201
 
     response_body = JSON.parse(last_response.body)
@@ -90,62 +83,22 @@ describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
     expect( CreneauEmploiDuTemps[response_body['id']].matiere_id ).to eq matiere_id
     # expect( CreneauEmploiDuTemps[response_body['id']].regroupement_id ).to eq regroupement_id
   end
-end
-
-describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
-  include Rack::Test::Methods
-
-  before :each do
-    TableCleaner.new( DB, [] ).clean
-
-    load_test_data
-  end
-
-  def app
-    CahierDeTextesAPI::API
-  end
 
   # Élève
-  before :all do
-    class HashedUser
-      def admin?
-        false
-      end
-    end
-
-    module Annuaire
-      module_function
-
-      def get_user( _uid )
-        MOCKED_DATA[:users][:eleve][:annuaire]
-      end
-
-      def get_user_regroupements( uid )
-        u = get_user( uid )
-        { 'classes' => u['classes'],
-          'groupes_eleves' => u['groupes_eleves'],
-          'groupes_libres' => u['groupes_libres']
-        }
-      end
-
-      def get_etablissement_regroupements( _uai )
-        MOCKED_DATA[:etablissement][:regroupements]
-      end
-    end
-
-    # Mock d'une session Eleve
+  it 'fails to renseigne un nouveau créneau' do
     module CahierDeTextesApp
       module Helpers
         module User
           def user
-            HashedUser.new( MOCKED_DATA[:users][:eleve][:rack_session] )
+            { user_detailed: { 'roles' => [ { 'etablissement_code_uai' => 'Test',
+                                              'role_id' => 'Rien' } ],
+                               'profil_actif' => { 'etablissement_code_uai' => 'Test',
+                                                   'profil_id' => 'ELV' } } }
           end
         end
       end
     end
-  end
 
-  it 'fails to renseigne un nouveau créneau' do
     jour = rand 1..7
     heure_debut = Time.now.beginning_of_hour.iso8601
     heure_fin = (Time.now.beginning_of_hour + ( (rand 1..5) * 1800 )).iso8601
@@ -158,168 +111,7 @@ describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
                                             matiere_id: matiere_id,
                                             regroupement_id: regroupement_id }
 
+    LOGGER.debug last_response if last_response.status == 500
     expect( last_response.status ).to eq 401
-  end
-end
-
-describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
-  include Rack::Test::Methods
-
-  before :each do
-    TableCleaner.new( DB, [] ).clean
-
-    load_test_data
-  end
-
-  def app
-    CahierDeTextesAPI::API
-  end
-
-  # Principal
-  before :all do
-    class HashedUser
-      def admin?
-        false
-      end
-    end
-
-    module Annuaire
-      module_function
-
-      def get_user( _uid )
-        MOCKED_DATA[:users][:principal][:annuaire]
-      end
-
-      def get_user_regroupements( uid )
-        u = get_user( uid )
-        { 'classes' => u['classes'],
-          'groupes_eleves' => u['groupes_eleves'],
-          'groupes_libres' => u['groupes_libres']
-        }
-      end
-
-      def get_etablissement_regroupements( _uai )
-        MOCKED_DATA[:etablissement][:regroupements]
-      end
-    end
-
-    # Mock d'une session Principal
-    module CahierDeTextesApp
-      module Helpers
-        module User
-          def user
-            HashedUser.new( MOCKED_DATA[:users][:principal][:rack_session] )
-          end
-        end
-      end
-    end
-  end
-end
-
-describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
-  include Rack::Test::Methods
-
-  before :each do
-    TableCleaner.new( DB, [] ).clean
-
-    load_test_data
-  end
-
-  def app
-    CahierDeTextesAPI::API
-  end
-
-  # Vie Scolaire
-  before :all do
-    class HashedUser
-      def admin?
-        false
-      end
-    end
-
-    module Annuaire
-      module_function
-
-      def get_user( _uid )
-        MOCKED_DATA[:users][:vie_scolaire][:annuaire]
-      end
-
-      def get_user_regroupements( uid )
-        u = get_user( uid )
-        { 'classes' => u['classes'],
-          'groupes_eleves' => u['groupes_eleves'],
-          'groupes_libres' => u['groupes_libres']
-        }
-      end
-
-      def get_etablissement_regroupements( _uai )
-        MOCKED_DATA[:etablissement][:regroupements]
-      end
-    end
-
-    # Mock d'une session Vie_Scolaire
-    module CahierDeTextesApp
-      module Helpers
-        module User
-          def user
-            HashedUser.new( MOCKED_DATA[:users][:vie_scolaire][:rack_session] )
-          end
-        end
-      end
-    end
-  end
-end
-
-describe CahierDeTextesAPI::V1::CreneauxEmploiDuTempsAPI do
-  include Rack::Test::Methods
-
-  before :each do
-    TableCleaner.new( DB, [] ).clean
-
-    load_test_data
-  end
-
-  def app
-    CahierDeTextesAPI::API
-  end
-
-  # Parent
-  before :all do
-    class HashedUser
-      def admin?
-        false
-      end
-    end
-
-    module Annuaire
-      module_function
-
-      def get_user( _uid )
-        MOCKED_DATA[:users][:parent][:annuaire]
-      end
-
-      def get_user_regroupements( uid )
-        u = get_user( uid )
-        { 'classes' => u['classes'],
-          'groupes_eleves' => u['groupes_eleves'],
-          'groupes_libres' => u['groupes_libres']
-        }
-      end
-
-      def get_etablissement_regroupements( _uai )
-        MOCKED_DATA[:etablissement][:regroupements]
-      end
-    end
-
-    # Mock d'une session Parent
-    module CahierDeTextesApp
-      module Helpers
-        module User
-          def user
-            HashedUser.new( MOCKED_DATA[:users][:parent][:rack_session] )
-          end
-        end
-      end
-    end
   end
 end
