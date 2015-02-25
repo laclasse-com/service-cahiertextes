bbce8d66ac7dbaa0cf253f592e552b0b80247c14
Gwenhael Le Moine <cycojesus@timac.erasme.org>
2015-02-25 16:07:33 +0100

    404 en cas d'établissement farfelu

--- api/v1/etablissements.rb
+++ api/v1/etablissements.rb
@@ -14,7 +14,11 @@ module CahierDeTextesAPI
         requires :uai, desc: 'Code UAI de l\'établissement'
       }
       get '/:uai/statistiques/classes' do
-        Etablissement.where(uai: params[:uai]).first.statistiques_classes
+        etablissement = Etablissement.where(uai: params[:uai]).first
+
+        etablissement.statistiques_classes unless etablissement.nil?
+
+        error!( "Établissement #{params[:uai]} inconnu", 404 ) if etablissement.nil?
       end
 
       desc 'statistiques du cahier de textes d\'une classe'
@@ -25,9 +29,9 @@ module CahierDeTextesAPI
       get '/:uai/statistiques/classes/:regroupement_id' do
         cahier_de_textes = CahierDeTextes[ regroupement_id: params[:regroupement_id] ]
 
-        error!( 'Classe inconnue', 404 ) if cahier_de_textes.nil?
+        error!( "Classe #{params[:regroupement_id]} inconnue dans l'établissement #{params[:uai]}", 404 ) if cahier_de_textes.nil?
 
-        cahier_de_textes.statistiques
+        cahier_de_textes.statistiques unless cahier_de_textes.nil?
       end
 
       desc 'statistiques des cahiers de textes par enseignants/mois'
@@ -35,7 +39,11 @@ module CahierDeTextesAPI
         requires :uai, desc: 'Code UAI de l\'établissement'
       }
       get '/:uai/statistiques/enseignants' do
-        Etablissement.where(UAI: params[:uai]).first.statistiques_enseignants
+        etablissement = Etablissement.where(UAI: params[:uai]).first
+
+        error!( "Établissement #{params[:uai]} inconnu", 404 ) if etablissement.nil?
+
+        etablissement.statistiques_enseignants unless etablissement.nil?
       end
 
       desc 'saisies détaillées d\'un enseignant dans les cahiers de textes par mois/classes'
@@ -44,7 +52,11 @@ module CahierDeTextesAPI
         requires :enseignant_id, desc: 'identifiant annuaire de l\'enseignant'
       }
       get '/:uai/statistiques/enseignants/:enseignant_id' do
-        Etablissement.where(uai: params[:uai]).first.saisies_enseignant( params[:enseignant_id] )
+        etablissement = Etablissement.where(uai: params[:uai]).first
+
+        error!( "Établissement #{params[:uai]} inconnu", 404 ) if etablissement.nil?
+
+        etablissement.saisies_enseignant( params[:enseignant_id] ) unless etablissement.nil?
       end
     end
   end
