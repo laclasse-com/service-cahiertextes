e2a26751cb13f8ff26dd3e79b57d1c70a3bbe2b2
Gwenhael Le Moine <cycojesus@timac.erasme.org>
2015-02-25 16:09:39 +0100

    modif mock data

--- spec/helper_mocks/helpers/authentication.rb
+++ spec/helper_mocks/helpers/authentication.rb
@@ -1,11 +1,28 @@
 # encoding: utf-8
 
-module CahierDeTextesApp
+module Laclasse
   module Helpers
     module Authentication
-      def is_logged?
+      def logged?
+        LOGGER.info 'Yeah yeah you are logged *wink*'
         true
       end
     end
+  end
+end
+
+module AnnuaireWrapper
+  def get_etablissement( _uai )
+    { 'classes' => [ { 'etablissement_code' => 'Test',
+                       'classe_id' => 1 },
+                     { 'etablissement_code' => 'Test',
+                       'classe_id' => 2 },
+                     { 'etablissement_code' => 'Invalid',
+                       'classe_id' => 3 } ],
+      'groupes_eleves' => [ { 'etablissement_code' => 'Test',
+                              'groupe_id' => 4 },
+                            { 'etablissement_code' => 'Invalid',
+                              'groupe_id' => 5 } ],
+      'groupes_libres' => [  ] }
   end
 end
