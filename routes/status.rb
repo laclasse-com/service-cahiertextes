5c02133ca53095aff6d6e19b61d98b5650c71095
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-03-25 11:03:48 +0100

    plus better

--- routes/status.rb
+++ routes/status.rb
@@ -5,15 +5,39 @@ module CahierDeTextesApp
     module Status
       def self.registered( app )
         app.get "#{APP_PATH}/status/?" do
-          { status: 'OK',
-            nb_etablissements: Etablissement.count,
+          content_type :json
+
+          status = 'OK'
+          reason = 'L\'application Cahier de Textes fonctionne.'
+
+          # test DB
+          if DB.nil?
+            status = 'KO'
+            reason = 'Base de données innaccessible.'
+          end
+          if DB.tables.length == 0
+            status = 'KO'
+            reason = 'Base de données non provisionnée.'
+          end
+
+          { app_id: ANNUAIRE[:app_id],
+            status: status,
+            reason: reason
+          }.to_json
+        end
+
+        app.get "#{APP_PATH}/status/report/?" do
+          content_type :json
+
+          { nb_etablissements: Etablissement.count,
             nb_cahiers_de_textes: CahierDeTextes.count,
             nb_sequences_pedagogiques: Cours.count,
             nb_devoirs: Devoir.count,
             nb_devoirs_faits: DevoirTodoItem.count,
             nb_ressources: Ressource.count,
             nb_utilisateurs_actifs: UserParameters.count,
-            nb_creneaux_emploi_du_temps: CreneauEmploiDuTemps.count }
+            nb_creneaux_emploi_du_temps: CreneauEmploiDuTemps.count
+          }.to_json
         end
       end
     end
