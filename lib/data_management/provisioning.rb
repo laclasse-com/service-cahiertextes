57309a396f47b0d3826710f86e94f1c6802e25c7
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-04-29 10:11:26 +0200

    error management

--- lib/data_management/provisioning.rb
+++ lib/data_management/provisioning.rb
@@ -6,21 +6,26 @@ module DataManagement
     module_function
 
     def provision( user )
-      user[:user_detailed]['etablissements']
-        .each do |etab|
-        etablissement = Etablissement.where(UAI: etab[ 'code_uai' ]).first
+      if user[:user_detailed].nil? || user[:user_detailed]['etablissements'].nil?
+        LOGGER.warn 'user has no etablissements defined'
+        LOGGER.warn "#{user}"
+      else
+        user[:user_detailed]['etablissements']
+          .each do |etab|
+          etablissement = Etablissement.where(UAI: etab[ 'code_uai' ]).first
 
-        next unless etablissement.nil?
+          next unless etablissement.nil?
 
-        etablissement = AnnuaireWrapper::Etablissement.get( etab[ 'code_uai' ] )
-        Etablissement.create(UAI: etablissement['code_uai' ] )
-        etablissement['classes']
-          .concat( etablissement['groupes_eleves'] )
-          .concat( etablissement['groupes_libres'] )
-          .each do |regroupement|
-          cdt = CahierDeTextes.where( regroupement_id: regroupement['id'] ).first
-          CahierDeTextes.create( date_creation: Time.now,
-                                 regroupement_id: regroupement['id'] ) if cdt.nil?
+          etablissement = AnnuaireWrapper::Etablissement.get( etab[ 'code_uai' ] )
+          Etablissement.create(UAI: etablissement['code_uai' ] )
+          etablissement['classes']
+            .concat( etablissement['groupes_eleves'] )
+            .concat( etablissement['groupes_libres'] )
+            .each do |regroupement|
+            cdt = CahierDeTextes.where( regroupement_id: regroupement['id'] ).first
+            CahierDeTextes.create( date_creation: Time.now,
+                                   regroupement_id: regroupement['id'] ) if cdt.nil?
+          end
         end
       end
     end
