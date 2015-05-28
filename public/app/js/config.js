542029d6db5692b71eb37eedddd1bc86fce7043c
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-05-28 09:34:48 +0200

    disable debug info when not running on localhost

--- public/app/js/config.js
+++ public/app/js/config.js
@@ -1,6 +1,14 @@
 'use strict';
 
 angular.module( 'cahierDeTextesClientApp' )
+    .config( [ '$compileProvider', 'LOCALHOST',
+	       function( $compileProvider, LOCALHOST ) {
+		   if ( ! LOCALHOST ) {
+		       $compileProvider.debugInfoEnabled( false );
+		   } else {
+		       console.log( 'Riding http://localhost !' )
+		   }
+	       } ] )
     .config( [ 'cfpLoadingBarProvider',
 	       function( cfpLoadingBarProvider ) {
 		   // n'affiche la barre de chargement que si la requête prends plus de 0,5s
