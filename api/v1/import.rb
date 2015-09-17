73cb9d217f33fa6c2694ec98930db267ee57c771
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-09-17 11:06:59 +0200

    create_creneaux is a string

--- api/v1/import.rb
+++ api/v1/import.rb
@@ -20,14 +20,15 @@ module CahierDeTextesAPI
       end
       post '/pronote' do
         uai = ProNote.extract_uai_from_xml( File.open( params[:file][:tempfile] ) )
-        create_creneaux = params.key?( :create_creneaux ) ? params[:create_creneaux] : true
+
+        create_creneaux = params.key?( :create_creneaux ) ? params[:create_creneaux] == 'true' : true
 
         error!( '401 Unauthorized', 401 ) unless user_is_profils_in_etablissement?( %w( DIR ENS ), uai )
 
         # on retourne un log succint des infos chargées
         { filename: params[:file][:filename],
           size: params[:file][:tempfile].size,
-          create_creneaux: params[:create_creneaux],
+          create_creneaux: create_creneaux,
           rapport: ProNote.decrypt_and_load_xml( File.open( params[:file][:tempfile] ),
                                                  create_creneaux ) }
       end
