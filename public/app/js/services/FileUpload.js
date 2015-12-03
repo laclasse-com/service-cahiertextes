5b8f4f7c296859dec90bd382f5af7f2bf66a45e5
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-12-03 11:04:11 +0100

    replace ng-file-upload with custom code
    Signed-off-by: Gwenhael Le Moine <gwenhael.le.moine@gmail.com>

---
+++ public/app/js/services/FileUpload.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module( 'cahierDeTextesClientApp' )
+    .service( 'fileUpload',
+              [ '$http',
+                function( $http ) {
+                    this.uploadFileToUrl = function( file, uploadUrl ) {
+                        var fd = new FormData();
+                        fd.append( 'file', file );
+                        return $http.post( uploadUrl, fd, {
+                            transformRequest: angular.identity,
+                            headers: { 'Content-Type': undefined }
+                        } );
+                    };
+                } ] );
