ff78e64e029f6c866fd20434b483dc61f88ce957
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2015-12-03 11:24:07 +0100

    ability to pass extra params to upload

--- public/app/js/services/FileUpload.js
+++ public/app/js/services/FileUpload.js
@@ -4,9 +4,13 @@ angular.module( 'cahierDeTextesClientApp' )
     .service( 'fileUpload',
               [ '$http',
                 function( $http ) {
-                    this.uploadFileToUrl = function( file, uploadUrl ) {
+                    this.uploadFileToUrl = function( file, uploadUrl, data ) {
                         var fd = new FormData();
                         fd.append( 'file', file );
+                        _.chain(data).keys().each( function( key ) {
+                            fd.append( key, data[ key ] );
+                        } );
+
                         return $http.post( uploadUrl, fd, {
                             transformRequest: angular.identity,
                             headers: { 'Content-Type': undefined }
