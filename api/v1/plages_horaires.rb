865b290f7e29de7a8ea2f963c783383884a25f05
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-03-07 14:21:47 +0100

    REST complete API for PlageHoraire

--- api/v1/plages_horaires.rb
+++ api/v1/plages_horaires.rb
@@ -8,12 +8,62 @@ module CahierDeTextesAPI
         PlageHoraire.all
       end
 
-      desc 'renvoi une plage horaire'
+      desc 'return the plage horaire'
       params do
         requires :id
       end
       get '/:id' do
-        PlageHoraire[ params[:id] ]
+        ph = PlageHoraire[ params[:id] ]
+        error!( "Plage horaire #{params[:id]} inconnu", 404 ) if ph.nil?
+
+        ph
+      end
+
+      desc 'create a plage horaire'
+      params do
+        requires :label
+        requires :debut
+        requires :fin
+      end
+      post '/' do
+        ph = DataManagement::Accessors.create_or_get( PlageHoraire,
+                                                      label: params[:label] )
+        ph.update( debut: params[:debut],
+                   fin: params[:fin] )
+        ph.save
+
+        ph
+      end
+
+      desc 'update the plage horaire'
+      params do
+        requires :id
+
+        optional :label
+        optional :debut
+        optional :fin
+      end
+      put '/:id' do
+        ph = PlageHoraire[ params[:id] ]
+        error!( "Plage horaire #{params[:id]} inconnu", 404 ) if ph.nil?
+
+        ph.label = params[:label] if params.key? :label
+        ph.debut = params[:debut] if params.key? :debut
+        ph.fin = params[:fin] if params.key? :fin
+        ph.save
+
+        ph
+      end
+
+      desc 'delete the plage horaire'
+      params do
+        requires :id
+      end
+      delete '/:id' do
+        ph = PlageHoraire[ params[:id] ]
+        error!( "Plage horaire #{params[:id]} inconnu", 404 ) if ph.nil?
+
+        ph.destroy
       end
     end
   end
