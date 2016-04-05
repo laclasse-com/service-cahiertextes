987c2b989fa79ebc8c913e603244130fa0ea66be
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-04-05 15:50:54 +0200

    countless useless API calls removed

--- public/app/js/controllers/EleveDevoirsCtrl.js
+++ public/app/js/controllers/EleveDevoirsCtrl.js
@@ -9,7 +9,6 @@ angular.module( 'cahierDeTextesClientApp' )
                             APP_PATH, DOCS_URL, API, Annuaire, Devoirs, Cours, CreneauxEmploiDuTemps,
                             current_user ) {
                       // popup d'affichage des détails
-                      var types_de_devoir = [];
                       $scope.affiche_faits = false;
                       $scope.tri_ascendant = true;
                       $scope.popup_ouverte = false;
@@ -34,78 +33,56 @@ angular.module( 'cahierDeTextesClientApp' )
 
                       $scope.current_user = current_user;
 
-                      API.query_types_de_devoir()
-                          .$promise.then( function( response ) {
-                              types_de_devoir = response;
+                      var retrieve_data = function() {
+                          $scope.from_date = moment().subtract( $scope.period_offset, 'months' ).subtract( 2, 'weeks' ).toDate();
+                          $scope.to_date = moment().subtract( $scope.period_offset, 'months' ).add( 2, 'weeks' ).toDate();
 
-                              var retrieve_data = function() {
-                                  $scope.from_date = moment().subtract( $scope.period_offset, 'months' ).subtract( 2, 'weeks' ).toDate();
-                                  $scope.to_date = moment().subtract( $scope.period_offset, 'months' ).add( 2, 'weeks' ).toDate();
+                          API.query_devoirs({ debut: $scope.from_date,
+                                              fin: $scope.to_date,
+                                              uid: $scope.current_user.profil_actif.profil_id == 'TUT' ? $scope.current_user.enfant_actif.enfant.id_ent : null })
+                              .$promise.then(function( response ) {
+                                  $scope.matieres = {};
 
-                                  API.query_devoirs({ debut: $scope.from_date,
-                                                      fin: $scope.to_date,
-                                                      uid: $scope.current_user.profil_actif.profil_id == 'TUT' ? $scope.current_user.enfant_actif.enfant.id_ent : null })
-                                      .$promise.then(function( response ) {
-                                          $scope.matieres = {};
+                                  $scope.all_devoirs = _(response).map( function( devoir ) {
+                                      $scope.matieres[ devoir.creneau_emploi_du_temps.matiere_id ] = _($scope.current_user.profil_actif.matieres).findWhere({ id: devoir.creneau_emploi_du_temps.matiere_id });
+                                      devoir.matiere = $scope.matieres[ devoir.creneau_emploi_du_temps.matiere_id ];
 
-                                          $scope.all_devoirs = _(response).map( function( devoir ) {
-                                              devoir.type_devoir = _(types_de_devoir).findWhere({id: devoir.type_devoir_id});
-
-                                              CreneauxEmploiDuTemps.get({ id: devoir.creneau_emploi_du_temps_id })
-                                                  .$promise.then( function success( response ) {
-                                                      devoir.creneau_emploi_du_temps = response;
-                                                      $scope.matieres[ devoir.creneau_emploi_du_temps.matiere_id ] = _($scope.current_user.profil_actif.matieres).findWhere({ id: devoir.creneau_emploi_du_temps.matiere_id });
-                                                      devoir.matiere = $scope.matieres[ devoir.creneau_emploi_du_temps.matiere_id ];
-                                                  });
-
-                                              devoir.cours = _(devoir.cours_id).isNull() ? null : Cours.get({ id: devoir.cours_id });
-
-                                              _(devoir.ressources).each( function( ressource ) {
-                                                  ressource.url = $sce.trustAsResourceUrl( DOCS_URL + '/api/connector?cmd=file&target=' + ressource.hash );
-                                              } );
-
-                                              return devoir;
-                                          });
-                                          $scope.devoirs = $scope.all_devoirs;
-                                      });
-                              };
-
-                              $scope.filter_data = function( matiere_id ) {
-                                  if ( _(matiere_id).isNull() ) {
-                                      $scope.devoirs = $scope.all_devoirs;
-                                  } else {
-                                      $scope.devoirs = _($scope.all_devoirs).select( function( devoir ) {
-                                          return devoir.creneau_emploi_du_temps.matiere_id == matiere_id;
+                                      _(devoir.ressources).each( function( ressource ) {
+                                          ressource.url = $sce.trustAsResourceUrl( DOCS_URL + '/api/connector?cmd=file&target=' + ressource.hash );
                                       } );
-                                  }
-                              };
 
-                              $scope.period_offset = 0;
+                                      return devoir;
+                                  });
+                                  $scope.devoirs = $scope.all_devoirs;
+                              });
+                      };
 
-                              // retrieve_data() when the value of week_offset changes
-                              // n.b.: triggered when period_offset is initialized above
-                              var nb_mois_depuis_septembre = Math.abs( 9 - ( moment().month() + 1 ) );
-                              $scope.period_offsets_list = _.range( nb_mois_depuis_septembre,
-                                                                    ( 10 - nb_mois_depuis_septembre ) * -1,
-                                                                    -1 )
-                                  .map( function( offset ) {
-                                      return { offset: offset,
-                                               label: offset == 0 ? 'ce mois' : moment().add( offset * -1, 'months' ).fromNow() };
-                                  } );
-                              // $scope.period_offsets_list.push( { offset: 9999,
-                              //                                         label: 'année complète'} );
-                              $scope.$watch( 'period_offset', function() {
-                                  retrieve_data();
+                      $scope.filter_data = function( matiere_id ) {
+                          if ( _(matiere_id).isNull() ) {
+                              $scope.devoirs = $scope.all_devoirs;
+                          } else {
+                              $scope.devoirs = _($scope.all_devoirs).select( function( devoir ) {
+                                  return devoir.creneau_emploi_du_temps.matiere_id == matiere_id;
                               } );
+                          }
+                      };
 
-                              $scope.incr_offset = function() {
-                                  $scope.period_offset++;
-                              };
-                              $scope.decr_offset = function() {
-                                  $scope.period_offset--;
-                              };
-                              $scope.reset_offset = function() {
-                                  $scope.period_offset = 0;
-                              };
-                          });
+                      $scope.period_offset = 0;
+
+                      // retrieve_data() when the value of week_offset changes
+                      // n.b.: triggered when period_offset is initialized above
+                      var nb_mois_depuis_septembre = Math.abs( 9 - ( moment().month() + 1 ) );
+                      $scope.period_offsets_list = _.range( nb_mois_depuis_septembre, ( 10 - nb_mois_depuis_septembre ) * -1, -1 )
+                          .map( function( offset ) {
+                              return { offset: offset,
+                                       label: offset == 0 ? 'ce mois' : moment().add( offset * -1, 'months' ).fromNow() };
+                          } );
+
+                      $scope.$watch( 'period_offset', function() {
+                          retrieve_data();
+                      } );
+
+                      $scope.incr_offset = function() { $scope.period_offset++; };
+                      $scope.decr_offset = function() { $scope.period_offset--; };
+                      $scope.reset_offset = function() { $scope.period_offset = 0; };
                   } ] );
