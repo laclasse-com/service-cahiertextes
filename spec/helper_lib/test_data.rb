b6f7a432e1c86f340bb7a8c76662861dc2736a71
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-04-29 15:55:36 +0200

    not much

--- spec/helper_lib/test_data.rb
+++ spec/helper_lib/test_data.rb
@@ -7,23 +7,22 @@ end
 def generate_test_data
   CahierDeTextes.all.each do |cahier_de_textes|
     12.times do |month|
-      rand(2..4).times do
+      rand( 2..4 ).times do
         creneau = CreneauEmploiDuTempsEnseignant.all.sample
 
-        cours = Cours.create(cahier_de_textes_id: cahier_de_textes.id,
-                             creneau_emploi_du_temps_id: creneau.creneau_emploi_du_temps_id,
-                             date_cours: '2013-' + (month + 1).to_s + '-29',
-                             contenu: 'Exemple de séquence pédagogique.',
-                             enseignant_id: creneau.enseignant_id )
+        cours = Cours.create( cahier_de_textes_id: cahier_de_textes.id,
+                              creneau_emploi_du_temps_id: creneau.creneau_emploi_du_temps_id,
+                              date_cours: "#{Time.now.year}-#{month + 1}-29",
+                              contenu: 'Exemple de séquence pédagogique.',
+                              enseignant_id: creneau.enseignant_id )
         STDERR.putc '.'
 
         next unless rand > 0.25
-        creneau_emploi_du_temps = CreneauEmploiDuTemps
-                                  .where(matiere_id: CreneauEmploiDuTemps[ creneau.creneau_emploi_du_temps_id ].matiere_id)
-                                  .where(jour_de_la_semaine: Date.tomorrow.wday)
-                                  .join(:creneaux_emploi_du_temps_enseignants, creneau_emploi_du_temps_id: :id)
-                                  .where(enseignant_id: cours.enseignant_id)
-                                  .first
+        creneau_emploi_du_temps = CreneauEmploiDuTemps.where(matiere_id: CreneauEmploiDuTemps[ creneau.creneau_emploi_du_temps_id ].matiere_id)
+                                                      .where(jour_de_la_semaine: Date.tomorrow.wday)
+                                                      .join(:creneaux_emploi_du_temps_enseignants, creneau_emploi_du_temps_id: :id)
+                                                      .where(enseignant_id: cours.enseignant_id)
+                                                      .first
 
         Devoir.create(cours_id: cours.id,
                       type_devoir_id: TypeDevoir.all.sample.id,
