2a516222d0d0a4408e66218d3527f02c31138cda
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-06-24 11:37:49 +0200

    patch version 'error' message

--- lib/utils/release_notes.rb
+++ lib/utils/release_notes.rb
@@ -4,7 +4,7 @@ module Utils
   module_function
 
   def release_notes_to_json( version )
-    return { version => 'Aucune note de version renseignée.' }.to_json if version.match( /[0-9]+\.[0-9]+/ ).nil?
+    return { version => { '0' => 'Aucune note de version renseignée.' } }.to_json if version.match( /[0-9]+\.[0-9]+/ ).nil?
 
     version = version.split('.').map(&:to_i)
     release_notes = YAML.load( File.read( File.expand_path( '../../../RELEASE_NOTES.yaml', __FILE__ ) ) )
