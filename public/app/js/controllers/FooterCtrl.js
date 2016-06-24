77317bf6bdd58ea80b41f58f4e9702339b7ea650
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-06-24 11:44:09 +0200

    simplicity

--- public/app/js/controllers/FooterCtrl.js
+++ public/app/js/controllers/FooterCtrl.js
@@ -5,13 +5,9 @@ angular.module( 'cahierDeTextesClientApp' )
                 [ '$scope', '$state', '$stateParams', '$sce', 'VERSION', 'RELEASE_NOTES', 'User',
                   function ( $scope, $state, $stateParams, $sce, VERSION, RELEASE_NOTES, User ) {
                       var release_notes_to_html = function( release_notes ) {
-                          var minor = _(release_notes).keys()[0];
-
-                          var html = '<div class="release-notes"><h4>' + minor + '</h4>';
+                          var html = '<div class="release-notes"><h4>' + VERSION + '</h4>';
                           html += '<ul>';
-                          html += _(release_notes[ minor ]).map( function( patch ) {
-                              return '<li><h5>' + _(patch).keys()[0] + '</h5><ul>' + _(patch).map( function( item ) { return '<li>' + item + '</li>'; } ) + '</ul>';
-                          } ).join('');
+                          html += _(release_notes).map( function( item ) { return '<li>' + item + '</li>'; } ).join('');
                           html += '</ul>';
                           html += '</div>';
                           html += '<sub>(<a href="https://github.com/laclasse-com/service-cahiertextes/blob/' + VERSION + '/RELEASE_NOTES.yaml">voir l\'historique complet</a>)</sub>';
@@ -20,16 +16,16 @@ angular.module( 'cahierDeTextesClientApp' )
                       };
 
                       $scope.version = VERSION;
-                  $scope.release_notes = $sce.trustAsHtml( release_notes_to_html( RELEASE_NOTES ) );
+                      $scope.release_notes = $sce.trustAsHtml( release_notes_to_html( RELEASE_NOTES ) );
 
-                  User.get_user().then( function( response ) {
-                      $scope.current_user = response.data;
+                User.get_user().then( function( response ) {
+                    $scope.current_user = response.data;
 
-                      $scope.save_and_reload = function() {
-                          User.update_parameters( $scope.current_user.parametrage_cahier_de_textes )
-                              .success( function() {
-                                  $state.transitionTo( $state.current, $stateParams, { reload: true, inherit: true, notify: true } );
-                              } );
-                      };
-                  } );
-                } ] );
+                    $scope.save_and_reload = function() {
+                        User.update_parameters( $scope.current_user.parametrage_cahier_de_textes )
+                            .success( function() {
+                                $state.transitionTo( $state.current, $stateParams, { reload: true, inherit: true, notify: true } );
+                            } );
+                    };
+                } );
+               } ] );
