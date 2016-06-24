285bb6c655ef42424f1cf5af60d39cc6e6abd415
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-06-24 11:34:54 +0200

    tweaks release notes display

--- lib/utils/release_notes.rb
+++ lib/utils/release_notes.rb
@@ -4,12 +4,12 @@ module Utils
   module_function
 
   def release_notes_to_json( version )
-    return { version => 'Aucune note de version renseignée.' } if version.match( /[0-9]+\.[0-9]+/ ).nil?
+    return { version => 'Aucune note de version renseignée.' }.to_json if version.match( /[0-9]+\.[0-9]+/ ).nil?
 
     version = version.split('.').map(&:to_i)
     release_notes = YAML.load( File.read( File.expand_path( '../../../RELEASE_NOTES.yaml', __FILE__ ) ) )
 
-    { "#{version[0]}" => { "#{version[1]}" => release_notes[ version[0] ][ version[1] ] } }
+    { "#{version[0]}#{version[1]}" => release_notes[ version[0] ][ version[1] ] }
       .to_json
   end
 end
--- public/app/js/controllers/FooterCtrl.js
+++ public/app/js/controllers/FooterCtrl.js
@@ -5,12 +5,11 @@ angular.module( 'cahierDeTextesClientApp' )
                 [ '$scope', '$state', '$stateParams', '$sce', 'VERSION', 'RELEASE_NOTES', 'User',
                   function ( $scope, $state, $stateParams, $sce, VERSION, RELEASE_NOTES, User ) {
                       var release_notes_to_html = function( release_notes ) {
-                          var major = _(release_notes).keys()[0];
-                          var minor = _(release_notes[ major ]).keys()[0];
+                          var minor = _(release_notes).keys()[0];
 
-                          var html = '<div class="release-notes"><h4>' + major + '.' + minor + '</h4>';
+                          var html = '<div class="release-notes"><h4>' + minor + '</h4>';
                           html += '<ul>';
-                          html += _(release_notes[ major ][ minor ]).map( function( patch ) {
+                          html += _(release_notes[ minor ]).map( function( patch ) {
                               return '<li><h5>' + _(patch).keys()[0] + '</h5><ul>' + _(patch).map( function( item ) { return '<li>' + item + '</li>'; } ) + '</ul>';
                           } ).join('');
                           html += '</ul>';
@@ -21,16 +20,16 @@ angular.module( 'cahierDeTextesClientApp' )
                       };
 
                       $scope.version = VERSION;
-                      $scope.release_notes = $sce.trustAsHtml( release_notes_to_html( RELEASE_NOTES ) );
+                  $scope.release_notes = $sce.trustAsHtml( release_notes_to_html( RELEASE_NOTES ) );
 
-                      User.get_user().then( function( response ) {
-                          $scope.current_user = response.data;
+                  User.get_user().then( function( response ) {
+                      $scope.current_user = response.data;
 
-                          $scope.save_and_reload = function() {
-                              User.update_parameters( $scope.current_user.parametrage_cahier_de_textes )
-                                  .success( function() {
-                                      $state.transitionTo( $state.current, $stateParams, { reload: true, inherit: true, notify: true } );
-                                  } );
-                          };
-                      } );
-                  } ] );
+                      $scope.save_and_reload = function() {
+                          User.update_parameters( $scope.current_user.parametrage_cahier_de_textes )
+                              .success( function() {
+                                  $state.transitionTo( $state.current, $stateParams, { reload: true, inherit: true, notify: true } );
+                              } );
+                      };
+                  } );
+                } ] );
