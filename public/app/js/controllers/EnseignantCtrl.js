4b3bb19541183c591a6556eb383852160f5da5bf
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-08-02 09:07:46 +0200

    indentation and 'Pronote'

--- public/app/js/controllers/EnseignantCtrl.js
+++ public/app/js/controllers/EnseignantCtrl.js
@@ -2,37 +2,37 @@
 
 angular.module( 'cahierDeTextesClientApp' )
     .controller('EnseignantCtrl',
-		[ '$scope', '$state', 'current_user',
-		  function ( $scope, $state, current_user ) {
-		      $scope.tabs = [ { heading: 'Emploi du temps',
-					uisref: 'enseignant.emploi_du_temps',
-					css_class: 'glyphicon glyphicon-calendar',
-					active: true },
-				      { heading: 'Cahier de textes',
-					uisref: 'enseignant.cahier_de_textes',
-					css_class: 'glyphicon glyphicon-list',
-					active: false },
-				      { heading: 'Statistiques',
-					uisref: 'enseignant.stats',
-					css_class: 'glyphicon glyphicon-stats',
-					active: false } ];
+                [ '$scope', '$state', 'current_user',
+                  function ( $scope, $state, current_user ) {
+                      $scope.tabs = [ { heading: 'Emploi du temps',
+                                        uisref: 'enseignant.emploi_du_temps',
+                                        css_class: 'glyphicon glyphicon-calendar',
+                                        active: true },
+                                      { heading: 'Cahier de textes',
+                                        uisref: 'enseignant.cahier_de_textes',
+                                        css_class: 'glyphicon glyphicon-list',
+                                        active: false },
+                                      { heading: 'Statistiques',
+                                        uisref: 'enseignant.stats',
+                                        css_class: 'glyphicon glyphicon-stats',
+                                        active: false } ];
 
-		      if ( current_user.profil_actif.admin ) {
-			  $scope.tabs.push( { heading: 'Imports',
-					      uisref: 'enseignant.import',
-					      css_class: 'glyphicon glyphicon-import',
-					      active: false } );
-		      }
+                      if ( current_user.profil_actif.admin ) {
+                          $scope.tabs.push( { heading: 'Import Pronote',
+                                              uisref: 'enseignant.import',
+                                              css_class: 'glyphicon glyphicon-import',
+                                              active: false } );
+                      }
 
-		      _($scope.tabs).each( function( tab ) {
-			  tab.active = tab.uisref == $state.current.name;
-		      } );
+                      _($scope.tabs).each( function( tab ) {
+                          tab.active = tab.uisref == $state.current.name;
+                      } );
 
-		      $scope.set_active_tab = function( uisref ) {
-			  _($scope.tabs).each( function( tab ) {
-			      tab.active = tab.uisref == uisref;
-			  } );
-		      };
+                      $scope.set_active_tab = function( uisref ) {
+                          _($scope.tabs).each( function( tab ) {
+                              tab.active = tab.uisref == uisref;
+                          } );
+                      };
 
-		      $scope.current_user = current_user;
-		  } ] );
+                      $scope.current_user = current_user;
+                  } ] );
