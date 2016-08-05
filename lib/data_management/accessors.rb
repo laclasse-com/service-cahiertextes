12d241c1bced2608904d8ec5befdc84a4b3b11fb
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-08-05 16:00:15 +0200

    date_creation with no default value crashes

--- lib/data_management/accessors.rb
+++ lib/data_management/accessors.rb
@@ -8,8 +8,10 @@ module DataManagement
     def create_or_get( model_class, params )
       objet = model_class.where( params ).first
 
-      objet = model_class.create( params ) if objet.nil?
-      objet.update( date_creation: Sequel::SQLTime.now ) if model_class.method_defined?( :date_creation )
+      if objet.nil?
+        params[:date_creation] = Time.now if model_class.method_defined?( :date_creation )
+        objet = model_class.create( params )
+      end
 
       objet
     end
