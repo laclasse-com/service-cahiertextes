57de27d3acd9e20b15e7318f34337fe594136d8f
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-08-17 11:08:05 +0200

    indentation and fix #255

--- public/app/js/controllers/StatsEnseignantCtrl.js
+++ public/app/js/controllers/StatsEnseignantCtrl.js
@@ -30,6 +30,10 @@ angular.module( 'cahierDeTextesClientApp' )
                            return data;
                        };
 
+                       $scope.detail_regroupement = function( id ) {
+                           return _($scope.classes).findWhere( { id: id } );
+                       };
+
                        $scope.valide = function( saisie ) {
                            var disable_toastr = _(saisie).has( 'disable_toastr' );
                            saisie.cours.$valide().then( function( response ) {
@@ -104,7 +108,7 @@ angular.module( 'cahierDeTextesClientApp' )
                                    .each( function ( classe ) {
                                        var filled = classe.length;
                                        var validated = _( classe ).where( { valide: true } ).length;
-                                       var nom_regroupement = _($scope.classes).findWhere( { id: classe[ 0 ].regroupement_id } ).libelle;
+                                       var nom_regroupement = $scope.detail_regroupement( classe[ 0 ].regroupement_id ).libelle;
 
                                        $scope.graphiques.multiBarChart.data[0].values.push( { key: nom_regroupement,
                                                                                               x: nom_regroupement,
@@ -117,124 +121,124 @@ angular.module( 'cahierDeTextesClientApp' )
                                        $scope.graphiques.pieChart.data[ 1 ].value += validated;
                                    } );
                            }
-                       };
+                   };
 
-                       $scope.process_data = function () {
-                           if ( $scope.raw_data !== undefined ) {
-                               var _2_semaines_avant = moment().subtract( 2, 'weeks' );
-                               $scope.raw_data = _( $scope.raw_data )
-                                   .map( function ( saisie, index ) {
-                                       // on référence l'index d'origine dans chaque élément pour propager la validation
-                                       saisie.index = index;
-                                       saisie.cours = new Cours( saisie.cours );
-                                       saisie.regroupement_id = parseInt( saisie.regroupement_id );
-                                       saisie.month = moment( saisie.cours.date_cours ).month();
-                                       saisie.recent = moment( saisie.cours.date_cours ).isAfter( _2_semaines_avant );
-                                       // saisie.devoir = new Devoirs( saisie.devoir );
-                                       return saisie;
-                                   } );
+                   $scope.process_data = function () {
+                       if ( $scope.raw_data !== undefined ) {
+                           var _2_semaines_avant = moment().subtract( 2, 'weeks' );
+                           $scope.raw_data = _( $scope.raw_data )
+                               .map( function ( saisie, index ) {
+                                   // on référence l'index d'origine dans chaque élément pour propager la validation
+                                   saisie.index = index;
+                                   saisie.cours = new Cours( saisie.cours );
+                                   saisie.regroupement_id = parseInt( saisie.regroupement_id );
+                                   saisie.month = moment( saisie.cours.date_cours ).month();
+                                   saisie.recent = moment( saisie.cours.date_cours ).isAfter( _2_semaines_avant );
+                                   // saisie.devoir = new Devoirs( saisie.devoir );
+                                   return saisie;
+                               } );
 
-                               // consommation des données par les graphiques
-                               $scope.graphiques.populate( $scope.raw_data );
-                           }
-                       };
-
-                       $scope.current_user = current_user;
-                       $scope.enseignant_id = $stateParams.enseignant_id;
-                       if ( $scope.enseignant_id === undefined && $scope.enseignant_id != $scope.current_user.uid ) {
-                           $scope.enseignant_id = $scope.current_user.uid;
+                           // consommation des données par les graphiques
+                           $scope.graphiques.populate( $scope.raw_data );
                        }
+                   };
 
-                       // Récupération et consommation des données
-                       Annuaire.get_user( $scope.enseignant_id )
-                           .$promise.then(
-                               function ( response ) {
-                                   $scope.enseignant = response;
+                   $scope.current_user = current_user;
+                   $scope.enseignant_id = $stateParams.enseignant_id;
+                   if ( $scope.enseignant_id === undefined && $scope.enseignant_id != $scope.current_user.uid ) {
+                       $scope.enseignant_id = $scope.current_user.uid;
+                   }
 
-                                   $scope.enseignant.email_principal = _($scope.enseignant.emails).find( { principal: true } );
-                                   if ( _($scope.enseignant.email_principal).isUndefined() ) {
-                                       $scope.enseignant.email_principal = _($scope.enseignant.emails).find( { type: 'Ent' } );
-                                   }
-                                   if ( _($scope.enseignant.email_principal).isUndefined() ) {
-                                       $scope.enseignant.email_principal = _($scope.enseignant.emails).first();
-                                   }
+                   // Récupération et consommation des données
+                   Annuaire.get_user( $scope.enseignant_id )
+                   .$promise.then(
+                       function ( response ) {
+                           $scope.enseignant = response;
 
-                                   // filtrer les classes de l'enseignant sur l'établissement actif
-                                   $scope.enseignant.liste_classes = _.chain( $scope.enseignant.classes )
-                                       .reject( function( classe ) {
-                                           return classe.etablissement_code != $scope.current_user.profil_actif.etablissement_code_uai;
-                                       } )
-                                       .uniq( function ( classe ) {
-                                           return classe.classe_id;
-                                       } )
-                                       .value();
+                           $scope.enseignant.email_principal = _($scope.enseignant.emails).find( { principal: true } );
+                           if ( _($scope.enseignant.email_principal).isUndefined() ) {
+                               $scope.enseignant.email_principal = _($scope.enseignant.emails).find( { type: 'Ent' } );
+                           }
+                           if ( _($scope.enseignant.email_principal).isUndefined() ) {
+                               $scope.enseignant.email_principal = _($scope.enseignant.emails).first();
+                           }
 
-                                   $scope.enseignant.liste_matieres = _.chain( $scope.enseignant.classes ).pluck('matiere_libelle').uniq().value();
+                           // filtrer les classes de l'enseignant sur l'établissement actif
+                           $scope.enseignant.liste_classes = _.chain( $scope.enseignant.classes )
+                               .reject( function( classe ) {
+                                   return classe.etablissement_code != $scope.current_user.profil_actif.etablissement_code_uai;
+                               } )
+                               .uniq( function ( classe ) {
+                                   return classe.classe_id;
+                               } )
+                               .value();
 
-                                   $scope.enseignant.prof_principal = _.chain( $scope.enseignant.classes )
-                                       .filter( function ( matiere ) {
-                                           return matiere.prof_principal == 'O';
-                                       } )
-                                       .map( function ( matiere ) {
-                                           return matiere.classe_libelle;
-                                       } )
-                                       .value();
-                               } );
+                           $scope.enseignant.liste_matieres = _.chain( $scope.enseignant.classes ).pluck('matiere_libelle').uniq().value();
 
-                       API.get_enseignant( {
-                           enseignant_id: $scope.enseignant_id,
-                           uai: $scope.current_user[ 'profil_actif' ][ 'etablissement_code_uai' ]
-                       } )
-                           .$promise.then(
-                               function success( response ) {
-                                   var extract = function( saisies, id_name, traitement ) {
-                                       return _.chain( saisies )
-                                           .flatten()
-                                           .pluck( id_name )
-                                           .uniq()
-                                           .compact()
-                                           .reject( function( item_id ) { return item_id === 'undefined'; } )
-                                           .map( function( item_id ) { return traitement( item_id ); } )
-                                           .object()
-                                           .value();
-                                   };
+                           $scope.enseignant.prof_principal = _.chain( $scope.enseignant.classes )
+                               .filter( function ( matiere ) {
+                                   return matiere.prof_principal == 'O';
+                               } )
+                               .map( function ( matiere ) {
+                                   return matiere.classe_libelle;
+                               } )
+                               .value();
+                       } );
 
-                                   $scope.raw_data = response.saisies;
+                   API.get_enseignant( {
+                       enseignant_id: $scope.enseignant_id,
+                       uai: $scope.current_user[ 'profil_actif' ][ 'etablissement_code_uai' ]
+                   } )
+                   .$promise.then(
+                       function success( response ) {
+                           var extract = function( saisies, id_name, traitement ) {
+                               return _.chain( saisies )
+                                   .flatten()
+                                   .pluck( id_name )
+                                   .uniq()
+                                   .compact()
+                                   .reject( function( item_id ) { return item_id === 'undefined'; } )
+                                   .map( function( item_id ) { return traitement( item_id ); } )
+                                   .object()
+                                   .value();
+                           };
 
-                                   $scope.nb_saisies_visables = calc_nb_saisies_visables( $scope.raw_data );
+                           $scope.raw_data = response.saisies;
 
-                                   $scope.matieres = extract( $scope.raw_data, 'matiere_id',
-                                                              function( matiere_id ) {
-                                                                  var matiere = _($scope.current_user.profil_actif.matieres).findWhere({ id: matiere_id });
-                                                                  if ( _(matiere).isUndefined() ) {
-                                                                      matiere = Annuaire.get_matiere( matiere_id );
-                                                                  }
+                           $scope.nb_saisies_visables = calc_nb_saisies_visables( $scope.raw_data );
 
-                                                                  return [ matiere_id, matiere ];
-                                                              } );
+                           $scope.matieres = extract( $scope.raw_data, 'matiere_id',
+                                                      function( matiere_id ) {
+                                                          var matiere = _($scope.current_user.profil_actif.matieres).findWhere({ id: matiere_id });
+                                                          if ( _(matiere).isUndefined() ) {
+                                                              matiere = Annuaire.get_matiere( matiere_id );
+                                                          }
 
-                                   $scope.classes = _( extract( $scope.raw_data, 'regroupement_id',
-                                                                function( regroupement_id ) {
-                                                                    regroupement_id = parseInt( regroupement_id );
-                                                                    var regroupement = _($scope.current_user.profil_actif.regroupements).findWhere({ id: regroupement_id });
-                                                                    if ( _(regroupement).isUndefined() ) {
-                                                                        regroupement = Annuaire.get_regroupement( regroupement_id );
-                                                                    }
-                                                                    return [ regroupement_id, regroupement ];
-                                                                } ) ).toArray();
+                                                          return [ matiere_id, matiere ];
+                                                      } );
 
-                                   $scope.select_all_regroupements = function() {
-                                       $scope.selected_regroupements = $scope.classes;
-                                       $scope.process_data();
-                                   };
+                           $scope.classes = _( extract( $scope.raw_data, 'regroupement_id',
+                                                        function( regroupement_id ) {
+                                                            regroupement_id = parseInt( regroupement_id );
+                                                            var regroupement = _($scope.current_user.profil_actif.regroupements).findWhere({ id: regroupement_id });
+                                                            if ( _(regroupement).isUndefined() ) {
+                                                                regroupement = Annuaire.get_regroupement( regroupement_id );
+                                                            }
+                                                            return [ regroupement_id, regroupement ];
+                                                        } ) ).toArray();
 
-                                   $scope.select_no_regroupements = function() {
-                                       $scope.selected_regroupements = [];
-                                       $scope.process_data();
-                                   };
+                           $scope.select_all_regroupements = function() {
+                               $scope.selected_regroupements = $scope.classes;
+                               $scope.process_data();
+                           };
 
-                                   $q.all( $scope.classes ).then( function() {
-                                       $scope.select_all_regroupements();
-                                   } );
-                               } );
-                   } ] );
+                           $scope.select_no_regroupements = function() {
+                               $scope.selected_regroupements = [];
+                               $scope.process_data();
+                           };
+
+                           $q.all( $scope.classes ).then( function() {
+                               $scope.select_all_regroupements();
+                           } );
+                       } );
+                 } ] );
