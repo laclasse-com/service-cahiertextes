c029d5e0acc1673442a5e8dddebf68fca23ec272
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-09-21 15:51:02 +0200

    cleaning

--- lib/pronote.rb
+++ lib/pronote.rb
@@ -9,7 +9,6 @@ require_relative './annuaire_wrapper'
 require_relative '../models/models'
 
 # Consomme le fichier Emploi du temps exporté par Pronote
-# rubocop:disable Metrics/ModuleLength
 module ProNote
   module_function
 
@@ -40,11 +39,11 @@ module ProNote
   def decrypt_xml( encrypted_xml )
     encrypted_edt_export_file = Nokogiri::XML( encrypted_xml )
 
-    crypted_wrapped_data = Base64.decode64( encrypted_edt_export_file
-                                            .search( 'PARTENAIRE' )
-                                            .find do |part|
+    crypted_wrapped_data = Base64.decode64( encrypted_edt_export_file.search( 'PARTENAIRE' )
+                                                                     .find do |part|
                                               part.attributes[ 'NOM' ].value == PRONOTE[:nom_integrateur]
-                                            end.text )
+                                            end
+                                                                     .text )
     decrypted_wrapped_data = decrypt_wrapped_data( crypted_wrapped_data, PRONOTE[:cle_integrateur] )
     aes_secret_key = decrypted_wrapped_data[ 0..16 ]
     aes_iv = decrypted_wrapped_data[ 16..32 ]
@@ -60,4 +59,3 @@ module ProNote
     Nokogiri::XML( xml ).search( field ).children.text
   end
 end
-# rubocop:enable Metrics/ModuleLength
