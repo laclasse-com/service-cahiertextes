a036d74ac4b65761a2a6fb72b3cf9542b589bbba
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-09-27 09:23:58 +0200

    truncate url sent to log at 1023

--- public/app/js/services/log.js
+++ public/app/js/services/log.js
@@ -1,20 +1,20 @@
 angular.module( 'cahierDeTextesClientApp' )
     .service( 'log',
-	      [ '$http', '$state', 'APP_PATH', 'User',
-		function( $http, $state, APP_PATH, User ) {
-		    this.add = function( app, url, params ) {
-			User.get_user(  )
-			    .then( function( response ) {
-				var user = response.data;
-				$http.post( APP_PATH + '/api/v1/log',
-					    { app: app,
-					      uid: user.uid,
-					      uai: user.profil_actif.etablissement_code_uai,
-					      user_type: user.profil_actif.profil_id,
-					      timestamp: Date.now(),
-					      url: _(url).isNull() ? APP_PATH + $state.current.url: url,
-					      params: _(params).isNull() ? _($state.params).map( function( value, key ) { return key + '=' + value; } ).join( '&' ) : params } );
-			    } );
-		    };
-		}
-	      ] );
+              [ '$http', '$state', 'APP_PATH', 'User',
+                function( $http, $state, APP_PATH, User ) {
+                    this.add = function( app, url, params ) {
+                        User.get_user(  )
+                            .then( function( response ) {
+                                var user = response.data;
+                                $http.post( APP_PATH + '/api/v1/log',
+                                            { app: app,
+                                              uid: user.uid,
+                                              uai: user.profil_actif.etablissement_code_uai,
+                                              user_type: user.profil_actif.profil_id,
+                                              timestamp: Date.now(),
+                                              url: ( _(url).isNull() ? APP_PATH + $state.current.url: url ).substr( 0, 1023 ),
+                                              params: _(params).isNull() ? _($state.params).map( function( value, key ) { return key + '=' + value; } ).join( '&' ) : params } );
+                            } );
+                    };
+                }
+              ] );
