183c3957c39827c53fd5b8cc6b46c59585be1276
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-09-27 09:23:44 +0200

    user doesn't need to know about error 500

--- public/app/js/http_interceptors.js
+++ public/app/js/http_interceptors.js
@@ -2,37 +2,37 @@
 
 angular.module( 'cahierDeTextesClientApp' )
     .config( [ '$httpProvider',
-	       function( $httpProvider ) {
-		   $httpProvider.interceptors.push( [ '$q',
-						      function( $q ) {
-							  return {
-							      'response': function( response ) {
-								  //Will only be called for HTTP up to 300
-								  return response;
-							      },
-							      'responseError': function( rejection ) {
-								  if ( rejection.status === 401 ) {
-								      swal( { title: 'Erreur',
-									      text: 'Votre session a expirée.',
-									      type: 'error',
-									      showCancelButton: false,
-									      confirmButtonColor: '#ff6b55',
-									      confirmButtonText: 'Fermer',
-									      timer: 3000
-									    } );
-								  }
-								  if ( rejection.status === 500 ) {
-								      swal( { title: 'Erreur',
-									      text: 'L\'application a rencontrée une erreur durant le traitement serveur.',
-									      type: 'error',
-									      showCancelButton: false,
-									      confirmButtonColor: '#ff6b55',
-									      confirmButtonText: 'Fermer',
-									      timer: 3000
-									    } );
-								  }
-								  return $q.reject( rejection );
-							      }
-							  };
-						      } ] );
-	       } ] );
+               function( $httpProvider ) {
+                   $httpProvider.interceptors.push( [ '$q',
+                                                      function( $q ) {
+                                                          return {
+                                                              'response': function( response ) {
+                                                                  //Will only be called for HTTP up to 300
+                                                                  return response;
+                                                              },
+                                                              'responseError': function( rejection ) {
+                                                                  if ( rejection.status === 401 ) {
+                                                                      swal( { title: 'Erreur',
+                                                                              text: 'Votre session a expirée.',
+                                                                              type: 'error',
+                                                                              showCancelButton: false,
+                                                                              confirmButtonColor: '#ff6b55',
+                                                                              confirmButtonText: 'Fermer',
+                                                                              timer: 3000
+                                                                            } );
+                                                                  }
+                                                                  // if ( rejection.status === 500 ) {
+                                                                  //     swal( { title: 'Erreur',
+                                                                  //             text: 'L\'application a rencontrée une erreur durant le traitement serveur.',
+                                                                  //             type: 'error',
+                                                                  //             showCancelButton: false,
+                                                                  //             confirmButtonColor: '#ff6b55',
+                                                                  //             confirmButtonText: 'Fermer',
+                                                                  //             timer: 3000
+                                                                  //           } );
+                                                                  // }
+                                                                  return $q.reject( rejection );
+                                                              }
+                                                          };
+                                                      } ] );
+               } ] );
