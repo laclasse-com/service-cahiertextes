f609266ede7696c717e6005fa89b6736e2ab0ec0
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2016-11-18 16:26:14 +0100

    useless options

--- lib/uglify.rb
+++ lib/uglify.rb
@@ -3,11 +3,11 @@
 module Uglify
   module_function
 
-  def those_files( ary_files, options = nil )
-    Uglifier.compile( ary_files.map { |fichier| File.read( fichier ) }.join, options )
+  def those_files( ary_files )
+    Uglifier.compile( ary_files.map { |fichier| File.read( fichier ) }.join )
   end
 
-  def those_files_with_map( ary_files, options = nil )
-    Uglifier.compile_with_map( ary_files.map { |fichier| File.read( fichier ) }.join, options )
+  def those_files_with_map( ary_files )
+    Uglifier.compile_with_map( ary_files.map { |fichier| File.read( fichier ) }.join )
   end
 end
