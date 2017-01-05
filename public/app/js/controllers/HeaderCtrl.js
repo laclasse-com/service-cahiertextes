6d713209afd5812e7727f24a1fbb79a3569d8a6b
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-01-05 15:05:48 +0100

    indentation

--- public/app/js/controllers/HeaderCtrl.js
+++ public/app/js/controllers/HeaderCtrl.js
@@ -2,17 +2,17 @@
 
 angular.module( 'cahierDeTextesClientApp' )
     .controller('HeaderCtrl',
-		[ '$scope', '$state', 'User', 'Redirection', '$sce', 'DOCS_URL',
-		  function ( $scope, $state, User, Redirection, $sce, DOCS_URL ) {
-		      $scope.DOCS_URL_login = $sce.trustAsResourceUrl( DOCS_URL + '/login' );
-		      $scope.embedded = window != window.top;
-		      User.get_user().success( function( response ) {
-			  $scope.current_user = response;
-		      });
+                [ '$scope', '$state', 'User', 'Redirection', '$sce', 'DOCS_URL',
+                  function ( $scope, $state, User, Redirection, $sce, DOCS_URL ) {
+                      $scope.DOCS_URL_login = $sce.trustAsResourceUrl( DOCS_URL + '/login' );
+                      $scope.embedded = window != window.top;
+                      User.get_user().then( function( response ) {
+                          $scope.current_user = response;
+                      });
 
-		      $scope.reload = function() {
-			  // Changement du profil actif dans l'annuaire
-			  // Rechargement de l'application
-			  Redirection.doorman( [  ] );
-		      };
-		  } ] );
+                      $scope.reload = function() {
+                          // Changement du profil actif dans l'annuaire
+                          // Rechargement de l'application
+                          Redirection.doorman( [  ] );
+                      };
+                  } ] );
