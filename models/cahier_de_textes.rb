0d8bba06f28a7bd254e42c07c47426d7d2f80312
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-01-26 15:52:10 +0100

    module name

--- models/cahier_de_textes.rb
+++ models/cahier_de_textes.rb
@@ -8,8 +8,7 @@ class CahierDeTextes < Sequel::Model( :cahiers_de_textes )
   def statistiques
     cours = Cours.where( cahier_de_textes_id: id )
                  .where( cours__deleted: false )
-                 .where( "DATE_FORMAT( cours.date_creation, '%Y-%m-%d') >= '#{Utils.date_rentree}'" )
-    # .where { cours__date_creation >= Utils.date_rentree }
+                 .where( "DATE_FORMAT( cours.date_creation, '%Y-%m-%d') >= '#{CahierDeTextesApp::Utils.date_rentree}'" )
 
     creneaux = creneaux_emploi_du_temps
 
@@ -25,7 +24,6 @@ class CahierDeTextes < Sequel::Model( :cahiers_de_textes )
             tmp_cours = cours.association_join( :creneau_emploi_du_temps )
                              .where( matiere_id: record.values[:matiere_id] )
                              .where( "extract( month from date_cours ) = #{month}" )
-            # .where { date_cours.month == month }
 
             { mois: month,
               filled: tmp_cours.count,
