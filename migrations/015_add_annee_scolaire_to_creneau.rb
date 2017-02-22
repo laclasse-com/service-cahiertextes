efc280c39bcdb26683b98c69927909443f0fa455
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-02-22 15:15:16 +0100

    rename table into matcheable

---
+++ migrations/015_add_annee_scolaire_to_creneau.rb
@@ -0,0 +1,14 @@
+# coding: utf-8
+
+Sequel.migration do
+  change do
+    alter_table(:matchable) do
+      rename_column :sha256, :hash
+      drop_foreign_key :import_id
+      drop_constraint :id, type: :primary_key
+      drop_column :id
+      add_constraint :hash, type: :primary_key
+      drop_column :date_creation
+    end
+  end
+end
