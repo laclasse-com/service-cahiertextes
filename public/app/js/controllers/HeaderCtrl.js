9e2c944ae49e7bd4c0dee86d50ba759e0b1a8794
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-03-07 09:35:06 +0100

    don't load docs while developing

--- public/app/js/controllers/HeaderCtrl.js
+++ public/app/js/controllers/HeaderCtrl.js
@@ -4,6 +4,8 @@ angular.module( 'cahierDeTextesClientApp' )
     .controller('HeaderCtrl',
                 [ '$scope', '$state', 'User', 'Redirection', '$sce', 'DOCS_URL',
                   function ( $scope, $state, User, Redirection, $sce, DOCS_URL ) {
+                      $scope.load_docs = window.location.hostname !== 'localhost';
+
                       $scope.DOCS_URL_login = $sce.trustAsResourceUrl( DOCS_URL + '/login' );
                       $scope.embedded = window != window.top;
                       User.get_user().then( function( response ) {
