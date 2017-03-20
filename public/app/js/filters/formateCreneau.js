4ea228120d35ce97e809dc6101126c8aa0732313
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-03-20 10:29:59 +0100

    formatCreneau

--- public/app/js/filters/formateCreneau.js
+++ public/app/js/filters/formateCreneau.js
@@ -7,11 +7,15 @@ angular.module( 'cahierDeTextesClientApp' )
                    // Afficher un créneau : date + heure de début et heure de fin
                    // ex : "Jeu 19 juin 15:00 - 16:00"
                    return function( creneau ) {
+                       var label = '';
+
                        if ( _(creneau).has( 'start' ) ) {
-                           return $filter( 'amDateFormat' )( creneau.start, 'ddd DD MMM HH:mm' ) + ' - ' + $filter( 'amDateFormat' )( creneau.end, 'HH:mm' );
+                           label += $filter( 'amDateFormat' )( creneau.start, 'ddd DD MMM HH:mm' ) + ' - ' + $filter( 'amDateFormat' )( creneau.end, 'HH:mm' );
                        } else {
-                           return $filter( 'amDateFormat' )( creneau.heure_debut, 'ddd DD MMM HH:mm' ) + ' - ' + $filter( 'amDateFormat' )( creneau.heure_fin, 'HH:mm' );
+                           label += $filter( 'amDateFormat' )( creneau.heure_debut, 'ddd DD MMM HH:mm' ) + ' - ' + $filter( 'amDateFormat' )( creneau.heure_fin, 'HH:mm' );
                        }
+
+                       return label;
                    };
                }
              ] );
