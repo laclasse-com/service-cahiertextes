0c6dbd9149391c4ec221f457d7049143dc0cf341
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-07-13 16:35:18 +0200

    dead code

--- api/emplois_du_temps.rb
+++ api/emplois_du_temps.rb
@@ -17,24 +17,5 @@ module CahierDeTextesApp
                                          user_regroupements_ids( params[:uid] ),
                                          params[:uid] )
     end
-
-    # format :txt
-    # desc 'emploi du temps de l\'utilisateur durant l\'intervalle de dates donné au format ical'
-    # params do
-    #   requires :debut, type: Date
-    #   requires :fin, type: Date
-
-    #   optional :uid
-    # end
-    # get '/ics' do
-    #   content_type 'text/calendar'
-    #   header 'Content-Disposition', "filename*=UTF-8''emploi_du_temps_#{params.key?(:uid) ? params[:uid] : user['id']}.ics"
-
-    #   DataManagement::EmploiDuTemps.ical( Date.parse( params[:debut].iso8601 ),
-    #                                       Date.parse( params[:fin].iso8601 ),
-    #                                       user_regroupements_ids( params[:uid] ),
-    #                                       user[:user_detailed]['profil_actif']['profil_id'],
-    #                                       params[:uid] )
-    # end
   end
 end
