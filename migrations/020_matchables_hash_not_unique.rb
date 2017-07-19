992c8a2f0ed78c018f26909346abaec961cc0f1f
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-07-19 15:46:26 +0200

    matchables.hash_item doesn't have to be unique

---
+++ migrations/020_matchables_hash_not_unique.rb
@@ -0,0 +1,10 @@
+# coding: utf-8
+
+Sequel.migration do
+  change do
+    alter_table( :matchables ) do
+      drop_index( :hash_item, name: :sha256 )
+    end
+  end
+end
+puts 'applying 020_matchables_hash_not_unique.rb'
