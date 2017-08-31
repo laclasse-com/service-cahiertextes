45615406e13a2b1e998395f9a28296eb63528e83
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-08-31 16:09:22 +0200

    now useless code

--- models/cahier_de_textes.rb
+++ models/cahier_de_textes.rb
@@ -13,30 +13,10 @@ class CahierDeTextes < Sequel::Model( :cahiers_de_textes )
   end
 
   def statistiques
-    cours = Cours.where( cahier_de_textes_id: id )
-                 .where( Sequel.~( Sequel.qualify( "cours", "deleted" ) ) )
-                 .where( Sequel.lit( "DATE_FORMAT( cours.date_creation, '%Y-%m-%d') >= '#{CahierDeTextesApp::Utils.date_rentree}'" ) )
-
     creneaux = creneaux_emploi_du_temps
 
     { regroupement_id: regroupement_id,
       creneaux_emploi_du_temps: { vides: creneaux.select { |creneau| creneau.cours.empty? && creneau.devoirs.empty? }.map( &:id ),
-                                  pleins: creneaux.select { |creneau| !creneau.cours.empty? || !creneau.devoirs.empty? }.map( &:id ) },
-      matieres: cours.association_join( :creneau_emploi_du_temps )
-                     .select( :matiere_id )
-                     .group_by( :matiere_id )
-                     .map do |record|
-        { matiere_id: record.values[ :matiere_id ],
-          mois: (1..12).map do |month|
-            tmp_cours = cours.association_join( :creneau_emploi_du_temps )
-                             .where( Sequel.&( Sequel.qualify( "creneau_emploi_du_temps", "deleted"), false ) )
-                             .where( matiere_id: record.values[:matiere_id] )
-                             .where( Sequel.lit( "extract( month from date_cours ) = #{month}" ) )
-
-            { mois: month,
-              filled: tmp_cours.count,
-              validated: tmp_cours.where( :date_validation ).count }
-          end }
-      end }
+                                  pleins: creneaux.select { |creneau| !creneau.cours.empty? || !creneau.devoirs.empty? }.map( &:id ) } }
   end
 end
