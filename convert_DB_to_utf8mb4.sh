0bd9ba2a75840ed5f097856298665d0adc4bd2a7
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-09-01 15:38:48 +0200

    add utf8mb4 conversion script

---
+++ convert_DB_to_utf8mb4.sh
@@ -0,0 +1,7 @@
+FILENAME=cahierdetextes_$(date+%F).sql
+
+mysqldump --add-drop-table -u cahierdetextes -p cahierdetextes > $FILENAME
+cp ${FILENAME} ${FILENAME}.orig
+sed -i 's|CHARSET=latin1|CHARSET=utf8mb4|g' $FILENAME
+cat $FILENAME | iconv -f latin1 -t utf8 > ${FILENAME}.utf8mb4
+mysql -u cahierdetextes -p cahierdetextes < $FILENAME.utf8mb4
