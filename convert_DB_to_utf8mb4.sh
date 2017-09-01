b91e7a72b2070d72936e1ab7df16bbd4ae16c2ff
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-09-01 15:43:22 +0200

    configurable DB name

--- convert_DB_to_utf8mb4.sh
+++ convert_DB_to_utf8mb4.sh
@@ -2,10 +2,12 @@
 
 cat config/database.rb
 
-FILENAME=cahierdetextes_$(date +%F).sql
+DBNAME=${DBNAME:-cahierdetextes}
 
-mysqldump --add-drop-table -u cahierdetextes -p cahierdetextes > $FILENAME
+FILENAME=${DBNAME}_$(date +%F).sql
+
+mysqldump --add-drop-table -u ${DBNAME} -p ${DBNAME} > $FILENAME
 cp ${FILENAME} ${FILENAME}.orig
 sed -i 's|CHARSET=latin1|CHARSET=utf8mb4|g' $FILENAME
 cat $FILENAME | iconv -f latin1 -t utf8 > ${FILENAME}.utf8mb4
-mysql -u cahierdetextes -p cahierdetextes < $FILENAME.utf8mb4
+mysql -u ${DBNAME} -p ${DBNAME} < $FILENAME.utf8mb4
