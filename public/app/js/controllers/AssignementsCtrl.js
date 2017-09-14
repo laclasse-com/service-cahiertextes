ad7ada6aa08bf20febc516023fcabaec8d516ec8
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2017-09-14 10:10:08 +0200

    -current_user in assignements

--- public/app/js/controllers/AssignementsCtrl.js
+++ public/app/js/controllers/AssignementsCtrl.js
@@ -3,18 +3,14 @@
 angular.module( 'cahierDeTextesClientApp' )
     .controller('AssignementsCtrl',
                 [ '$scope', '$sce', '$timeout', 'toastr', '$state', 'moment',
-                  'APP_PATH', 'URL_DOCS', 'API', 'Annuaire', 'Devoirs', 'Cours', 'CreneauxEmploiDuTemps',
-                  'current_user',
+                  'APP_PATH', 'URL_DOCS', 'API', 'Annuaire', 'Devoirs', 'Cours', 'CreneauxEmploiDuTemps', 'User',
                   function( $scope, $sce, $timeout, toastr, $state, moment,
-                            APP_PATH, URL_DOCS, API, Annuaire, Devoirs, Cours, CreneauxEmploiDuTemps,
-                            current_user ) {
+                            APP_PATH, URL_DOCS, API, Annuaire, Devoirs, Cours, CreneauxEmploiDuTemps, User ) {
                       // popup d'affichage des détails
                       $scope.affiche_faits = false;
                       $scope.tri_ascendant = true;
                       $scope.popup_ouverte = false;
                       $scope.matiere_selected = null;
-
-                      $scope.current_user = current_user;
 
                       var getCours = _.memoize( function( id ) {
                           return Cours.get({ id: id });
@@ -23,40 +19,6 @@ angular.module( 'cahierDeTextesClientApp' )
                       var getCreneauxEmploiDuTemps = _.memoize( function( id ) {
                           return CreneauxEmploiDuTemps.get({ id: id });
                       } );
-
-                      var retrieve_data = function() {
-                          $scope.from_date = moment().subtract( $scope.period_offset, 'months' ).subtract( 2, 'weeks' ).toDate();
-                          $scope.to_date = moment().subtract( $scope.period_offset, 'months' ).add( 2, 'weeks' ).toDate();
-
-                          API.query_devoirs({ 'date_due>': $scope.from_date,
-                                              'date_due<': $scope.to_date,
-                                              'groups_ids[]': $scope.current_user.profil_actif.type === 'TUT' ? _($scope.current_user.enfant_actif.enfant.groups).pluck('group_id') : _($scope.current_user.groups).pluck('group_id'),
-                                              uid: $scope.current_user.profil_actif.type === 'TUT' ? $scope.current_user.enfant_actif.child_id : null })
-                              .$promise.then(function( response ) {
-                                  $scope.matieres = {};
-
-                                  $scope.all_devoirs = _(response).map( function( devoir ) {
-                                      devoir.cours = getCours( devoir.cours_id );
-                                      devoir.creneau_emploi_du_temps = getCreneauxEmploiDuTemps( devoir.creneau_emploi_du_temps_id );
-
-                                      devoir.creneau_emploi_du_temps.$promise
-                                          .then( function() {
-                                              Annuaire.get_subject( devoir.creneau_emploi_du_temps.matiere_id )
-                                                  .then( function( response ) {
-                                                      devoir.matiere = response.data;
-                                                      $scope.matieres[ devoir.matiere.id ] = devoir.matiere;
-                                                  } );
-                                          } );
-
-                                      _(devoir.ressources).each( function( ressource ) {
-                                          ressource.url = $sce.trustAsResourceUrl( URL_DOCS + '/api/connector?cmd=file&target=' + ressource.hash );
-                                      } );
-
-                                      return devoir;
-                                  });
-                                  $scope.filter_data( $scope.matiere_selected );
-                              });
-                      };
 
                       $scope.filter_data = function( matiere ) {
                           if ( _(matiere).isNull() ) {
@@ -79,11 +41,51 @@ angular.module( 'cahierDeTextesClientApp' )
                                        label: offset == 0 ? 'ce mois' : moment().add( offset * -1, 'months' ).fromNow() };
                           } );
 
-                      $scope.$watch( 'period_offset', function() {
-                          retrieve_data();
-                      } );
-
                       $scope.incr_offset = function() { $scope.period_offset++; };
                       $scope.decr_offset = function() { $scope.period_offset--; };
                       $scope.reset_offset = function() { $scope.period_offset = 0; };
+
+                      User.get_user()
+                          .then( function( response ) {
+                              $scope.current_user = response.data;
+
+                              var retrieve_data = function() {
+                                  $scope.from_date = moment().subtract( $scope.period_offset, 'months' ).subtract( 2, 'weeks' ).toDate();
+                                  $scope.to_date = moment().subtract( $scope.period_offset, 'months' ).add( 2, 'weeks' ).toDate();
+
+                                  API.query_devoirs({ 'date_due>': $scope.from_date,
+                                                      'date_due<': $scope.to_date,
+                                                      'groups_ids[]': $scope.current_user.profil_actif.type === 'TUT' ? _($scope.current_user.enfant_actif.enfant.groups).pluck('group_id') : _($scope.current_user.groups).pluck('group_id'),
+                                                      uid: $scope.current_user.profil_actif.type === 'TUT' ? $scope.current_user.enfant_actif.child_id : null })
+                                      .$promise.then(function( response ) {
+                                          $scope.matieres = {};
+
+                                          $scope.all_devoirs = _(response).map( function( devoir ) {
+                                              devoir.cours = getCours( devoir.cours_id );
+                                              devoir.creneau_emploi_du_temps = getCreneauxEmploiDuTemps( devoir.creneau_emploi_du_temps_id );
+
+                                              devoir.creneau_emploi_du_temps.$promise
+                                                  .then( function() {
+                                                      Annuaire.get_subject( devoir.creneau_emploi_du_temps.matiere_id )
+                                                          .then( function( response ) {
+                                                              devoir.matiere = response.data;
+                                                              $scope.matieres[ devoir.matiere.id ] = devoir.matiere;
+                                                          } );
+                                                  } );
+
+                                              _(devoir.ressources).each( function( ressource ) {
+                                                  ressource.url = $sce.trustAsResourceUrl( URL_DOCS + '/api/connector?cmd=file&target=' + ressource.hash );
+                                              } );
+
+                                              return devoir;
+                                          });
+                                          $scope.filter_data( $scope.matiere_selected );
+                                      });
+                              };
+
+
+                              $scope.$watch( 'period_offset', function() {
+                                  retrieve_data();
+                              } );
+                          } );
                   } ] );
