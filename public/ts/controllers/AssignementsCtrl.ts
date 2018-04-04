9a2d4575bff924f7135a01d3d59b037b48c032f6
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2018-04-04 16:10:06 +0200

    refactoring assignements

--- public/ts/controllers/AssignementsCtrl.ts
+++ public/ts/controllers/AssignementsCtrl.ts
@@ -7,10 +7,10 @@ angular.module('cahierDeTextesClientApp')
       function($scope, $sce, $timeout, toastr, $state, moment,
         APP_PATH, URL_DOCS, API, Annuaire, Devoirs, Cours, CreneauxEmploiDuTemps, CurrentUser) {
         let ctrl = $scope;
+        ctrl.$ctrl = ctrl;
 
         ctrl.affiche_faits = false;
         ctrl.tri_ascendant = true;
-        ctrl.popup_ouverte = false;
         ctrl.matiere_selected = null;
 
         let getCours = _.memoize(function(id) {
@@ -48,47 +48,46 @@ angular.module('cahierDeTextesClientApp')
         ctrl.decr_offset = function() { ctrl.period_offset--; };
         ctrl.reset_offset = function() { ctrl.period_offset = 0; };
 
+        let retrieve_data = function() {
+          ctrl.from_date = moment().subtract(ctrl.period_offset, 'months').subtract(2, 'weeks').toDate();
+          ctrl.to_date = moment().subtract(ctrl.period_offset, 'months').add(2, 'weeks').toDate();
+
+          API.query_devoirs({
+            'date_due>': ctrl.from_date,
+            'date_due<': ctrl.to_date,
+            'groups_ids[]': ctrl.current_user.enfant_actif ? _(ctrl.current_user.enfant_actif.enfant.groups).pluck('group_id') : _(ctrl.current_user.groups).pluck('group_id'),
+            'uid': ctrl.current_user.enfant_actif ? ctrl.current_user.enfant_actif.child_id : ctrl.current_user.id,
+            'check_done': ctrl.current_user.is(['ELV'])
+          })
+            .$promise.then(function(response) {
+              ctrl.matieres = {};
+
+              ctrl.all_devoirs = _(response).map(function(devoir) {
+                devoir.cours = getCours(devoir.cours_id);
+                devoir.creneau_emploi_du_temps = getCreneauxEmploiDuTemps(devoir.creneau_emploi_du_temps_id);
+
+                devoir.creneau_emploi_du_temps.$promise
+                  .then(function() {
+                    Annuaire.get_subject(devoir.creneau_emploi_du_temps.matiere_id)
+                      .then(function(response) {
+                        devoir.matiere = response.data;
+                        ctrl.matieres[devoir.matiere.id] = devoir.matiere;
+                      });
+                  });
+
+                _(devoir.ressources).each(function(ressource) {
+                  ressource.url = $sce.trustAsResourceUrl(`${URL_DOCS}/api/connector?cmd=file&target=${ressource.hash}`);
+                });
+
+                return devoir;
+              });
+              ctrl.filter_data(ctrl.matiere_selected);
+            });
+        };
+
         CurrentUser.get()
           .then(function(response) {
             ctrl.current_user = response.data;
-
-            let retrieve_data = function() {
-              ctrl.from_date = moment().subtract(ctrl.period_offset, 'months').subtract(2, 'weeks').toDate();
-              ctrl.to_date = moment().subtract(ctrl.period_offset, 'months').add(2, 'weeks').toDate();
-
-              API.query_devoirs({
-                'date_due>': ctrl.from_date,
-                'date_due<': ctrl.to_date,
-                'groups_ids[]': ctrl.current_user.enfant_actif ? _(ctrl.current_user.enfant_actif.enfant.groups).pluck('group_id') : _(ctrl.current_user.groups).pluck('group_id'),
-                'uid': ctrl.current_user.enfant_actif ? ctrl.current_user.enfant_actif.child_id : ctrl.current_user.id,
-                'check_done': ctrl.current_user.is(['ELV'])
-              })
-                .$promise.then(function(response) {
-                  ctrl.matieres = {};
-
-                  ctrl.all_devoirs = _(response).map(function(devoir) {
-                    devoir.cours = getCours(devoir.cours_id);
-                    devoir.creneau_emploi_du_temps = getCreneauxEmploiDuTemps(devoir.creneau_emploi_du_temps_id);
-
-                    devoir.creneau_emploi_du_temps.$promise
-                      .then(function() {
-                        Annuaire.get_subject(devoir.creneau_emploi_du_temps.matiere_id)
-                          .then(function(response) {
-                            devoir.matiere = response.data;
-                            ctrl.matieres[devoir.matiere.id] = devoir.matiere;
-                          });
-                      });
-
-                    _(devoir.ressources).each(function(ressource) {
-                      ressource.url = $sce.trustAsResourceUrl(`${URL_DOCS}/api/connector?cmd=file&target=${ressource.hash}`);
-                    });
-
-                    return devoir;
-                  });
-                  ctrl.filter_data(ctrl.matiere_selected);
-                });
-            };
-
 
             ctrl.$watch('period_offset', function() {
               retrieve_data();
