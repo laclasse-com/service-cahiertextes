bee35dc3c38c875942ca0210b457810522adcfe4
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2018-09-06 09:34:40 +0200

    don't log connections

---
+++ migrations/021_drop_date_connexion.rb
@@ -0,0 +1,8 @@
+Sequel.migration do
+    up do
+        alter_table( :users_parameters ) do
+            drop_column :date_connexion
+        end
+    end
+end
+puts 'applying 021_drop_date_connexion.rb'
