13c21d78fb11c451b85a9266e6c82b9940271e1b
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2018-10-30 15:22:46 +0100

    refactor decrypted pronote data output

--- routes/api/import.rb
+++ routes/api/import.rb
@@ -46,11 +46,76 @@ module Routes
                                   hash = Hash.from_xml( File.open( params['file']['tempfile'] ) )[:ExportEmploiDuTemps]
                               end
 
-                              %w[ Eleves Etiquettes MotifsAbsence Absences Professeurs Personnels Materiels Nomenclatures Responsables ].each do |key|
-                                  hash.delete( key.to_sym )
-                              end
+                              slot_length_minutes = hash[:GrilleHoraire].first[:DureePlace].to_i
+                              slots_list = hash[:GrilleHoraire].first[:PlacesParJour].first[:Place]
 
-                              hash
+                              {
+                                  structure_id: hash[:UAI],
+                                  subjects: hash[:Matieres].first[:Matiere].map do |subject|
+                                      {
+                                          id: subject[:Ident].to_i,
+                                          name: subject[:Libelle]
+                                      }
+                                  end,
+                                  rooms: hash[:Salles].first[:Salle].map do |room|
+                                      {
+                                          id: room[:Ident].to_i,
+                                          name: room[:Nom]
+                                      }
+                                  end,
+                                  classes: hash[:Classes].first[:Classe].map do |group|
+                                      {
+                                          id: group[:Ident].to_i,
+                                          name: group[:Nom],
+                                          parts_ids: !group.key?(:PartieDeClasse) ? [] : group[:PartieDeClasse].map { |pdc| pdc[:Ident].to_i }
+                                      }
+                                  end,
+                                  groupes: hash[:Groupes].first[:Groupe].map do |group|
+                                      {
+                                          id: group[:Ident].to_i,
+                                          name: group[:Nom],
+                                          parts_ids: group[:PartieDeClasse].map { |pdc| pdc[:Ident].to_i }
+                                      }
+                                  end,
+                                  timeslots: hash[:Cours].first[:Cours].map do |timeslot|
+                                      start_time = slots_list[ timeslot[:NumeroPlaceDebut].to_i ][:LibelleHeureDebut]
+                                      end_time = (Time.parse(start_time) + timeslot[:NombrePlaces].to_i * slot_length_minutes.minutes).iso8601.split('T')[1].split('+').first
+
+                                      {
+                                          yearly: timeslot[:Annuel] == "1",
+                                          weekday: timeslot[:Jour].to_i,
+                                          start_time: start_time,
+                                          end_time: end_time,
+                                          subjects_ids: !timeslot.key?(:Matiere) ? -1 : timeslot[:Matiere].map { |subject| subject[:Ident].to_i },
+                                          classes: !timeslot.key?(:Classe) ? [] : timeslot[:Classe].map do |group| # rubocop:disable Style/MultilineTernaryOperator
+                                              {
+                                                  id: group[:Ident].to_i,
+                                                  active_weeks: group[:Semaines].to_i
+                                              }
+                                          end,
+                                          partial_classes: !timeslot.key?(:PartieDeClasse) ? [] : timeslot[:PartieDeClasse].map do |group| # rubocop:disable Style/MultilineTernaryOperator
+                                              {
+                                                  id: group[:Ident].to_i,
+                                                  active_weeks: group[:Semaines].to_i
+                                              }
+                                          end,
+                                          groupes: !timeslot.key?(:Groupe) ? [] : timeslot[:Groupe].map do |group| # rubocop:disable Style/MultilineTernaryOperator
+                                              {
+                                                  id: group[:Ident].to_i,
+                                                  active_weeks: group[:Semaines].to_i
+                                              }
+                                          end,
+                                          rooms: !timeslot.key?(:Salle) ? [] : timeslot[:Salle].map do |room| # rubocop:disable Style/MultilineTernaryOperator
+                                              {
+                                                  id: room[:Ident].to_i,
+                                                  active_weeks: room[:Semaines].to_i
+                                              }
+                                          end
+                                          # ,
+                                          # cancelled_weeks: !timeslot.key?(:SemainesAnnulation) ? -1 : timeslot[:SemainesAnnulation].to_i
+                                      }
+                                  end
+                              }
                           end )
                 end
             end
