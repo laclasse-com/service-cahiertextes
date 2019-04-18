48d44479b3d29a1c749a9e12fee017228469877b
Gwenhael Le Moine <gwenhael.le.moine@gmail.com>
2019-04-18 09:34:42 +0200

    remove obsolete code

--- lib/data_management.rb
+++ lib/data_management.rb
@@ -4,4 +4,3 @@ require_relative '../models/models'
 
 require_relative './data_management/accessors'
 require_relative './data_management/cleansing'
-require_relative './data_management/emploi_du_temps'
